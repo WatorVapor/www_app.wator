d56f299ac293243ec87444afe5656fc00363b59b
Wator Vapor <watorvapor@gmail.com>
2017-09-06 21:50:29 +0900

    www

--- wator/app/Providers/EventServiceProvider.php
+++ wator/app/Providers/EventServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Providers;
+namespace Wator\Providers;
 
 use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -13,8 +13,8 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\Event' => [
-            'App\Listeners\EventListener',
+        'Wator\Events\Event' => [
+            'Wator\Listeners\EventListener',
         ],
     ];
 
