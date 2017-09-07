9dbd4df12f5f00aa14443ac05c2252cb9d5c378d
Wator Vapor <watorvapor@gmail.com>
2017-09-07 23:45:43 +0900

    Update navbar.blade.php

--- wator/resources/views/wator/navbar.blade.php
+++ wator/resources/views/wator/navbar.blade.php
@@ -38,7 +38,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container">
   <div class="collapse navbar-collapse">
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+    <ul class="navbar-nav mr-auto d-lg-table-cell">
       <li class="nav-item active">
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
