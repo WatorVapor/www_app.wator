b5e3520fe6b2e46a8b3cdf47c8f8130c0f08c165
Wator Vapor <watorvapor@gmail.com>
2017-09-07 22:33:35 +0900

    home

---
+++ wator/resources/views/home/about.blade.php
@@ -0,0 +1,14 @@
+@extends('wator.app')
+@section('content')
+<div class="container">
+  <div class="row" >
+    <div class="col-md-10 col-md-offset-1">
+    <div class="col-text text-left">
+      <pre class="bg-danger">
+       {{trans('about.content')}}
+      </pre>
+    </div>
+    </div>
+  </div>
+</div>
+@endsection
