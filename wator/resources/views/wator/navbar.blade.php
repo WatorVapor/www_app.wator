432529bea6a63aa78c79bfec3ca8dc3a228532c3
Wator Vapor <watorvapor@gmail.com>
2017-09-07 23:36:07 +0900

    Update navbar.blade.php

--- wator/resources/views/wator/navbar.blade.php
+++ wator/resources/views/wator/navbar.blade.php
@@ -35,10 +35,36 @@
 ?>
 
 
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#">Navbar</a>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<!--
 
 <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light ">
   <div class="container">
-    <div class="collapse navbar-collapse" id="navbarNav">
+    <div class="collapse navbar-collapse navbar-left" id="navbarNav">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
           <a class="btn btn-lg btn-primary" href="/" role="button">
@@ -52,8 +78,29 @@
         </li>
      </ul>
     </div>	
+
+    <div class="collapse navbar-collapse navbar-right">
+      <a tabindex="0" href="#" class="btn btn-lg btn-success" data-container="body" data-html="true" data-trigger="focus" data-toggle="popover" data-placement="bottom" data-content="{{ $langs }}">
+        <i class="material-icons md-48">language</i>
+      </a>
+      <a tabindex="1" href="#" class="btn btn-lg btn-success" data-container="body" data-html="true" data-trigger="focus" data-toggle="popover" data-placement="bottom" data-content="{{ $apps }}">
+        <i class="material-icons md-48">apps</i>
+      </a>
+      @if(isset($RSAAuth_Passed))
+      <a tabindex="2" href="#" role="button" class="btn btn-lg btn-success" data-container="body" data-html="true" data-trigger="focus" data-toggle="popover" data-placement="bottom" title="{{ $user_title }}" data-content="{{ $user }}">
+        <i class="material-icons md-48">person</i>
+        <span class="icon-bar">{{ mb_substr($nav_login_show_name,0,2,'UTF-8') }}</span>
+      </a>
+      @else
+      <a role="button" class="btn btn-success btn-lg" href="/account/signup" role="button">
+        <i class="material-icons md-48">person_add</i>
+      </a>
+      @endif
+    </div>
+
   </div>
 </nav>
+-->
 
 <!--
 
