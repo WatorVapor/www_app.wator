1e07764ae76390697740ce898e4e33d41c388aec
Wator Vapor <watorvapor@gmail.com>
2017-09-08 00:18:00 +0900

    Update app.blade.php

--- wator/resources/views/wator/app.blade.php
+++ wator/resources/views/wator/app.blade.php
@@ -16,7 +16,16 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-    @yield('navbar')
+
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light ">
+      <div class="container">
+        @include('wator.navbar')
+        @yield('appnavbar')
+      </div>
+      <script>
+        $('[data-toggle="popover"]').popover();
+      </script>
+    </nav>
     @yield('content')
   </body>
 </html>
