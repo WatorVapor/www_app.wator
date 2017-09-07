e5f4288e740569fcc02d6abc9d17c7f10f215ef6
Wator Vapor <watorvapor@gmail.com>
2017-09-08 00:21:09 +0900

    Update navbar.blade.php

--- wator/resources/views/home/navbar.blade.php
+++ wator/resources/views/home/navbar.blade.php
@@ -1,1 +1,12 @@
-
+<ul class="navbar-nav justify-content-center">
+  <li class="nav-item active">
+    <a class="btn btn-lg btn-primary nav-btn" href="/" role="button">
+      <i class="material-icons md-48">help</i>
+    </a>
+  </li>
+  <li class="nav-item active ml-lg-3">
+    <a class="btn btn-lg btn-primary nav-btn" href="/about" role="button">
+      <i class="material-icons md-48">settings</i>
+    </a>
+  </li>
+</ul>
