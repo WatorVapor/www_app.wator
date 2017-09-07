46445753d58cdace89fa60cb9c218f38bbda692c
Wator Vapor <watorvapor@gmail.com>
2017-09-07 22:25:52 +0900

    Update web.php

--- wator/routes/web.php
+++ wator/routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/about', 'AboutController@index');
+Route::get('/welcome', 'WelcomeController@index');
+Route::get('/', 'WelcomeController@index');
