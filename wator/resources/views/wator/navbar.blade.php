248644a07f7d27b3acd250a77da9fd3dab181fd9
Wator Vapor <watorvapor@gmail.com>
2017-09-07 22:53:17 +0900

    Update navbar.blade.php

--- wator/resources/views/wator/navbar.blade.php
+++ wator/resources/views/wator/navbar.blade.php
@@ -37,6 +37,10 @@
 
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">
+    <img src="/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+    Bootstrap
+  </a>
 </nav>
 
 <!--
