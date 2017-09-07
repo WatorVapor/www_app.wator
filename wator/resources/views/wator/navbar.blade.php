52e5da4d464710e6aadd8d64f1fab2411d2449f4
Wator Vapor <watorvapor@gmail.com>
2017-09-08 00:06:16 +0900

    Update navbar.blade.php

--- wator/resources/views/wator/navbar.blade.php
+++ wator/resources/views/wator/navbar.blade.php
@@ -37,12 +37,12 @@
 <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light ">
   <div class="container">
     <ul class="navbar-nav justify-content-start">
-      <li class="nav-item active ml-lg-1">
+      <li class="nav-item active">
         <a class="btn btn-lg btn-primary nav-btn" href="/" role="button">
           <i class="material-icons md-48">home</i>
         </a>
       </li>
-      <li class="nav-item active ml-lg-1">
+      <li class="nav-item active ml-lg-3">
         <a class="btn btn-lg btn-primary nav-btn" href="/about" role="button">
           <i class="material-icons md-48">info</i>
         </a>
@@ -50,18 +50,21 @@
    </ul>
 
     <ul class="navbar-nav justify-content-end">
-      <li class="nav-item active">
-        <a tabindex="0" href="#" class="btn btn-lg btn-success" data-container="body" data-html="true" data-trigger="focus" data-toggle="popover" data-placement="bottom" data-content="{{ $langs }}">
+      <li class="nav-item active mr-lg-3">
+        <a tabindex="0" href="#" class="btn btn-lg btn-success nav-btn" data-container="body" data-html="true" data-trigger="focus" data-toggle="popover" data-placement="bottom" data-content="{{ $langs }}">
           <i class="material-icons md-48">language</i>
         </a>
       </li>
       <li class="nav-item active">
-        <a tabindex="1" href="#" class="btn btn-lg btn-success" data-container="body" data-html="true" data-trigger="focus" data-toggle="popover" data-placement="bottom" data-content="{{ $apps }}">
+        <a tabindex="1" href="#" class="btn btn-lg btn-success nav-btn" data-container="body" data-html="true" data-trigger="focus" data-toggle="popover" data-placement="bottom" data-content="{{ $apps }}">
           <i class="material-icons md-48">apps</i>
         </a>
       </li>
    </ul>
   </div>
+  <script>
+    $('[data-toggle="popover"]').popover();
+  </script>
 </nav>
 
 
