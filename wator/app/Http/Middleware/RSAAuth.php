cef71d6038a72f70a59bc7eb581a65fd417d97a8
Wator Vapor <watorvapor@gmail.com>
2017-09-09 00:32:14 +0900

    home

---
+++ wator/app/Http/Middleware/RSAAuth.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Wator\Http\Middleware;
+
+use Closure;
+
+class RSAAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request);
+    }
+}
