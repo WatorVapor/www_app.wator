1462afc2802017c0738b5304b58b3c54b2bb171b
Wator Vapor <watorvapor@gmail.com>
2017-09-09 00:59:42 +0900

    home

---
+++ www_root/wator/js/lang.js
@@ -0,0 +1,51 @@
+$(function () {
+  $('.language_button').click(function(){
+    let lang = this.value;
+    console.log('lang=<' + lang + '>');
+    if(lang) {
+      localStorage.setItem('operation.lang',lang);
+      updateLanguage();
+    }
+  });
+});
+
+function onClickLanguage(lang) {
+  console.log('lang=<' + lang + '>');
+  if(lang) {
+    localStorage.setItem('operation.lang',lang);
+    updateLanguage();
+  }
+}
+function updateLanguage() {
+  var lang = localStorage.getItem('operation.lang')
+  if(lang && typeof lang === 'string') {
+    var url = '/account/language';
+    var JSONdata ={lang:lang};
+    $.ajax({
+      type : 'post',
+      url : url,
+      data : JSON.stringify(JSONdata),
+      contentType: 'application/JSON',
+      dataType : 'JSON',
+      scriptCharset: 'utf-8',
+      success : function(data) {
+        // Success
+        console.log(data);
+        sessionStorage.setItem('operation.lang.run',data);
+        location.reload(true);
+      },
+      error : function(data) {
+        // Error
+        console.log(data);
+      }
+    });
+  }
+}
+
+/*
+check language at boot.
+*/
+var run = sessionStorage.getItem('operation.lang.run')
+if(! run) {
+  updateLanguage();
+}
---
+++ www_root/wator/js/login.js
@@ -0,0 +1,135 @@
+var RSAAuth = RSAAuth || {};
+
+RSAAuth.getPubKey = function() {
+  return localStorage.getItem('auth.rsa.key.public');
+}
+RSAAuth.getPriKey = function() {
+  return localStorage.getItem('auth.rsa.key.private');
+}
+RSAAuth.getToken = function() {
+  return localStorage.getItem('auth.rsa.token');
+}
+
+
+RSAAuth.isLoginRun_ = function() {
+  let run = sessionStorage.getItem('auth.rsa.run');
+  console.log('run=<',run,'>');
+  if (run) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+RSAAuth.getAccess_ = function() {
+  let access = sessionStorage.getItem('auth.rsa.access') || RSAAuth.uniAccess_();
+  return access;
+}
+
+RSAAuth.reLogin_ = function() {
+  RSAAuth.login();
+}
+
+RSAAuth.uniAccess_ = function() {
+  let JSONdata ={};
+  let url = '/account/access';
+  $.ajax({
+    type : 'post',
+    url : url,
+    success : function(data) {
+      // Success
+      console.log(data);
+      //window.location.href = '/';
+      sessionStorage.setItem('auth.rsa.access',data);
+      // reload in
+      setTimeout(RSAAuth.reLogin_,0);
+    },
+    error : function(e) {
+      console.error(e);
+      window.location.href = '/account/error';
+    }
+  });
+}
+
+RSAAuth.signLogin_ = function(privateKey,token,access) {
+  let rsaKey = KEYUTIL.getKeyFromPlainPrivatePKCS8PEM(privateKey);
+  //console.log(rsaKey);
+  let signature = rsaKey.signString(access,"sha256");
+  //console.log(signature);
+  let JSONdata ={};
+  JSONdata.token = token;
+  JSONdata.auth = {};
+  JSONdata.auth.access = access;
+  JSONdata.auth.sign = signature;
+  let url = '/account/login';
+  $.ajax({
+    type : 'post',
+    url : url,
+    data : JSON.stringify(JSONdata),
+    contentType: 'application/JSON',
+    dataType : 'JSON',
+    scriptCharset: 'utf-8',
+    success : function(data) {
+      // Success
+      console.log(data);
+      sessionStorage.setItem('auth.rsa.run',JSON.stringify(data));
+      window.location.href = window.location.href;
+    },
+    error : function(data) {
+      // Error
+      console.log(data);
+      sessionStorage.setItem('auth.rsa.run',JSON.stringify(data));
+      window.location.href = '/account/error';
+    }
+  });
+}
+
+RSAAuth.clear = function() {
+  let JSONdata ={};
+  let url = '/account/login';
+  $.ajax({
+    type : 'post',
+    url : url,
+    data : JSON.stringify(JSONdata),
+    contentType: 'application/JSON',
+    dataType : 'JSON',
+    scriptCharset: 'utf-8',
+    success : function(data) {
+      // Success
+      console.log(data);
+      sessionStorage.setItem('auth.rsa.run',JSON.stringify(data));
+      //window.location.href = '/';
+    },
+    error : function(data) {
+      // Error
+      console.log(data);
+      sessionStorage.setItem('auth.rsa.run',JSON.stringify(data));
+      window.location.href = '/account/error';
+    }
+  });
+}
+
+RSAAuth.login = function() {
+  let privateKey = RSAAuth.getPriKey();
+  let token = RSAAuth.getToken();
+  let access = RSAAuth.getAccess_();
+  if(!access) {
+    console.warn('No access Key create it');
+    return
+  }
+  //console.log(privateKey);
+  if (privateKey && 'string'== typeof privateKey && token && 'string'== typeof token ) {
+    RSAAuth.signLogin_(privateKey,token,access);
+  } else {
+    RSAAuth.clear();
+  }
+}
+
+$(document).ready(function(){
+  let run = RSAAuth.isLoginRun_();
+  console.log(run);
+  if(!run) {
+    RSAAuth.login();
+  }
+});
