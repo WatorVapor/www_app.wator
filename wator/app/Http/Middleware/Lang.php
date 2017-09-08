768ee3347e1b75367aaf1a7a64c329e184f1abc4
Wator Vapor <watorvapor@gmail.com>
2017-09-09 00:45:22 +0900

    Update Lang.php

--- wator/app/Http/Middleware/Lang.php
+++ wator/app/Http/Middleware/Lang.php
@@ -16,7 +16,7 @@ class Lang
     public function handle($request, Closure $next)
     {
         $lang = $request->session()->get('user.operation.lang');
-        if (isset($lang) {
+        if (isset($lang)) {
             app()->setLocale($lang);
         }
         return $next($request);
