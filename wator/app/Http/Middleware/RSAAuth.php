529f6b6dc97a535a0c2a094e4fe25e930acfd189
Wator Vapor <watorvapor@gmail.com>
2017-09-09 00:41:50 +0900

    Update RSAAuth.php

--- wator/app/Http/Middleware/RSAAuth.php
+++ wator/app/Http/Middleware/RSAAuth.php
@@ -15,6 +15,21 @@ class RSAAuth
      */
     public function handle($request, Closure $next)
     {
+        $status = $request->session()->get('account.rsa.login.status');
+        if (isset($status) {
+            if ( $status === 'success') {
+                $name = $request->session()->get('account.rsa.login.name');
+                if (isset($name) {
+                    view()->share('nav_login_show_name', $name);
+                } else {
+                    view()->share('nav_login_show_name', 'navbar.profile');
+                }
+                view()->share('nav_login_url', '/account/profile');
+            } else {
+                view()->share('nav_login_show_name', 'navbar.fix');
+                view()->share('nav_login_url', '/account/fix');
+            }
+        }
         return $next($request);
     }
 }
