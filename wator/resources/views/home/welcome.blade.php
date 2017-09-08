71ae05dbfee87863817e2175a22a1db2151b9b2d
Wator Vapor <watorvapor@gmail.com>
2017-09-09 00:17:55 +0900

    Update welcome.blade.php

--- wator/resources/views/home/welcome.blade.php
+++ wator/resources/views/home/welcome.blade.php
@@ -7,10 +7,5 @@
 
 @section('content')
 <div class="row" >
-  <div class="col"></div>
-  <div class="col-10 bg-danger text-nowrap">
-      {{trans('about.content')}}
-  </div>
-  <div class="col"></div>
 </div>
 @endsection
