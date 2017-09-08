4a519f7c1be1bc81ce4f0389a1de5e5265235abe
Wator Vapor <watorvapor@gmail.com>
2017-09-09 00:15:14 +0900

    Update about.blade.php

--- wator/resources/views/home/about.blade.php
+++ wator/resources/views/home/about.blade.php
@@ -10,7 +10,7 @@
 <div class="row" >
   <div class="col"></div>
   <div class="col-10 bg-secondary text-left text-justify">
-    <p>{{trans('about.content')}}</p>
+    {{trans('about.content')}}
   </div>
   <div class="col"></div>
 </div>
