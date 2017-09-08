044013111170047e6831def8cf47a411b2b8cb77
Wator Vapor <watorvapor@gmail.com>
2017-09-08 23:09:12 +0900

    Update navbar_r.blade.php

--- wator/resources/views/wator/navbar_r.blade.php
+++ wator/resources/views/wator/navbar_r.blade.php
@@ -1,35 +1,35 @@
 <?php
   $langs = '';
-  $langs .= '<a class="btn btn-success" onclick="onClickLanguage(\'zh\')" role="button">';
+  $langs .= '<a class="btn btn-success mr-lg-5" onclick="onClickLanguage(\'zh\')" role="button">';
   $langs .= '<span class="flag-icon flag-icon-background flag-icon-cn"></span>';
   $langs .= '</a>';
-  $langs .= '<a class="btn btn-success" onclick="onClickLanguage(\'ja\')" role="button">';
+  $langs .= '<a class="btn btn-success mr-lg-5" onclick="onClickLanguage(\'ja\')" role="button">';
   $langs .= '<span class="flag-icon flag-icon-background flag-icon-jp"></span>';
   $langs .= '</a>';
   $langs .= '<br/>';
-  $langs .= '<a class="btn btn-success" onclick="onClickLanguage(\'en\')" role="button">';
+  $langs .= '<a class="btn btn-success mr-lg-5" onclick="onClickLanguage(\'en\')" role="button">';
   $langs .= '<span class="flag-icon flag-icon-background flag-icon-us"></span>';
   $langs .= '</a>';
-  $langs .= '<a class="btn btn-success" onclick="onClickLanguage(\'ru\')" role="button">';
+  $langs .= '<a class="btn btn-success mr-lg-5" onclick="onClickLanguage(\'ru\')" role="button">';
   $langs .= '<span class="flag-icon flag-icon-background flag-icon-ru"></span>';
   $langs .= '</a>';
 ?>
 <?php
   $apps = '';
-  $apps .= '<a class="btn btn-lg btn-success" href="/xuniverse" role="button">';
+  $apps .= '<a class="btn btn-lg btn-success mr-lg-5" href="/xuniverse" role="button">';
   $apps .= '<i class="material-icons md-48">filter_vintage</i>';
   $apps .= '</a>';
-  $apps .= '<a class="btn btn-lg btn-success" href="/p2pio" role="button">';
+  $apps .= '<a class="btn btn-lg btn-success mr-lg-5" href="/p2pio" role="button">';
   $apps .= '<i class="material-icons md-48">device_hub</i>';
   $apps .= '</a>';
-  $apps .= '<a class="btn btn-lg btn-success" href="/story" role="button">';
+  $apps .= '<a class="btn btn-lg btn-success mr-lg-5" href="/story" role="button">';
   $apps .= '<i class="material-icons md-48">book</i>';
   $apps .= '</a>';
   $apps .= '<hr/>';
-  $apps .= '<a class="btn btn-lg btn-success" href="/wai" role="button">';
+  $apps .= '<a class="btn btn-lg btn-success mr-lg-5" href="/wai" role="button">';
   $apps .= '<i class="material-icons md-48">mic</i>';
   $apps .= '</a>';
-  $apps .= '<a class="btn btn-lg btn-success" href="/scope" role="button">';
+  $apps .= '<a class="btn btn-lg btn-success mr-lg-5" href="/scope" role="button">';
   $apps .= '<i class="material-icons md-48">photo</i>';
   $apps .= '</a>';
 ?>
