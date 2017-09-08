2288639698c3a8e77e00c2c29b5f6bf36be29348
Wator Vapor <watorvapor@gmail.com>
2017-09-09 00:13:42 +0900

    Update about.blade.php

--- wator/resources/views/home/about.blade.php
+++ wator/resources/views/home/about.blade.php
@@ -9,7 +9,7 @@
 @section('content')
 <div class="row" >
   <div class="col"></div>
-  <div class="col-10 bg-secondary text-left text-nowrap">
+  <div class="col-10 bg-secondary text-left text-justify">
     <pre>{{trans('about.content')}}</pre>
   </div>
   <div class="col"></div>
