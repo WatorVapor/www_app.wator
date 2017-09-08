3beb24790ba120097c7b8a40e39adb63ee9521d2
Wator Vapor <watorvapor@gmail.com>
2017-09-08 23:45:02 +0900

    Update about.blade.php

--- wator/resources/views/home/about.blade.php
+++ wator/resources/views/home/about.blade.php
@@ -7,15 +7,13 @@
 
 
 @section('content')
-<div class="container-fluid">
-  <div class="row" >
-    <div class="col-md-10 col-md-offset-1">
-    <div class="col-text text-left">
-      <pre class="bg-danger">
-       {{trans('about.content')}}
-      </pre>
-    </div>
-    </div>
+<div class="row" >
+  <div class="col-md-10 col-md-offset-1">
+  <div class="col-text text-left">
+    <pre class="bg-danger">
+     {{trans('about.content')}}
+    </pre>
+  </div>
   </div>
 </div>
 @endsection
