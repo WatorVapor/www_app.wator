cdc78dc6323060504a6c9d43802cf26ed907d6a0
Wator Vapor <watorvapor@gmail.com>
2017-09-09 18:58:13 +0900

    rsa

---
+++ wator/resources/lang/ru/rsaauth/signup.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+    'import' => 'Импорт ключа  от  счета  уже существует ',
+    'agree_robot' => 'Ты робот  или  искусственный интеллект  ? ',
+    'agree_friend' => 'AТы их  друзей  ? ',
+    'agree_yes' => 'Хорошо',
+    'agree_no' => 'не',
+    'key_waiting' => 'штриховка,  ключ  ,  пожалуйста, подождите  ... ',
+    'key_important' => '
+тот ключ  будет  очень  важным, если  потери  не могут войти в  этот сайт  . 
+этот ключ  хранится в  браузер  только  местные  Временного регионального  ,  убирать  страницы  резюме  таких актов  будут утеряны  . 
+Пожалуйста,  загрузить  файл  Сохранить  как  ,  надлежащим образом  . 
+',
+    'key_save_file' => 'сохранить в  файл ',
+    'key_copy' => 'Копировать в буфер обмена',
+    'key_send_mail' => 'отправить сообщение',
+    'key_next' => 'Следующий шаг',
+];
