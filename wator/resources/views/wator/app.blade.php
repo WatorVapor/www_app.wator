ac61c94dd7cbc573391efd90d6f435d89b304625
Wator Vapor <watorvapor@gmail.com>
2017-09-10 08:05:31 +0900

    Update app.blade.php

--- wator/resources/views/wator/app.blade.php
+++ wator/resources/views/wator/app.blade.php
@@ -19,8 +19,6 @@
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-    <script src="/assets/js/app.js" type="text/javascript"></script>
     <script src="/wator/js/clipboard.min.js" type="text/javascript"></script>
     <script src="/wator/js/ga.js" type="text/javascript"></script>
     <script src="/wator/js/login.js" type="text/javascript"></script>
@@ -39,11 +37,5 @@
     <div class="container-fluid">
       @yield('content')
     </div>
-    
-    $.ajaxSetup({
-      headers: {
-          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-      }
-    });
   </body>
 </html>
