60572bf3e184deaac258baf0a6916c6fa8b0032d
Wator Vapor <watorvapor@gmail.com>
2017-09-09 17:15:12 +0900

    Update web.php

--- wator/routes/web.php
+++ wator/routes/web.php
@@ -14,3 +14,22 @@
 Route::get('/about', 'Home\AboutController@index');
 Route::get('/welcome', 'Home\WelcomeController@index');
 Route::get('/', 'Home\WelcomeController@index');
+
+
+Route::get('/rsaauth/signup', 'RsaAuth\SignUpController@index');
+Route::post('/rsaauth/signup', 'RsaAuth\SignUpController@store');
+Route::get('/rsaauth/import', 'RsaAuth\ImportController@index');
+Route::post('/rsaauth/import', 'RsaAuth\ImportController@store');
+Route::get('/rsaauth/profile', 'RsaAuth\ProfileController@index');
+Route::post('/rsaauth/profile', 'RsaAuth\ProfileController@store');
+Route::get('/rsaauth/login', 'RsaAuth\LoginController@index');
+Route::post('/rsaauth/login', 'RsaAuth\LoginController@store');
+Route::get('/rsaauth/fix', 'RsaAuth\FixController@index');
+Route::post('/rsaauth/fix', 'RsaAuth\FixController@store');
+Route::get('/rsaauth/access', 'RsaAuth\AccessController@index');
+Route::post('/rsaauth/access', 'RsaAuth\AccessController@store');
+Route::get('/rsaauth/language', 'RsaAuth\LanguageController@index');
+Route::post('/rsaauth/language', 'RsaAuth\LanguageController@store');
+Route::get('/rsaauth/debug', 'RsaAuth\DebugController@index');
+Route::get('/rsaauth/error', 'RsaAuth\DebugController@index');
+Route::get('/rsaauth', 'RsaAuth\LoginController@index');
