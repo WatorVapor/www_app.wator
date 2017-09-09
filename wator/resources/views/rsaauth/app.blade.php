7bdc31959768281a9e9e520b19075d2f7fd01546
Wator Vapor <watorvapor@gmail.com>
2017-09-10 07:56:46 +0900

    Update app.blade.php

--- wator/resources/views/rsaauth/app.blade.php
+++ wator/resources/views/rsaauth/app.blade.php
@@ -20,6 +20,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jsrsasign/8.0.3/jsrsasign-all-min.js" type="text/javascript"></script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
     <script src="/assets/js/app.js" type="text/javascript"></script>
     <script src="/wator/js/clipboard.min.js" type="text/javascript"></script>
     <script src="/wator/js/ga.js" type="text/javascript"></script>
