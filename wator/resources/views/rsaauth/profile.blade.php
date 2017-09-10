ded0cf4d95ba781a06ce269591b08306f6bf9156
Wator Vapor <watorvapor@gmail.com>
2017-09-10 09:20:50 +0900

    Update profile.blade.php

--- wator/resources/views/rsaauth/profile.blade.php
+++ wator/resources/views/rsaauth/profile.blade.php
@@ -7,47 +7,23 @@
 
 @section('content')
 
-<script src="/wator/rsaauth/js/profile.js" type="text/javascript"></script>
-<script>
-  $(document).ready(function() {
-    $('#apply-name').click(function(){
-      var name= $('#user-name').val();
-      console.log(name);
-      RSAAuth.profile({'user':name});
-    });
-  });
-</script>
-
 <div class="row justify-content-md-center">
   <div class="col-lg-8">
     <div class="card mt-5">
-      <div class="card-header">
-        {{trans('profile.title')}}
-      </div>
+      <div class="card-header">{{trans('profile.title')}}</div>
       <div class="card-body">
-        <form  class="mt-2 mb-2">
+        <form  class="mt-2 mb-2" method="POST" action="/rsaauth/profile">
+          {{ csrf_field() }}
           <div class="input-group">
              <span class="input-group-btn">
                <button id="apply-name" type="button" class="btn btn-default">{{trans('profile.apply_name')}}</button>
              </span>
-             <input type="text" id = "user-name" class="form-control" placeholder="{{ $user_name }}" aria-describedby="basic-addon1">
+             <input type="text" name="user-name" class="form-control" placeholder="{{ $user_name }}" aria-describedby="basic-addon1">
           </div>
         </form>
       </div>
     </div>
   </div>
-<!--
-  <div class="col-md-6 bg-primary mt-5">
-    <form  class="mt-2 mb-2">
-      <div class="input-group">
-         <span class="input-group-btn">
-           <button id="apply-name" type="button" class="btn btn-default">{{trans('profile.apply_name')}}</button>
-         </span>
-         <input type="text" id = "user-name" class="form-control" placeholder="{{ $user_name }}" aria-describedby="basic-addon1">
-      </div>
-    </form>
-  </div>
--->
 </div>
 
 
