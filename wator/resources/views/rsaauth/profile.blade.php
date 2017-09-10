f67b09a3360cb3d541db85ee8a19de4c1978a7ea
Wator Vapor <watorvapor@gmail.com>
2017-09-10 09:08:12 +0900

    Update profile.blade.php

--- wator/resources/views/rsaauth/profile.blade.php
+++ wator/resources/views/rsaauth/profile.blade.php
@@ -20,14 +20,18 @@
 
 <div class="row justify-content-md-center">
   <div class="card mt-5" style="width: 20rem;">
-    <img class="card-img-top" src="..." alt="Card image cap">
     <div class="card-body">
-      <h4 class="card-title">Card title</h4>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <form  class="mt-2 mb-2">
+        <div class="input-group">
+           <span class="input-group-btn">
+             <button id="apply-name" type="button" class="btn btn-default">{{trans('profile.apply_name')}}</button>
+           </span>
+           <input type="text" id = "user-name" class="form-control" placeholder="{{ $user_name }}" aria-describedby="basic-addon1">
+        </div>
+      </form>
     </div>
   </div>
-
+<!--
   <div class="col-md-6 bg-primary mt-5">
     <form  class="mt-2 mb-2">
       <div class="input-group">
@@ -38,6 +42,7 @@
       </div>
     </form>
   </div>
+-->
 </div>
 
 
