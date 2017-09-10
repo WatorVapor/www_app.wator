83f1a6f0fac0fb4b9bf88663a9661fcb62e49d55
Wator Vapor <watorvapor@gmail.com>
2017-09-10 09:52:30 +0900

    Update LoginController.php

--- wator/app/Http/Controllers/RsaAuth/LoginController.php
+++ wator/app/Http/Controllers/RsaAuth/LoginController.php
@@ -18,7 +18,7 @@ class LoginController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         //
         return view('rsaauth.login');
