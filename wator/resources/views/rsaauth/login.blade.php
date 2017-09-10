9b6a3c95c6778032f41221044cce5bbbf51d96fe
Wator Vapor <watorvapor@gmail.com>
2017-09-10 09:59:34 +0900

    Update login.blade.php

--- wator/resources/views/rsaauth/login.blade.php
+++ wator/resources/views/rsaauth/login.blade.php
@@ -1,4 +1,4 @@
-@extends('wator.app')
+@extends('rsaauth.app')
 
 @section('appnavbar')
   @include('rsaauth.navbar')
@@ -6,6 +6,25 @@
 
 
 @section('content')
-<div class="row" >
+
+<div class="row justify-content-md-center">
+  <div class="col-lg-8">
+    <div class="card mt-5">
+      <div class="card-header">{{trans('login.title')}}</div>
+      <div class="card-body">
+        <form  class="mt-2 mb-2" method="POST" action="/rsaauth/login">
+          {{ csrf_field() }}
+          <div class="input-group">
+             <span class="input-group-btn">
+               <button type="submit" class="btn btn-default">{{trans('profile.login')}}</button>
+             </span>
+             <input type="text" name="accessToken" class="form-control" placeholder="" aria-describedby="basic-addon1">
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </div>
+
+
 @endsection
