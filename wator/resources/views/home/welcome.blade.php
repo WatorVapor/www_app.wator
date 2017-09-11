bd8de9a0a51349d054c3ebc656ae79c79e35c68a
Wator Vapor <watorvapor@gmail.com>
2017-09-11 21:38:56 +0900

    Update welcome.blade.php

--- wator/resources/views/home/welcome.blade.php
+++ wator/resources/views/home/welcome.blade.php
@@ -17,13 +17,10 @@
       </ol>
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img class="d-block w-100" src="..." alt="First slide">
         </div>
         <div class="carousel-item">
-          <img class="d-block w-100" src="..." alt="Second slide">
         </div>
         <div class="carousel-item">
-          <img class="d-block w-100" src="..." alt="Third slide">
         </div>
       </div>
       <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
