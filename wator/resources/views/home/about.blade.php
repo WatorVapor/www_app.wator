be81ea304eb6eb6d5ca19a8e1ae2e58181ef3fbb
Wator Vapor <watorvapor@gmail.com>
2017-09-11 21:34:03 +0900

    Update about.blade.php

--- wator/resources/views/home/about.blade.php
+++ wator/resources/views/home/about.blade.php
@@ -8,10 +8,8 @@
 
 @section('content')
 <div class="row bg-secondary" >
-  <div class="col"></div>
   <div class="col-10 text-left text-nowrap">
     <pre>{{trans('about.content')}}</pre>
   </div>
-  <div class="col"></div>
 </div>
 @endsection
