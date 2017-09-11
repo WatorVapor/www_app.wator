f77248388871d4597bcc49108f7d98b996788422
Wator Vapor <watorvapor@gmail.com>
2017-09-11 22:07:41 +0900

    Update welcome.blade.php

--- wator/resources/views/home/welcome.blade.php
+++ wator/resources/views/home/welcome.blade.php
@@ -17,7 +17,7 @@
 @section('content')
 <div class="row justify-content-center" >
   <div class="col-10">
-    <div id="carouselExampleIndicators" class="carousel slide mt-5" data-ride="carousel">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
       <ol class="carousel-indicators">
         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
