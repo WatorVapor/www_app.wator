df63623d5e894bf75ca1176b763b947a4c6ea730
Wator Vapor <watorvapor@gmail.com>
2017-09-11 21:59:48 +0900

    Update welcome.blade.php

--- wator/resources/views/home/welcome.blade.php
+++ wator/resources/views/home/welcome.blade.php
@@ -25,16 +25,19 @@
       </ol>
       <div class="carousel-inner">
         <div class="carousel-item active">
+          <img src="https://www.wator.xyz/img/brain.orig.png" alt="{{trans('welcome.ThirtyYearBrainResearch')}}">
           <div class="carousel-caption d-none d-md-block">
             <h3>{{trans('welcome.ThirtyYearBrainResearch')}}</h3>
           </div>
         </div>
         <div class="carousel-item">
+          <img src="https://www.wator.xyz/img/brain.orig.png" alt="{{trans('welcome.ThirtyYearBrainResearch')}}">
           <div class="carousel-caption d-none d-md-block">
             <h3>{{trans('welcome.ThirtyYearStrongAI')}}</h3>
           </div>
         </div>
         <div class="carousel-item">
+          <img src="https://www.wator.xyz/img/brain.orig.png" alt="{{trans('welcome.ThirtyYearBrainResearch')}}">
           <div class="carousel-caption d-none d-md-block">
             <h3>{{trans('welcome.FortyYearBrainRead')}}</h3>
           </div>
