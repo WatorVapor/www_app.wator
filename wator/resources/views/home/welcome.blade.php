ec28ff4c0db8df8ac60b0370e7ba6b86de3d2656
Wator Vapor <watorvapor@gmail.com>
2017-09-11 21:57:12 +0900

    Update welcome.blade.php

--- wator/resources/views/home/welcome.blade.php
+++ wator/resources/views/home/welcome.blade.php
@@ -25,10 +25,19 @@
       </ol>
       <div class="carousel-inner">
         <div class="carousel-item active">
+          <div class="carousel-caption d-none d-md-block">
+            <h3>{{trans('welcome.ThirtyYearBrainResearch')}}</h3>
+          </div>
         </div>
         <div class="carousel-item">
+          <div class="carousel-caption d-none d-md-block">
+            <h3>{{trans('welcome.ThirtyYearStrongAI')}}</h3>
+          </div>
         </div>
         <div class="carousel-item">
+          <div class="carousel-caption d-none d-md-block">
+            <h3>{{trans('welcome.FortyYearBrainRead')}}</h3>
+          </div>
         </div>
       </div>
       <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
