49bc84175f4a9b97c5e5bc4b417b0385cc2d1023
Wator Vapor <watorvapor@gmail.com>
2017-09-11 20:51:22 +0900

    Update profile.blade.php

--- wator/resources/views/rsaauth/profile.blade.php
+++ wator/resources/views/rsaauth/profile.blade.php
@@ -10,13 +10,13 @@
 <div class="row justify-content-md-center">
   <div class="col-lg-8">
     <div class="card mt-5">
-      <div class="card-header">{{trans('profile.title')}}</div>
+      <div class="card-header">{{trans('rsaauth_profile.title')}}</div>
       <div class="card-body">
         <form  class="mt-2 mb-2" method="POST" action="/rsaauth/profile">
           {{ csrf_field() }}
           <div class="input-group">
              <span class="input-group-btn">
-               <button type="submit" class="btn btn-default">{{trans('profile.apply_name')}}</button>
+               <button type="submit" class="btn btn-default">{{trans('rsaauth_profile.apply_name')}}</button>
              </span>
              <input type="text" name="user-name" class="form-control" placeholder="{{ $user_name }}" aria-describedby="basic-addon1">
           </div>
