f4963f0f8513eec9aebe5497284d7255c8043012
Wator Vapor <watorvapor@gmail.com>
2017-09-11 22:37:23 +0900

    Update welcome.blade.php

--- wator/resources/views/home/welcome.blade.php
+++ wator/resources/views/home/welcome.blade.php
@@ -29,62 +29,62 @@
       </ol>
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img src="/wator/images/brain.orig.png" alt="{{trans('welcome.ThirtyYearBrainResearch')}}">
-          <img src="/wator/images/brain.header.jpg" alt="{{trans('welcome.ThirtyYearBrainResearch')}}">
-          <img src="/wator/images/digital.brain.reading.jpg" alt="{{trans('welcome.ThirtyYearBrainResearch')}}">
-          <img src="/wator/images//brain.skelton.jpg" alt="{{trans('welcome.ThirtyYearBrainResearch')}}">
+          <img src="/wator/images/brain.orig.png" alt="">
+          <img src="/wator/images/brain.header.jpg" alt="">
+          <img src="/wator/images/digital.brain.reading.jpg" alt="">
+          <img src="/wator/images//brain.skelton.jpg" alt="">
           <div class="carousel-caption d-none d-md-block">
             <h3 class="text-success">{{trans('welcome.ThirtyYearBrainResearch')}}</h3>
           </div>
         </div>
         <div class="carousel-item">
-          <img src="/wator/images/ai.nl.jpg" alt="{{trans('welcome.ThirtyYearStrongAI')}}">
-          <img src="/wator/images/ai.dnn.jpg" alt="{{trans('welcome.ThirtyYearStrongAI')}}">
-          <img src="/wator/images/ai.dream.jpg" alt="{{trans('welcome.ThirtyYearStrongAI')}}">
-          <img src="/wator/images/i.am.robot.png" alt="{{trans('welcome.ThirtyYearStrongAI')}}">
+          <img src="/wator/images/ai.nl.jpg" alt="">
+          <img src="/wator/images/ai.dnn.jpg" alt="">
+          <img src="/wator/images/ai.dream.jpg" alt="">
+          <img src="/wator/images/i.am.robot.png" alt="">
           <div class="carousel-caption d-none d-md-block">
             <h3 class="text-success">{{trans('welcome.ThirtyYearStrongAI')}}</h3>
           </div>
         </div>
         <div class="carousel-item">
-          <img src="/wator/images/digital.Consciousness.reading.jpg" alt="{{trans('welcome.FortyYearBrainRead')}}">
-          <img src="/wator/images/digital.brain.block.jpg" alt="{{trans('welcome.FortyYearBrainRead')}}">
-          <img src="/wator/images/digital.soul.jpg" alt="{{trans('welcome.FortyYearBrainRead')}}">
-          <img src="/wator/images/digital.Consciousness.binary.jpg" alt="{{trans('welcome.FortyYearBrainRead')}}">
-          <img src="/wator/images/digital.brain.block.3d.jpg" alt="{{trans('welcome.FortyYearBrainRead')}}">
+          <img src="/wator/images/digital.Consciousness.reading.jpg" alt="">
+          <img src="/wator/images/digital.brain.block.jpg" alt="">
+          <img src="/wator/images/digital.soul.jpg" alt="">
+          <img src="/wator/images/digital.Consciousness.binary.jpg" alt="">
+          <img src="/wator/images/digital.brain.block.3d.jpg" alt="">
           <div class="carousel-caption d-none d-md-block">
             <h3 class="text-success">{{trans('welcome.FortyYearBrainRead')}}</h3>
           </div>
         </div>
         <div class="carousel-item">
-          <img src="/wator/images/human.robot.jpg" alt="{{trans('welcome.FortyYearBrain2Robot')}}">
-          <img src="/wator/images/robot.brain.why.jpg" alt="{{trans('welcome.FortyYearBrain2Robot')}}">
-          <img src="/wator/images/robot.brain.white.jpg" alt="{{trans('welcome.FortyYearBrain2Robot')}}">
-          <img src="/wator/images/robot.thinking.pose.jpg" alt="{{trans('welcome.FortyYearBrain2Robot')}}">
+          <img src="/wator/images/human.robot.jpg" alt="">
+          <img src="/wator/images/robot.brain.why.jpg" alt="">
+          <img src="/wator/images/robot.brain.white.jpg" alt="">
+          <img src="/wator/images/robot.thinking.pose.jpg" alt="">
           <div class="carousel-caption d-none d-md-block">
             <h3 class="text-success">{{trans('welcome.FortyYearBrain2Robot')}}</h3>
           </div>
         </div>
         <div class="carousel-item">
-          <img src="/wator/images/robot.rocket.block.jpg" alt="{{trans('welcome.FiftyYearRobot2Universe')}}">
-          <img src="/wator/images/robot.star.space.jpg" alt="{{trans('welcome.FiftyYearRobot2Universe')}}">
-          <img src="/wator/images/space.travel.jpg" alt="{{trans('welcome.FiftyYearRobot2Universe')}}">
-          <img src="/wator/images/space.travel.land.jpg" alt="{{trans('welcome.FiftyYearRobot2Universe')}}">
+          <img src="/wator/images/robot.rocket.block.jpg" alt="">
+          <img src="/wator/images/robot.star.space.jpg" alt="">
+          <img src="/wator/images/space.travel.jpg" alt="">
+          <img src="/wator/images/space.travel.land.jpg" alt="">
           <div class="carousel-caption d-none d-md-block">
             <h3 class="text-success">{{trans('welcome.FiftyYearRobot2Universe')}}</h3>
           </div>
         </div>
         <div class="carousel-item">
-          <img src="/wator/images/earth.kepler-20.jpg" alt="{{trans('welcome.HundredYearDason')}}">
-          <img src="/wator/images/earth.kepler-20.glaxy.jpg" alt="{{trans('welcome.HundredYearDason')}}">
-          <img src="/wator/images/kepler.daison.jpg" alt="{{trans('welcome.HundredYearDason')}}">
-          <img src="/wator/images/kepler.daison.black.jpg" alt="{{trans('welcome.HundredYearDason')}}">
+          <img src="/wator/images/earth.kepler-20.jpg" alt="">
+          <img src="/wator/images/earth.kepler-20.glaxy.jpg" alt="">
+          <img src="/wator/images/kepler.daison.jpg" alt="">
+          <img src="/wator/images/kepler.daison.black.jpg" alt="">
           <div class="carousel-caption d-none d-md-block">
             <h3 class="text-success">{{trans('welcome.HundredYearDason')}}</h3>
           </div>
         </div>
         <div class="carousel-item">
-          <img src="/wator/images/human.robot.jpg" alt="{{trans('welcome.TwoHundredYearHuman')}}">
+          <img src="/wator/images/human.robot.jpg" alt="">
           <div class="carousel-caption d-none d-md-block">
             <h3 class="text-success">{{trans('welcome.TwoHundredYearHuman')}}</h3>
           </div>
