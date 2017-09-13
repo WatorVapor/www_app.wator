f1ff891e9fc18dd7627227b3bb9990680f87ed64
Wator Vapor <watorvapor@gmail.com>
2017-09-13 21:02:00 +0900

    Create navbar.blade.php

---
+++ wator/resources/views/wai/navbar.blade.php
@@ -0,0 +1,18 @@
+<a class="btn btn-lg btn-danger wator-navbar-btn-my-first" href="/wai" role="button">
+  <i class="material-icons md-48">help_outline</i>
+</a>
+<a class="btn btn-lg btn-danger " href="/wai/text/participle" role="button">
+  <i class="material-icons md-48">text_fields</i>
+</a>
+
+<a class="btn btn-lg btn-danger " href="/wai/text/train/crawler/summary" role="button">
+  <i class="material-icons md-48">info</i>
+</a>
+
+<a class="btn btn-lg btn-danger " href="/wai/text/train/ostrich/summary" role="button">
+  <i class="material-icons md-48">info</i>
+</a>
+
+<a class="btn btn-lg btn-danger " href="/wai/text/train/parrot/summary" role="button">
+  <i class="material-icons md-48">info</i>
+</a>
