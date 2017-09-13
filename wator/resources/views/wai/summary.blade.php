7d8133f2b7eec28c0399d1c0a96c88f6064fe6ea
Wator Vapor <watorvapor@gmail.com>
2017-09-13 21:56:29 +0900

    Update summary.blade.php

--- wator/resources/views/wai/summary.blade.php
+++ wator/resources/views/wai/summary.blade.php
@@ -3,8 +3,8 @@
   @include('wai.navbar')
 @endsection
 @section('content')
-<div class="row">
-  <div class="col-lg-10 col-lg-offset-1">
+<div class="row justify-content-center">
+  <div class="col-lg-10">
     <div class="panel panel-default">
       <div class="panel-body">
         <pre style="white-space: pre-wrap ;">{{ $summary }}</pre>
