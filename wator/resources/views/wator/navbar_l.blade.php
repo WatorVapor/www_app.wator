d3c83802bc686bbee3cc29c60a8596e64d4c1eb6
Wator Vapor <watorvapor@gmail.com>
2017-09-14 22:15:19 +0900

    Update navbar_l.blade.php

--- wator/resources/views/wator/navbar_l.blade.php
+++ wator/resources/views/wator/navbar_l.blade.php
@@ -1,14 +1,12 @@
-<div class="collapse navbar-collapse" id="navbarNav">
-  <ul class="navbar-nav justify-content-start">
-    <li class="nav-item active">
-      <a class="btn btn-lg btn-primary nav-btn" href="/" role="button">
-        <i class="material-icons md-48">home</i>
-      </a>
-    </li>
-    <li class="nav-item active ml-lg-3">
-      <a class="btn btn-lg btn-primary nav-btn" href="/about" role="button">
-        <i class="material-icons md-48">info</i>
-      </a>
-    </li>
-  </ul>
-</div>
+<ul class="navbar-nav nav justify-content-start">
+  <li class="nav-item active">
+    <a class="btn btn-lg btn-primary nav-btn" href="/" role="button">
+      <i class="material-icons md-48">home</i>
+    </a>
+  </li>
+  <li class="nav-item active ml-lg-3">
+    <a class="btn btn-lg btn-primary nav-btn" href="/about" role="button">
+      <i class="material-icons md-48">info</i>
+    </a>
+  </li>
+</ul>
