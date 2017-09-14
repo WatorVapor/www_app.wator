dd554188816a45d374780d5c08ce9713173df6b6
Wator Vapor <watorvapor@gmail.com>
2017-09-14 22:46:20 +0900

    Update navbar.blade.php

--- wator/resources/views/wai/navbar.blade.php
+++ wator/resources/views/wai/navbar.blade.php
@@ -1,18 +1,31 @@
-<a class="btn btn-lg btn-danger wator-navbar-btn-my-first" href="/wai" role="button">
-  <i class="material-icons md-48">help_outline</i>
-</a>
-<a class="btn btn-lg btn-danger " href="/wai/text/participle" role="button">
-  <i class="material-icons md-48">text_fields</i>
-</a>
+<ul class="navbar-nav nav justify-content-start">
 
-<a class="btn btn-lg btn-danger " href="/wai/text/train/crawler/summary" role="button">
-  <i class="material-icons md-48">info</i>
-</a>
+  <li class="nav-item active">
+    <a class="btn btn-lg btn-danger " href="/wai" role="button">
+      <i class="material-icons md-48">help_outline</i>
+    </a>
+  </li>
+  <li class="nav-item active">
+    <a class="btn btn-lg btn-danger nav-btn" href="/wai/text/participle" role="button">
+      <i class="material-icons md-48">text_fields</i>
+    </a>
+  </li>
 
-<a class="btn btn-lg btn-danger " href="/wai/text/train/ostrich/summary" role="button">
-  <i class="material-icons md-48">info</i>
-</a>
+  <li class="nav-item active">
+    <a class="btn btn-lg btn-danger nav-btn" href="/wai/text/train/crawler/summary" role="button">
+      <i class="material-icons md-48">info</i>
+    </a>
+  </li>
 
-<a class="btn btn-lg btn-danger " href="/wai/text/train/parrot/summary" role="button">
-  <i class="material-icons md-48">info</i>
-</a>
+  <li class="nav-item active">
+    <a class="btn btn-lg btn-danger nav-btn" href="/wai/text/train/ostrich/summary" role="button">
+      <i class="material-icons md-48">info</i>
+    </a>
+  </li>
+
+  <li class="nav-item active">
+    <a class="btn btn-lg btn-danger nav-btn" href="/wai/text/train/parrot/summary" role="button">
+      <i class="material-icons md-48">info</i>
+    </a>
+  </li>
+/ul>
