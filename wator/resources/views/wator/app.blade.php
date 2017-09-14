726f48ce57871625710e727a4e7d794a0858f3a2
Wator Vapor <watorvapor@gmail.com>
2017-09-14 22:35:51 +0900

    Update app.blade.php

--- wator/resources/views/wator/app.blade.php
+++ wator/resources/views/wator/app.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width,initial-scale=0.2, maximum-scale=0.2,minimum-scale=0.2">
+    <meta name="viewport" content="width=device-width,initial-scale=0.3, maximum-scale=0.3,minimum-scale=0.3">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Wator</title>
     <!-- Fonts -->
