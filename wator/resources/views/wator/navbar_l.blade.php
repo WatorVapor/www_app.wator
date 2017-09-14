49a8a4d06a06b91872b9ff4b22f5efeb0cd2b70f
Wator Vapor <watorvapor@gmail.com>
2017-09-14 22:34:01 +0900

    Update navbar_l.blade.php

--- wator/resources/views/wator/navbar_l.blade.php
+++ wator/resources/views/wator/navbar_l.blade.php
@@ -1,4 +1,4 @@
-<ul class="navbar-nav nav">
+<ul class="navbar-nav nav justify-content-start">
   <li class="nav-item active">
     <a class="btn btn-lg btn-primary nav-btn" href="/" role="button">
       <i class="material-icons md-48">home</i>
