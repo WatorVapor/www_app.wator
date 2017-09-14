d5ac12d17192fd92e84e07efb826926eaea94dae
Wator Vapor <watorvapor@gmail.com>
2017-09-14 22:47:16 +0900

    Update navbar.blade.php

--- wator/resources/views/wai/navbar.blade.php
+++ wator/resources/views/wai/navbar.blade.php
@@ -1,5 +1,4 @@
-<ul class="navbar-nav nav justify-content-start">
-
+<ul class="navbar-nav nav justify-content-center">
   <li class="nav-item active">
     <a class="btn btn-lg btn-danger " href="/wai" role="button">
       <i class="material-icons md-48">help_outline</i>
@@ -10,22 +9,19 @@
       <i class="material-icons md-48">text_fields</i>
     </a>
   </li>
-
   <li class="nav-item active">
     <a class="btn btn-lg btn-danger nav-btn" href="/wai/text/train/crawler/summary" role="button">
       <i class="material-icons md-48">info</i>
     </a>
   </li>
-
   <li class="nav-item active">
     <a class="btn btn-lg btn-danger nav-btn" href="/wai/text/train/ostrich/summary" role="button">
       <i class="material-icons md-48">info</i>
     </a>
   </li>
-
   <li class="nav-item active">
     <a class="btn btn-lg btn-danger nav-btn" href="/wai/text/train/parrot/summary" role="button">
       <i class="material-icons md-48">info</i>
     </a>
   </li>
-/ul>
+</ul>
