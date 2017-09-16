048c999917adefb91deb566dd99826374c65bab2
Wator Vapor <watorvapor@gmail.com>
2017-09-16 10:58:00 +0900

    Update navbar_r.blade.php

--- wator/resources/views/wator/navbar_r.blade.php
+++ wator/resources/views/wator/navbar_r.blade.php
@@ -7,10 +7,10 @@
   $langs .= '<span class="flag-icon flag-icon-background flag-icon-jp"></span>';
   $langs .= '</a>';
   $langs .= '<br/>';
-  $langs .= '<a class="btn btn-success mr-lg-5 mt-lg-2" onclick="onClickLanguage(\'en\')" role="button">';
+  $langs .= '<a class="btn btn-success mr-lg-5 mt-lg-5" onclick="onClickLanguage(\'en\')" role="button">';
   $langs .= '<span class="flag-icon flag-icon-background flag-icon-us"></span>';
   $langs .= '</a>';
-  $langs .= '<a class="btn btn-success mt-lg-2" onclick="onClickLanguage(\'ru\')" role="button">';
+  $langs .= '<a class="btn btn-success mt-lg-5" onclick="onClickLanguage(\'ru\')" role="button">';
   $langs .= '<span class="flag-icon flag-icon-background flag-icon-ru"></span>';
   $langs .= '</a>';
 ?>
