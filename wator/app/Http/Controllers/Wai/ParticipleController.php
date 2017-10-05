2ede440ef6a08634edfaa912e8a6725c46fcf6ed
Wator Vapor <watorvapor@gmail.com>
2017-10-05 23:32:56 +0900

    Update ParticipleController.php

--- wator/app/Http/Controllers/Wai/ParticipleController.php
+++ wator/app/Http/Controllers/Wai/ParticipleController.php
@@ -3,7 +3,6 @@ namespace Wator\Http\Controllers\Wai;
 use Wator\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
-use Thujohn\Twitter\Twitter;
 
 class ParticipleController extends Controller
 {
