4a5bee4c15211e35bb8edb48d66604d96d778c8b
Wator Vapor <watorvapor@gmail.com>
2017-10-05 23:48:34 +0900

    Update TwitterController.php

--- wator/app/Http/Controllers/AIBot/TwitterController.php
+++ wator/app/Http/Controllers/AIBot/TwitterController.php
@@ -18,6 +18,7 @@ class TwitterController extends Controller
         //
         //$tw = new Twitter();
         //return $tw->postTweet(array('status' => '面倒くさそうにしてるキャラを格好良いと思ってたのが間違いだったんだと思う', 'format' => 'json'));
-        return Twitter::getHomeTimeline(['count' => 2, 'format' => 'json']);
+        //return Twitter::getHomeTimeline(['count' => 2, 'format' => 'json']);
+        return Twitter::getUserTimeline(['count' => 20, 'format' => 'array']);
     }
 }
