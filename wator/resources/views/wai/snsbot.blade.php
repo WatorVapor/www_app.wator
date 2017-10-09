bc1dfa6aac48a4ab02058765e7967d24ff9abc01
Wator Vapor <watorvapor@gmail.com>
2017-10-09 09:30:05 +0900

    Update snsbot.blade.php

--- wator/resources/views/wai/snsbot.blade.php
+++ wator/resources/views/wai/snsbot.blade.php
@@ -14,7 +14,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=0.35, maximum-scale=0.35,minimum-scale=0.35">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta property="twitter:image" content="{{ $img_url }}"/> 
+    <meta name="twitter:image" content="{{ $img_url }}"/> 
     <title>Wator</title>
     <!-- Fonts -->
     <link href="//fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
