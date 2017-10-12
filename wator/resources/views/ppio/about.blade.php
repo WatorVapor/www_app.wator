66442bdc6392b130949d7ba5b9e43d7055999429
Wator Vapor <watorvapor@gmail.com>
2017-10-12 20:06:09 +0900

    Update about.blade.php

--- wator/resources/views/ppio/about.blade.php
+++ wator/resources/views/ppio/about.blade.php
@@ -5,54 +5,54 @@
 
 @section('content')
 <div class="row">
-  <div class="col-md-3">
-    <div class="panel panel-default">
-      <div class="panel-heading">
+  <div class="col">
+    <div class="card card-default">
+      <div class="card-heading">
         <h6>Devices</h6>
       </div>
-      <div class="panel-body">
+      <div class="card-body">
         <img src="/images/ppio/SmartHome-WebRTC-IoT-Hub.png" class="img-thumbnail" alt="rsa gif" width="100%">
       </div>
     </div>
   </div>
-  <div class="col-md-3">
-    <div class="panel panel-default">
-      <div class="panel-heading">
+  <div class="col">
+    <div class="card card-default">
+      <div class="card-heading">
         <h6>Webrtc</h6>
       </div>
-      <div class="panel-body">
+      <div class="card-body">
         <img src="/images/ppio/webrtc-sample-image.png" class="img-thumbnail" alt="webrtc over rsa peer" width="100%">
       </div>
     </div>
   </div>
-  <div class="col-md-3">
-    <div class="panel panel-default">
-      <div class="panel-heading">
+  <div class="col">
+    <div class="card card-default">
+      <div class="card-heading">
         <h6>Webrtc Signal</h6>
       </div>
-      <div class="panel-body">
+      <div class="card-body">
         <img src="/images/ppio/webrtc-another-web-3-638.jpg" class="img-thumbnail" alt="signal" width="100%">
       </div>
     </div>
   </div>
-  <div class="col-md-3">
-    <div class="panel panel-default">
-      <div class="panel-heading">
+  <div class="col">
+    <div class="card card-default">
+      <div class="card-heading">
         <h6>RSA</h6>
       </div>
-      <div class="panel-body">
+      <div class="card-body">
         <img src="/images/ppio/eCCob.png" class="img-thumbnail" alt="rsa gif" width="100%">
       </div>
     </div>
   </div>
 </div>
 <div class="row">
-  <div class="col-md-3">
-    <div class="panel panel-default">
-      <div class="panel-heading">
+  <div class="col">
+    <div class="card card-default">
+      <div class="card-heading">
         <h6>Web Bluetooth</h6>
       </div>
-      <div class="panel-body">
+      <div class="card-body">
         <img src="/img/ddstar/Web-Bluetooth-1-768x454.jpg" class="img-thumbnail" alt="Web Bluetooth API" width="100%">
       </div>
     </div>
