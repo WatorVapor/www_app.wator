0728fa6c95370998f28e7dc1293301fd45d913d1
Wator Vapor <watorvapor@gmail.com>
2017-10-12 21:05:03 +0900

    Update ble_chart.blade.php

--- wator/resources/views/ppio/ble_chart.blade.php
+++ wator/resources/views/ppio/ble_chart.blade.php
@@ -10,44 +10,46 @@
     height:200px;
   }
 </style>
-<div class="container">
-  <div class="row">
-    <div class="col-md-6">
-      <div class="panel panel-default">
-        <div class="panel-heading">8bit signed</div>
-        <div class="panel-body">
-          <canvas class="ble-chart-canvas ble-chart-canvas-signed"></canvas>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-6">
-      <div class="panel panel-default">
-        <div class="panel-heading">8bit unsigned</div>
-        <div class="panel-body">
-          <canvas class="ble-chart-canvas ble-chart-canvas-unsigned"></canvas>
-        </div>
+<div class="row">
+  <div class="col-md-6">
+    <div class="panel panel-default">
+      <div class="panel-heading">8bit signed</div>
+      <div class="panel-body">
+        <canvas class="ble-chart-canvas ble-chart-canvas-signed"></canvas>
       </div>
     </div>
   </div>
-  <div class="row">
-    <div class="col-md-6">
-      <div class="panel panel-default">
-        <div class="panel-heading">16bit signed</div>
-        <div class="panel-body">
-          <canvas class="ble-chart-canvas ble-chart-canvas-signed"></canvas>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-6">
-      <div class="panel panel-default">
-        <div class="panel-heading">16bit unsigned</div>
-        <div class="panel-body">
-          <canvas class="ble-chart-canvas ble-chart-canvas-unsigned"></canvas>
-        </div>
+  <div class="col-md-6">
+    <div class="panel panel-default">
+      <div class="panel-heading">8bit unsigned</div>
+      <div class="panel-body">
+        <canvas class="ble-chart-canvas ble-chart-canvas-unsigned"></canvas>
       </div>
     </div>
   </div>
 </div>
+<div class="row">
+  <div class="col-md-6">
+    <div class="panel panel-default">
+      <div class="panel-heading">16bit signed</div>
+      <div class="panel-body">
+        <canvas class="ble-chart-canvas ble-chart-canvas-signed"></canvas>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-6">
+    <div class="panel panel-default">
+      <div class="panel-heading">16bit unsigned</div>
+      <div class="panel-body">
+        <canvas class="ble-chart-canvas ble-chart-canvas-unsigned"></canvas>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
 <script type="text/javascript">
   function drawSignedCanvas(canvas) {
     var ctx = canvas.getContext("2d");
