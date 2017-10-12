911db228ba768ee20ce93c8627f9c69af61ca51e
Wator Vapor <watorvapor@gmail.com>
2017-10-12 19:53:03 +0900

    Create navbar.blade.php

---
+++ wator/resources/views/ppio/navbar.blade.php
@@ -0,0 +1,27 @@
+<ul class="navbar-nav nav justify-content-center">
+  <li class="nav-item active ml-lg-3">
+    <a class="btn btn-lg btn-danger " href="/wai" role="button">
+      <i class="material-icons md-48">help_outline</i>
+    </a>
+  </li>
+  <li class="nav-item active ml-lg-3">
+    <a class="btn btn-lg btn-danger nav-btn" href="/wai/text/participle" role="button">
+      <i class="material-icons md-48">text_fields</i>
+    </a>
+  </li>
+  <li class="nav-item active ml-lg-3">
+    <a class="btn btn-lg btn-danger nav-btn" href="/wai/text/train/crawler/summary" role="button">
+      <i class="material-icons md-48">info</i>
+    </a>
+  </li>
+  <li class="nav-item active ml-lg-3">
+    <a class="btn btn-lg btn-danger nav-btn" href="/wai/text/train/ostrich/summary" role="button">
+      <i class="material-icons md-48">info</i>
+    </a>
+  </li>
+  <li class="nav-item active ml-lg-3">
+    <a class="btn btn-lg btn-danger nav-btn" href="/wai/text/train/parrot/summary" role="button">
+      <i class="material-icons md-48">info</i>
+    </a>
+  </li>
+</ul>
