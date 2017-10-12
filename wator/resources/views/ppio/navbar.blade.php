0c53b6745b0ea92c631b9ff8cf09f1267219c3e3
Wator Vapor <watorvapor@gmail.com>
2017-10-12 21:57:11 +0900

    Update navbar.blade.php

--- wator/resources/views/ppio/navbar.blade.php
+++ wator/resources/views/ppio/navbar.blade.php
@@ -13,11 +13,33 @@
   $bluetooths .= '<a class="btn btn-lg btn-danger mr-lg-5 " href="/ppio/ble/search" role="button">';
   $bluetooths .= '<i class="material-icons md-48">bluetooth_searching</i>';
   $bluetooths .= '</a>';
-  $bluetooths .= '<a class="btn btn-lg btn-danger mr-lg-5" href="/ppio/ble/raw" role="button">';
+  $bluetooths .= '<a class="btn btn-lg btn-danger mr-lg-5" href="/ppio/ble/raw" role="button" disabled>';
   $bluetooths .= '<i class="material-icons md-48">bug_report</i>';
   $bluetooths .= '</a>';
   $bluetooths .= '</div>';
 ?>
+
+<?php
+  $clouds = '';
+  $clouds .= '<div class="row justify-content-around mt-lg-3 ml-lg-4 mr-lg-4">';
+  $clouds .= '<a class="btn btn-lg btn-danger mr-lg-5 " href="/ppio/cloud/gofuro" role="button">';
+  $clouds .= '<i class="material-icons md-48">hot_tub</i>';
+  $clouds .= '</a>';
+  $clouds .= '<a class="btn btn-lg btn-danger mr-lg-5" href="/ppio/cloud/chart" role="button" disabled>';
+  $clouds .= '<i class="material-icons md-48">timeline</i>';
+  $clouds .= '</a>';
+  $clouds .= '</div>';
+  $clouds .= '<hr/>';
+  $clouds .= '<div class="row justify-content-around mt-lg-3 ml-lg-4 mr-lg-4">';
+  $clouds .= '<a class="btn btn-lg btn-danger mr-lg-5 " href="/ppio/cloud/search" role="button" disabled >';
+  $clouds .= '<i class="material-icons md-48">bluetooth_searching</i>';
+  $clouds .= '</a>';
+  $clouds .= '<a class="btn btn-lg btn-danger mr-lg-5" href="/ppio/cloud/raw" role="button" disabled>';
+  $clouds .= '<i class="material-icons md-48">bug_report</i>';
+  $clouds .= '</a>';
+  $clouds .= '</div>';
+?>
+
 
 
 <ul class="navbar-nav nav justify-content-center">
@@ -29,6 +51,11 @@
   <li class="nav-item active ml-lg-3">
     <a tabindex="0" href="#" class="btn btn-lg btn-danger nav-btn" data-container="body" data-html="true" data-trigger="focus" data-toggle="popover" data-placement="bottom" data-content="{{ $bluetooths }}">
       <i class="material-icons md-48">bluetooth</i>
+    </a>
+  </li>
+  <li class="nav-item active ml-lg-3">
+    <a tabindex="0" href="#" class="btn btn-lg btn-danger nav-btn" data-container="body" data-html="true" data-trigger="focus" data-toggle="popover" data-placement="bottom" data-content="{{ $clouds }}">
+      <i class="material-icons md-48">cloud</i>
     </a>
   </li>
 </ul>
