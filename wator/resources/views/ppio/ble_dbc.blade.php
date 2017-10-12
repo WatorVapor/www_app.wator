eed111b9f9c41c903f3c353a53ab977154bf0095
Wator Vapor <watorvapor@gmail.com>
2017-10-12 20:21:50 +0900

    Create ble_dbc.blade.php

---
+++ wator/resources/views/ppio/ble_dbc.blade.php
@@ -0,0 +1,18 @@
+@extends('wator.app')
+@section('appnavbar')
+  @include('ppio.navbar')
+@endsection
+
+@section('content')
+<div class="row">
+  <button type="button" class="btn btn-success btn-lg btn-block" onclick="onBLESearch(this)">Search</button>
+</div>
+
+<script src="/ppio/js/ble.web.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+  function onBLESearch(element) {
+    runBLESource();
+  }
+</script>
+@endsection
