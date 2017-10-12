0c866e4768ac96e69c1b3aaddf7494946ca4db51
Wator Vapor <watorvapor@gmail.com>
2017-10-12 19:59:13 +0900

    Update navbar.blade.php

--- wator/resources/views/ppio/navbar.blade.php
+++ wator/resources/views/ppio/navbar.blade.php
@@ -1,26 +1,16 @@
 <ul class="navbar-nav nav justify-content-center">
   <li class="nav-item active ml-lg-3">
-    <a class="btn btn-lg btn-danger " href="/wai" role="button">
+    <a class="btn btn-lg btn-danger " href="/ppio" role="button">
       <i class="material-icons md-48">help_outline</i>
     </a>
   </li>
   <li class="nav-item active ml-lg-3">
-    <a class="btn btn-lg btn-danger nav-btn" href="/wai/text/participle" role="button">
+    <a class="btn btn-lg btn-danger nav-btn" href="/ppio/ble/dbc" role="button">
       <i class="material-icons md-48">text_fields</i>
     </a>
   </li>
   <li class="nav-item active ml-lg-3">
-    <a class="btn btn-lg btn-danger nav-btn" href="/wai/text/train/crawler/summary" role="button">
-      <i class="material-icons md-48">info</i>
-    </a>
-  </li>
-  <li class="nav-item active ml-lg-3">
-    <a class="btn btn-lg btn-danger nav-btn" href="/wai/text/train/ostrich/summary" role="button">
-      <i class="material-icons md-48">info</i>
-    </a>
-  </li>
-  <li class="nav-item active ml-lg-3">
-    <a class="btn btn-lg btn-danger nav-btn" href="/wai/text/train/parrot/summary" role="button">
+    <a class="btn btn-lg btn-danger nav-btn" href="/ppio/ble/chart" role="button">
       <i class="material-icons md-48">info</i>
     </a>
   </li>
