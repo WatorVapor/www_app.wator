57ed22f6e335f640b08a63b12896b8d8badd83fd
Wator Vapor <watorvapor@gmail.com>
2017-10-12 22:08:56 +0900

    Create CloudGoFuroController.php

---
+++ wator/app/Http/Controllers/PPio/CloudGoFuroController.php
@@ -0,0 +1,17 @@
+<?php
+namespace Wator\Http\Controllers\PPio;
+use Wator\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+class BLEDualBoxCarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return view('ppio.ble_dbc');
+    }
+}
