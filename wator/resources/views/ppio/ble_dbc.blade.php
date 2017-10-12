4c95057dd1dfd75451d32ba5d282960c38e288d9
Wator Vapor <watorvapor@gmail.com>
2017-10-12 20:35:03 +0900

    Update ble_dbc.blade.php

--- wator/resources/views/ppio/ble_dbc.blade.php
+++ wator/resources/views/ppio/ble_dbc.blade.php
@@ -10,6 +10,38 @@
   </div>
 </div>
 
+
+<div class="container mt-lg-5">
+  <div class="row mt-lg-5 justify-content-center">
+    <div class="col-6">
+      <div class="input-group">
+        <span class="input-group-btn">
+          <button class="btn btn-primary" type="submit">1-255</button>
+        </span>
+        <input type="text" class="form-control" name="speed" placeholder="50"  value="50" />
+    </div>
+  </div>
+</div>
+
+<div class="container mt-lg-5">
+  <div class="row mt-lg-5 justify-content-center">
+    <div class="col-6">
+      <div class="row align-items-start justify-content-center">
+        <button type="submit" class="btn btn-lg btn-success"><i class="material-icons">keyboard_arrow_up</i></button>
+      </div>
+      <div class="row align-items-center justify-content-center">
+        <button type="submit" class="btn btn-lg btn-success"><i class="material-icons">keyboard_arrow_left</i></button>
+        <button type="submit" class="btn btn-lg btn-danger"><i class="material-icons">stop</i></button>
+        <button type="submit" class="btn btn-lg btn-success"><i class="material-icons">keyboard_arrow_right</i></button>
+      </div>
+      <div class="row align-items-end justify-content-center">
+        <button type="submit" class="btn btn-lg btn-success"><i class="material-icons">keyboard_arrow_down</i></button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
 <script src="/wator/ppio/js/ble.web.js" type="text/javascript"></script>
 
 <script type="text/javascript">
