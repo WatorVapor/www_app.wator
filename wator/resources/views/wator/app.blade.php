513a12904ae3a9d923bea646f7f643402ad58503
Wator Vapor <watorvapor@gmail.com>
2017-10-16 21:08:20 +0900

    Update app.blade.php

--- wator/resources/views/wator/app.blade.php
+++ wator/resources/views/wator/app.blade.php
@@ -28,6 +28,17 @@
         max-width: 400px !important;
       }
     </style>
+    <style type="text/css">
+      .footer {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        /* Set the fixed height of the footer here */
+        height: 60px;
+        line-height: 60px; /* Vertically center the text there */
+        background-color: #f5f5f5;
+      }
+    </style>
     
   </head>
   <body>
