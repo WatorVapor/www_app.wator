a5b853db81443e325016a16d856d94ffddc5f0ff
Wator Vapor <watorvapor@gmail.com>
2017-10-17 23:16:20 +0900

    Update weibo.blade.php

--- wator/resources/views/aibot/weibo.blade.php
+++ wator/resources/views/aibot/weibo.blade.php
@@ -5,7 +5,6 @@
 @section('content')
 <div class="row justify-content-center">
   <div class="col-6">
-     <img height="120" src="/wator/images/story/ai.story.{{ $animate }}.jpg">
    </div>
 </div>
 @endsection
