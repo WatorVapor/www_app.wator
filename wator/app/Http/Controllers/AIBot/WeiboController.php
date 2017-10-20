f70e290daf6f16a3a8ce5a5ae473339756f82e40
Wator Vapor <watorvapor@gmail.com>
2017-10-20 22:15:20 +0900

    Update WeiboController.php

--- wator/app/Http/Controllers/AIBot/WeiboController.php
+++ wator/app/Http/Controllers/AIBot/WeiboController.php
@@ -7,15 +7,25 @@ use Illuminate\Http\Request;
 
 class WeiboController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function auth()
     {
         //
         return \Socialite::with('weibo')->redirect();
+    }
+    public function index()
+    {
+        //
+        try {
+            $oauthUser = \Socialite::with('weibo')->user();
+            var_dump($oauthUser->getId());
+            var_dump($oauthUser->getNickname());
+            var_dump($oauthUser->getName());
+            var_dump($oauthUser->getEmail());
+            var_dump($oauthUser->getAvatar());        
+        } catch( \Exception $e ) {
+            var_dump($e->getMessage());
+        }
+        return view('aibot.weibo_cancel');
     }
     public function cancel()
     {
