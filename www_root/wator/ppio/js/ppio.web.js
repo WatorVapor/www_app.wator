1a2b87c9cf7d583b85e22541372145964019b2af
Wator Vapor <watorvapor@gmail.com>
2017-10-24 22:11:50 +0900

    Update ppio.web.js

--- www_root/wator/ppio/js/ppio.web.js
+++ www_root/wator/ppio/js/ppio.web.js
@@ -1,1 +1,20 @@
+var uri = 'wss://' + location.host + '/ws/starbian';
+console.log('uri =<',uri,'>');
+var ws = new WebSocket(uri);
+ws.onopen = onOpen;
+ws.onmessage = onMessage;
+ws.onclose = onClose;
+ws.onerror = onError;
 
+function onOpen(event) {
+  console.log('event =<',event,'>');
+}
+function onMessage(event) {
+  console.log('event =<',event,'>');
+}
+function onError(event) {
+  console.log('event =<',event,'>');
+}
+function onClose(event) {
+  console.log('event =<',event,'>');
+}
