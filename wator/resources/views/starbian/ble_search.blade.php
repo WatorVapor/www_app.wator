834ebd1daf1e15901755d5179f49b868bb5f9191
Wator Vapor <watorvapor@gmail.com>
2017-10-26 08:32:11 +0900

    Update ble_search.blade.php

--- wator/resources/views/starbian/ble_search.blade.php
+++ wator/resources/views/starbian/ble_search.blade.php
@@ -8,7 +8,7 @@
   <button type="button" class="btn btn-success btn-lg btn-block" onclick="onBLESearch(this)">Search</button>
 </div>
 
-<script src="/p2pio/js/ble.web.js" type="text/javascript"></script>
+<script src="/wator/starbian/js/ble.web.js" type="text/javascript"></script>
 <script type="text/javascript">
   function onBLESearch(element) {
     runBLESource();
