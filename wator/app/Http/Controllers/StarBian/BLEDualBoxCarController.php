595c13f6b1922b8efefa2b27cde12830b4cb1601
Wator Vapor <watorvapor@gmail.com>
2017-10-26 08:11:16 +0900

    Update BLEDualBoxCarController.php

--- wator/app/Http/Controllers/StarBian/BLEDualBoxCarController.php
+++ wator/app/Http/Controllers/StarBian/BLEDualBoxCarController.php
@@ -1,5 +1,5 @@
 <?php
-namespace Wator\Http\Controllers\PPio;
+namespace Wator\Http\Controllers\StarBian;
 use Wator\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 class BLEDualBoxCarController extends Controller
