91bf1b328b4fd84ecb6a963fc39d00d111acde7f
Wator Vapor <watorvapor@gmail.com>
2017-12-06 22:00:16 +0900

    Update navbar_l.blade.php

--- wator/resources/views/wator/navbar_l.blade.php
+++ wator/resources/views/wator/navbar_l.blade.php
@@ -11,7 +11,7 @@
   </li>
   <li class="nav-item active ml-lg-4">
     <a class="btn btn-lg btn-default nav-btn" href="/ethereum" role="button">
-      <img src="/wator/images/ethereum/Ethereum.jpg" />
+      <img src="/wator/images/ethereum/ethereum_icon.png" />
     </a>
   </li>
 </ul>
