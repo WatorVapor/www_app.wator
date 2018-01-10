21112549044cd8843955ebcee105fe68363dc07f
Wator Vapor <watorvapor@gmail.com>
2018-01-10 21:57:08 +0900

    Update gofuro.blade.php

--- wator/resources/views/starbian/gofuro.blade.php
+++ wator/resources/views/starbian/gofuro.blade.php
@@ -35,9 +35,7 @@
   </div>
 </div>
 
-<!--
 <script src="///unpkg.com/ipfs-api/dist/index.js"></script>
--->
 <script src="//unpkg.com/ipfs/dist/index.js"></script>
 <script src="/wator/starbian/js/starbian.web.js" type="text/javascript"></script>
 
