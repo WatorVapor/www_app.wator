77ad36e3a362534275977c28b9c47dc0e62db614
Wator Vapor <watorvapor@gmail.com>
2018-02-14 23:45:30 +0900

    Update app.js

--- notify_ws/app.js
+++ notify_ws/app.js
@@ -1,5 +1,5 @@
 const WebSocket = require('ws');
-const config = {host:'127.0.01', port: 8080 }
+const config = {host:'127.0.01', port: 19082 }
 const wss = new WebSocket.Server(config);
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
