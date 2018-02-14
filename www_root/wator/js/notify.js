501a95df7d8678c005395f29cc95ac31ded81d16
Wator Vapor <watorvapor@gmail.com>
2018-02-14 23:19:25 +0900

    Update notify.js

--- www_root/wator/js/notify.js
+++ www_root/wator/js/notify.js
@@ -1,1 +1,19 @@
+let uri = "ws://" + location.host + "/wss/notify";
+let ws = new WebSocket(uri);
+ws.onopen = onNotifyOpen_;
+ws.onmessage = onNotifyMessage_;
+ws.onclose = onNotifyClose_;
+ws.onerror = onNotifyError_;
 
+function onNotifyOpen_(evt) {
+  console.log('onNotifyOpen_:evt=<',evt,'>');
+}
+function onNotifyMessage_(evt) {
+  console.log('onNotifyMessage_:evt=<',evt,'>');
+}
+function onNotifyClose_(evt) {
+  console.log('onNotifyClose_:evt=<',evt,'>');
+}
+function onNotifyError_(evt) {
+  console.log('onNotifyError_:evt=<',evt,'>');
+}
