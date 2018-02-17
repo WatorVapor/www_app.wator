8e2fef3b8f84d1a4393edad551f474caa27ee422
Wator Vapor <watorvapor@gmail.com>
2018-02-17 16:26:49 +0900

    Update app.js

--- notify_ws/app.js
+++ notify_ws/app.js
@@ -4,5 +4,32 @@ const wss = new WebSocket.Server(config);
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     console.log('received: message=<', message,'>');
+    let jsonMsg = JSON.parse(message);
+    if(jsonMsg) {
+      if(jsonMsg.channel && jsonMsg.msg) {
+        setTimeout(function() {
+          if(pub) {
+            pub.publish(jsonMsg.channel,JSON.stringify(jsonMsg.msg));
+          }
+        },1);
+      }
+    }
   });
 });
+
+const redis = require("redis");
+const sub = redis.createClient();
+sub.on('message', function(channel, message) {
+  console.log('sub::channel=<',channel,'>');
+  console.log('sub::message=<',message,'>');
+  console.log('sub::wss=<',wss,'>');
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(message);
+    }
+  })
+});
+sub.subscribe("wator/wai/webapp/notify");
+
+
+const pub  = redis.createClient();
