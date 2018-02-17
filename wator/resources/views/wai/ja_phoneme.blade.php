0a5cc0035c170f47267aa614daa56e5eaebb1682
Wator Vapor <watorvapor@gmail.com>
2018-02-18 00:23:46 +0900

    Update ja_phoneme.blade.php

--- wator/resources/views/wai/ja_phoneme.blade.php
+++ wator/resources/views/wai/ja_phoneme.blade.php
@@ -3,13 +3,13 @@
     <h2 class="card-title">{{ $phoneme }}</h2>
     <div class="row">
       <div class="col">
-        <button type="submit" value="cn" name="lang" class="btn btn-primary">
-          <i class="material-icons md-18" style="color:green;">hearing</i>
+        <button type="submit" value="cn" name="lang" class="btn btn-dark">
+          <i class="material-icons " style="color:green;">hearing</i>
         </button>
       </div>
       <div class="col">
-        <button type="submit" value="cn" name="lang" class="btn btn-primary">
-          <i class="material-icons md-18" style="color:red;">mic</i>
+        <button type="submit" value="cn" name="lang" class="btn btn-dark">
+          <i class="material-icons " style="color:red;">mic</i>
         </button>
       </div>
     </div>
