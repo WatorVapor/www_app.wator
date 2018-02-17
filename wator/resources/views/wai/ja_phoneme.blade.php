b3498a4eb7eeb7b96fe7f5eda8f18c2d3cd8d504
Wator Vapor <watorvapor@gmail.com>
2018-02-17 23:51:57 +0900

    Update ja_phoneme.blade.php

--- wator/resources/views/wai/ja_phoneme.blade.php
+++ wator/resources/views/wai/ja_phoneme.blade.php
@@ -3,7 +3,7 @@ $phoneme = $phonemeTable[$phonemeIndex];
 $phonemeIndex = $phonemeIndex + 1;
 @endphp
 <div class="row">
-  <div class="col">{{ $phoneme }}@{{ $phonemeIndex }}</div>
+  <div class="col">{{ $phoneme }}.{{ $phonemeIndex }}</div>
   <div class="col">
     <button type="submit" value="cn" name="lang" class="btn btn-block btn-primary">
       <i class="material-icons md-48">hearing</i>
