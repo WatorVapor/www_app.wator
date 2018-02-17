d2326d2ba41dcb747931ccbbd9b72b04e46a8b9b
Wator Vapor <watorvapor@gmail.com>
2018-02-17 17:45:14 +0900

    Update participle.blade.php

--- wator/resources/views/wai/participle.blade.php
+++ wator/resources/views/wai/participle.blade.php
@@ -66,8 +66,12 @@ foreach ($result as $sentence) {
   </div>
 </div>
 
-
-
 <script src="/wator/wai/notify.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+  function onUpdateData(msg) {
+    console.log('onUpdateData:msg=<',msg,'>');
+  }
+</script>
 
 @endsection
