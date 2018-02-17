2577cd9bf3388994392bf0713b289a13a8b15d4d
Wator Vapor <watorvapor@gmail.com>
2018-02-18 00:38:58 +0900

    Update ja_phoneme.blade.php

--- wator/resources/views/wai/ja_phoneme.blade.php
+++ wator/resources/views/wai/ja_phoneme.blade.php
@@ -2,15 +2,15 @@
   <div class="card-body">
     <div class="row">
       <div class="col-sm">
-        <button type="submit" class="btn btn-sm btn-dark rounded-circle">
+        <button type="submit" class="btn btn-sm btn-dark rounded-circle m-0 p-0">
           <i class="material-icons " style="font-size:12px;color:green;">hearing</i>
         </button>
       </div>
       <div class="col-sm">
-        <h6 class="rounded-circle">{{ $phoneme }}</h6>
+        <h6 class="rounded-circle m-0 p-0">{{ $phoneme }}</h6>
       </div>
       <div class="col-sm">
-        <button type="submit" class="btn btn-sm btn-dark rounded-circle">
+        <button type="submit" class="btn btn-sm btn-dark rounded-circle m-0 p-0">
           <i class="material-icons " style="font-size:12px;color:red;">mic</i>
         </button>
       </div>
