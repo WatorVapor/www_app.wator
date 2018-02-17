eea6b61c58145a0d01331323e823cf5f7f438562
Wator Vapor <watorvapor@gmail.com>
2018-02-17 23:33:49 +0900

    Update recorder_phoneme.blade.php

--- wator/resources/views/wai/recorder_phoneme.blade.php
+++ wator/resources/views/wai/recorder_phoneme.blade.php
@@ -1,1 +1,14 @@
+<div class="row">
+  <div class="col">{{phoneme}}</div>
+  <div class="col">
+    <button type="submit" value="cn" name="lang" class="btn btn-block btn-primary">
+      <i class="material-icons md-48">hearing</i>
+    </button>
+  </div>
+  <div class="col">
+    <button type="submit" value="cn" name="lang" class="btn btn-block btn-primary">
+      <i class="material-icons md-48">mic</i>
+    </button>
+  </div>
+</div>
 
