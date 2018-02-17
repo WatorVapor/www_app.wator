9f51a7a8583a9e365c0c7a4f731f3980de63d441
Wator Vapor <watorvapor@gmail.com>
2018-02-18 00:16:09 +0900

    Update ja_phoneme.blade.php

--- wator/resources/views/wai/ja_phoneme.blade.php
+++ wator/resources/views/wai/ja_phoneme.blade.php
@@ -1,24 +1,17 @@
 <div class="card card-default text-center border border-danger">
   <div class="card-body">
     <h2 class="card-title">{{ $phoneme }}</h2>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-
-<div class="row justify-content-center">
-  <div class="col justify-content-center">{{ $phoneme }}</div>
-</div>
-
-<div class="row">
-  <div class="col">
-    <button type="submit" value="cn" name="lang" class="btn btn-primary">
-      <i class="material-icons">hearing</i>
-    </button>
-  </div>
-  <div class="col">
-    <button type="submit" value="cn" name="lang" class="btn btn-primary">
-      <i class="material-icons">mic</i>
-    </button>
+    <div class="row">
+      <div class="col">
+        <button type="submit" value="cn" name="lang" class="btn btn-primary">
+          <i class="material-icons">hearing</i>
+        </button>
+      </div>
+      <div class="col">
+        <button type="submit" value="cn" name="lang" class="btn btn-primary">
+          <i class="material-icons">mic</i>
+        </button>
+      </div>
+    </div>
   </div>
 </div>
