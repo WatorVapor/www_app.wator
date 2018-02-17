376c3ed20ce72f34f3a2b6978c6fe7fd8fdae5f9
Wator Vapor <watorvapor@gmail.com>
2018-02-17 23:14:12 +0900

    Update recorder_voice.blade.php

--- wator/resources/views/wai/recorder_voice.blade.php
+++ wator/resources/views/wai/recorder_voice.blade.php
@@ -8,6 +8,17 @@
 @section('content')
 
 
+<div class="row justify-content-center">
+  <div class="col-lg-10 ">
+    <div class="card card-default">
+      <div class="card-body">
+        <h4 class="card-title">{{ trans('wai_record_voice.hirakana') }}</h4>
+      </div>
+    </div>
+  </div>
+</div>
+
+
 <script src="/wator/wai/notify.js" type="text/javascript"></script>
 
 <script type="text/javascript">
