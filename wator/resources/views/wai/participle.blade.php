bcfc1e54acbb63a49de51e7104f63b8f1750b79a
Wator Vapor <watorvapor@gmail.com>
2018-02-17 21:05:26 +0900

    Update participle.blade.php

--- wator/resources/views/wai/participle.blade.php
+++ wator/resources/views/wai/participle.blade.php
@@ -6,7 +6,6 @@
 @endsection
 
 @section('content')
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <div class="row justify-content-center">
   <div class="col-lg-10 ">
