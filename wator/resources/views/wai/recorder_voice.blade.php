51c8817dcdf901e87fd90f54c80271c2159bad4d
Wator Vapor <watorvapor@gmail.com>
2018-02-17 23:14:58 +0900

    Update recorder_voice.blade.php

--- wator/resources/views/wai/recorder_voice.blade.php
+++ wator/resources/views/wai/recorder_voice.blade.php
@@ -18,6 +18,18 @@
   </div>
 </div>
 
+<hr/>
+<div class="row justify-content-center">
+  <div class="col-lg-10 ">
+    <div class="card card-default">
+      <div class="card-body">
+        <h4 class="card-title">{{ trans('wai_record_voice.pinyin') }}</h4>
+      </div>
+    </div>
+  </div>
+</div>
+
+
 
 <script src="/wator/wai/notify.js" type="text/javascript"></script>
 
