95ef02e55166cf860ad657993b2506b8bef48057
Wator Vapor <watorvapor@gmail.com>
2018-02-18 21:24:14 +0900

    Update upload.js

--- www_root/wator/wai/upload.js
+++ www_root/wator/wai/upload.js
@@ -48,9 +48,9 @@ function uploadSlice(chunks,phoneme) {
   let audioElem = document.getElementById('upload-form-audio');
   console.log('uploadSlice:audioElem=<',audioElem,'>');
   */
-  let bufText = Buffer.from(chunks, 'utf8');
+  let bufText = Buffer.from(chunks, 'binary');
   if(ipfs) {
-    ipfs.files.add(blob,function(bufText, result){
+    ipfs.files.add(bufText,function(err, result){
       if (err) {
         throw err;
       }
