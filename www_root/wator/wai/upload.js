fd7c4b76f090685b5cdff990f2530bdbe2b44aaa
Wator Vapor <watorvapor@gmail.com>
2018-02-18 17:22:26 +0900

    Update upload.js

--- www_root/wator/wai/upload.js
+++ www_root/wator/wai/upload.js
@@ -1,4 +1,4 @@
-
+/*
 function uploadSlice(chunks,phoneme) {
   let blob = new Blob(chunks, {type:"application/octet-stream"});
   console.log('uploadSlice:blob=<',blob,'>');
@@ -24,6 +24,21 @@ function uploadSlice(chunks,phoneme) {
     }
   });
 }
+*/
+
+function uploadSlice(chunks,phoneme) {
+  const blob = new Blob(chunks, { type: 'audio/webm' });
+  let urlBlob = window.URL.createObjectURL(blob);
+  console.log('saveToFile:urlBlob=<',urlBlob,'>');
+  let a = document.createElement('a');
+  document.body.appendChild(a);
+  a.style = 'display: none';
+  a.href = urlBlob;
+  a.download = phoneme + '.test.webm';
+  a.click();
+  window.URL.revokeObjectURL(url);
+}
+
 
 
 function saveToFile(chunks,phoneme) {
