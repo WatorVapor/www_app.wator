aa961bc8f33f62d783d04d4673fad3e6efc55de0
Wator Vapor <watorvapor@gmail.com>
2018-02-18 17:35:38 +0900

    Update upload.js

--- www_root/wator/wai/upload.js
+++ www_root/wator/wai/upload.js
@@ -29,14 +29,12 @@ function uploadSlice(chunks,phoneme) {
 function uploadSlice(chunks,phoneme) {
   const blob = new Blob(chunks, { type: 'audio/webm' });
   let urlBlob = window.URL.createObjectURL(blob);
-  console.log('saveToFile:urlBlob=<',urlBlob,'>');
-  let a = document.createElement('a');
-  document.body.appendChild(a);
-  a.style = 'display: none';
-  a.href = urlBlob;
-  a.download = phoneme + '.test.webm';
-  a.click();
-  window.URL.revokeObjectURL(urlBlob);
+  console.log('uploadSlice:urlBlob=<',urlBlob,'>');
+  let phonemeElem = document.getElementById('upload-form-phoneme');
+  console.log('uploadSlice:phonemeElem=<',phonemeElem,'>');
+  phonemeElem.textContent = phoneme;
+  let audioElem = document.getElementById('upload-form-audio');
+  console.log('uploadSlice:audioElem=<',audioElem,'>');
 }
 
 
