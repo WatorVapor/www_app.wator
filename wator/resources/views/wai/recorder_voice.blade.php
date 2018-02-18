401d6bcc1bf5be05b1e058ef521e43a84d51d9e4
Wator Vapor <watorvapor@gmail.com>
2018-02-18 10:54:14 +0900

    Update recorder_voice.blade.php

--- wator/resources/views/wai/recorder_voice.blade.php
+++ wator/resources/views/wai/recorder_voice.blade.php
@@ -118,15 +118,13 @@
     let mr = new MediaRecorder(stream);
     mr.mimeType = 'audio/webm'; // audio/webm or audio/ogg or audio/wav
     mr.ondataavailable = function (blob) {
-      setTimeout(function(){
-        mr.stop();      
-      },1);
+      mr.stop(); 
       console.log('ondataavailable:blob=<',blob,'>');
       let blobURL = window.URL.createObjectURL(blob);
       console.log('ondataavailable:phoneme=<',phoneme,'>');
       console.log('ondataavailable:blobURL=<',blobURL,'>');
     }
-    mr.start(3000);
+    mr.start(5000);
   }
   function onMediaError(e) {
     console.error('media error e=<', e,'>');
