d47e0ada4cf8e85f48d29f52c065ebf440067042
Wator Vapor <watorvapor@gmail.com>
2018-02-18 12:24:50 +0900

    Update upload.js

--- www_root/wator/wai/upload.js
+++ www_root/wator/wai/upload.js
@@ -1,18 +1,19 @@
 
-function uploadSlice(audio,phoneme) {
+function uploadSlice(chunks,phoneme) {
   console.log('uploadSlice:audio=<',audio,'>');
+  let blob = new Blob(chunks, {type:"application/octet-stream"}
   let formData = new FormData();
   formData.append('filename', phoneme + '.webm');
-  formData.append('audio', audio);
+  formData.append('audio', blob);
   console.log('uploadSlice:formData=<',formData,'>');
   var url = window.location.href;
   $.ajax({
     type : 'post',
     url : url,
     data : formData,
+    cache : false,
     contentType: false,
     processData: false,
-    dataType: "script",
     success : function(data) {
       // Success
       console.log(data);
@@ -25,11 +26,14 @@ function uploadSlice(audio,phoneme) {
 }
 
 
-function saveToFile(url,phoneme) {
+function saveToFile(chunks,phoneme) {
+  const blob = new Blob(chunks, { type: 'audio/webm' });
+  let urlBlob = window.URL.createObjectURL(blob);
+  console.log('saveToFile:urlBlob=<',urlBlob,'>');
   let a = document.createElement('a');
   document.body.appendChild(a);
   a.style = 'display: none';
-  a.href = url;
+  a.href = urlBlob;
   a.download = phoneme + '.test.webm';
   a.click();
   window.URL.revokeObjectURL(url);
