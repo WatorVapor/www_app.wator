0c15384adb7eba372c7d1f9e26a4e0bf2a05a91f
Wator Vapor <watorvapor@gmail.com>
2018-02-19 23:19:26 +0900

    Create recode_code.blade.php

---
+++ wator/resources/views/wai/recode_code.blade.php
@@ -0,0 +1,57 @@
+<script type="text/javascript">
+
+function onUpdateData(msg) {
+  console.log('onUpdateData:msg=<',msg,'>');
+}
+function onClickRecordBtn(elem) {
+  console.log('onClickRecordBtn:elem=<',elem,'>');
+  let root = elem.parentElement.parentElement;
+  //console.log('onClickRecordBtn:root=<',root,'>');
+  let phoneme = root.getElementsByTagName('h4')[0].textContent;
+  console.log('onClickRecordBtn:phoneme=<',phoneme,'>');
+  doAudioRecord(phoneme);
+}
+
+function doAudioRecord(phoneme) {
+  console.log('doAudioRecord:phoneme=<',phoneme,'>');
+  let mediaConstraints = { audio: true};
+  navigator.getUserMedia(mediaConstraints, function(stream) {
+      onMediaSuccess(stream,phoneme);
+    }, onMediaError);
+}
+function onMediaSuccess(stream,phoneme) {
+  let mr = new MediaRecorder(stream);
+  mr.mimeType = 'audio/webm'; // audio/webm or audio/ogg or audio/wav
+  let chunks4clip = {};
+  let chunks4analyze = [];
+  let clipIndex = 0;
+  mr.ondataavailable = function (e) {
+    //console.log('ondataavailable:e=<',e,'>');
+    //console.log('ondataavailable:phoneme=<',phoneme,'>');
+    //console.log('ondataavailable:window.URL=<',window.URL,'>');
+    chunks4clip[clipIndex++] = e.data;
+    chunks4analyze.push(e.data);
+   //saveToFile(chunks,phoneme);
+   //uploadSlice(chunks,phoneme);
+  }
+  mr.onerror = function (e) {
+    console.log('error:e=<',e,'>');
+  }
+  mr.onstart = function (e) {
+    console.log('onstart:e=<',e,'>');
+  }
+  mr.onstop = function (e) {
+    console.log('onstop:e=<',e,'>');
+    console.log('onstop:chunks4clip=<',chunks4clip,'>');
+    console.log('onstop:chunks4analyze=<',chunks4analyze,'>');
+  }
+  mr.start(50);
+  setTimeout(function(){
+    mr.stop();
+  },2000);
+}
+function onMediaError(e) {
+  console.error('media error e=<', e,'>');
+}
+
+</script>
