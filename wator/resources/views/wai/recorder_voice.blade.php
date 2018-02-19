6748399d98b4407d7f4e7e48e557a82265ce45c4
Wator Vapor <watorvapor@gmail.com>
2018-02-19 23:09:18 +0900

    Update recorder_voice.blade.php

--- wator/resources/views/wai/recorder_voice.blade.php
+++ wator/resources/views/wai/recorder_voice.blade.php
@@ -136,6 +136,63 @@
 
 <script src="/wator/wai/notify.js" type="text/javascript"></script>
 <script src="/wator/wai/upload.js" type="text/javascript"></script>
+<!--
 <script src="/wator/wai/record.js" type="text/javascript"></script>
+-->
+
+<script type="text/javascript">
+
+function onUpdateData(msg) {
+  console.log('onUpdateData:msg=<',msg,'>');
+}
+function onClickRecordBtn(elem) {
+  console.log('onClickRecordBtn:elem=<',elem,'>');
+  let root = elem.parentElement.parentElement;
+  //console.log('onClickRecordBtn:root=<',root,'>');
+  let phoneme = root.getElementsByTagName('h4')[0].textContent;
+  console.log('onClickRecordBtn:phoneme=<',phoneme,'>');
+  doAudioRecord(phoneme);
+}
+
+function doAudioRecord(phoneme) {
+  console.log('doAudioRecord:phoneme=<',phoneme,'>');
+  let mediaConstraints = { audio: true};
+  navigator.getUserMedia(mediaConstraints, function(stream) {
+      onMediaSuccess(stream,phoneme);
+    }, onMediaError);
+}
+function onMediaSuccess(stream,phoneme) {
+  let mr = new MediaRecorder(stream);
+  mr.mimeType = 'audio/webm'; // audio/webm or audio/ogg or audio/wav
+  let chunks = {};
+  let clipIndex = 0;
+  mr.ondataavailable = function (e) {
+    //console.log('ondataavailable:e=<',e,'>');
+    //console.log('ondataavailable:phoneme=<',phoneme,'>');
+    //console.log('ondataavailable:window.URL=<',window.URL,'>');
+    chunks[clipIndex++] = e.data;
+   //saveToFile(chunks,phoneme);
+   //uploadSlice(chunks,phoneme);
+  }
+  mr.onerror = function (e) {
+    console.log('error:e=<',e,'>');
+  }
+  mr.onstart = function (e) {
+    console.log('onstart:e=<',e,'>');
+  }
+  mr.onstop = function (e) {
+    console.log('onstop:e=<',e,'>');
+  }
+  mr.start(20);
+  setTimeout(function(){
+    mr.stop();
+  },1000);
+}
+function onMediaError(e) {
+  console.error('media error e=<', e,'>');
+}
+
+</script>
+
 
 @endsection
