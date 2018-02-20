9ed92fb07aacf2cf566d0ea5f995c512e34c4c84
Wator Vapor <watorvapor@gmail.com>
2018-02-20 23:56:48 +0900

    Update ja_phoneme.blade.php

--- wator/resources/views/wai/ja_phoneme.blade.php
+++ wator/resources/views/wai/ja_phoneme.blade.php
@@ -13,7 +13,7 @@
           <i class="material-icons " style="font-size:12px;color:red;">mic</i>
         </button>
         <div class="progress">
-          <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
+          <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 0%"></div>
         </div>
       </div>
     </div>
