f7299fe5bf4f04246bc71c66add26809eaff7703
Wator Vapor <watorvapor@gmail.com>
2018-02-20 23:12:37 +0900

    Update ja_phoneme.blade.php

--- wator/resources/views/wai/ja_phoneme.blade.php
+++ wator/resources/views/wai/ja_phoneme.blade.php
@@ -11,6 +11,9 @@
         <button type="submit" class="btn btn-dark rounded-circle" onclick="onClickRecordBtn(this)">
           <i class="material-icons " style="font-size:12px;color:red;">mic</i>
         </button>
+				<div class="progress">
+					<div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
+				</div>
       </div>
     </div>
 
