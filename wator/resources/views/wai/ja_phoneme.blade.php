e4dc52f62f685349e80e023b4132dfb702c0add6
Wator Vapor <watorvapor@gmail.com>
2018-02-20 22:32:06 +0900

    Update ja_phoneme.blade.php

--- wator/resources/views/wai/ja_phoneme.blade.php
+++ wator/resources/views/wai/ja_phoneme.blade.php
@@ -2,25 +2,25 @@
   <div class="card-body">
     <h1 class="card-title">{{ $phoneme }}</h1>
     <div class="row">
-      <div class="col-sm  m-0 p-0">
-        <button type="submit" class="btn btn-sm btn-dark rounded-circle">
+      <div class="col  m-0 p-0">
+        <button type="submit" class="btn btn-dark rounded-circle">
           <i class="material-icons " style="font-size:12px;color:green;">hearing</i>
         </button>
       </div>
-      <div class="col-sm  m-0 p-0">
-        <button type="submit" class="btn btn-sm btn-dark rounded-circle" onclick="onClickRecordBtn(this)">
+      <div class="col  m-0 p-0">
+        <button type="submit" class="btn btn-dark rounded-circle" onclick="onClickRecordBtn(this)">
           <i class="material-icons " style="font-size:12px;color:red;">mic</i>
         </button>
       </div>
     </div>
 
     <div class="row">
-      <div class="col-sm  m-0 p-0">
+      <div class="col  m-0 p-0">
         <audio controls="controls" id="wai-recoder-audio-standard">
           <source src="" type="audio/webm">
         </audio>
       </div>
-      <div class="col-sm  m-0 p-0">
+      <div class="col  m-0 p-0">
         <audio controls="controls" id="wai-recoder-audio-train">
           <source src="" type="audio/webm">
         </audio>
@@ -29,10 +29,10 @@
 
 
     <div class="row">
-      <div class="col-sm  m-0 p-0">
+      <div class="col  m-0 p-0">
         <canvas id="wai-recoder-canvas-standard"></canvas>
       </div>
-      <div class="col-sm  m-0 p-0">
+      <div class="col  m-0 p-0">
         <canvas id="wai-recoder-canvas-train"></canvas>
       </div>
     </div>
