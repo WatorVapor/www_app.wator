c1b5397d6b9d7bad2a02095c20bd86d54dc74363
Wator Vapor <watorvapor@gmail.com>
2018-02-20 23:33:25 +0900

    Update ja_phoneme.blade.php

--- wator/resources/views/wai/ja_phoneme.blade.php
+++ wator/resources/views/wai/ja_phoneme.blade.php
@@ -9,14 +9,11 @@
         </button>
       </div>
       <div class="col  m-0 p-0">
-        <div class="row">
-          <button type="submit" class="btn btn-dark rounded-circle" onclick="onClickRecordBtn(this)">
-            <i class="material-icons " style="font-size:12px;color:red;">mic</i>
-          </button>
-          <br/>
-          <div class="progress">
-            <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
-          </div>
+        <button type="submit" class="btn btn-dark rounded-circle" onclick="onClickRecordBtn(this)">
+          <i class="material-icons " style="font-size:12px;color:red;">mic</i>
+        </button>
+        <div class="progress">
+          <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
         </div>
       </div>
     </div>
