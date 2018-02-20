beb79b28c3fc9a584c89f9d79354b94785560e60
Wator Vapor <watorvapor@gmail.com>
2018-02-21 07:12:06 +0900

    Update recorder_voice.blade.php

--- wator/resources/views/wai/recorder_voice.blade.php
+++ wator/resources/views/wai/recorder_voice.blade.php
@@ -9,105 +9,6 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/10.0.0/css/bootstrap-slider.css" integrity="sha256-qkeO+BtgpANRnm6UfrclSLyB+QdfOK4qtspUK6qpnGk=" crossorigin="anonymous" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/10.0.0/bootstrap-slider.js" integrity="sha256-CMbY8O/nsUqZQgyh7wMVktFXm31lLdWlCkl86V2eSCg=" crossorigin="anonymous"></script>
 
-<!--
-<div class="row justify-content-center">
-  <div class="col-lg-10 ">
-    <form action="#" method="post" enctype="multipart/form-data">
-      {{ csrf_field() }}
-      <input type="text" name="phoneme" id="upload-form-phoneme">
-      <input type="text" name="token" id="upload-form-token">
-      <input type="file" name="audio" id="upload-form-audio">
-      <input type="submit" value="Upload Image" name="submit">
-    </form>
-  </div>
-</div>
--->
-
-
-<!--
-<div class="row justify-content-center">
-  <div class="col-lg-10 ">
-    <div class="card card-default">
-      <div class="card-header text-center">
-        {{ trans('wai_record_voice.pinyin') }}
-      </div>
-      <div class="card-body">
-      </div>
-    </div>
-  </div>
-</div>
--->
-
-<!--
-
-<div class="row justify-content-center">
-  <div class="col-lg-10 ">
-    <div class="card card-default">
-      <div class="card-header text-center">
-        {{ trans('wai_record_voice.hirakana') }}
-      </div>
-      <div class="card-body">
-        <div class="row justify-content-center">
-          <div class="col">
-            @include('wai.ja_phoneme',['phoneme' => 'あ'])
-          </div>
-          <div class="col">
-            @include('wai.ja_phoneme',['phoneme' => 'い'])
-          </div>
-          <div class="col">
-            @include('wai.ja_phoneme',['phoneme' => 'う'])
-          </div>
-          <div class="col">
-            @include('wai.ja_phoneme',['phoneme' => 'え'])
-          </div>
-          <div class="col">
-            @include('wai.ja_phoneme',['phoneme' => 'お'])
-          </div>
-        </div>
-
-        <div class="row justify-content-center">
-          <div class="col">
-            @include('wai.ja_phoneme',['phoneme' => 'か'])
-          </div>
-          <div class="col">
-            @include('wai.ja_phoneme',['phoneme' => 'き'])
-          </div>
-          <div class="col">
-            @include('wai.ja_phoneme',['phoneme' => 'く'])
-          </div>
-          <div class="col">
-            @include('wai.ja_phoneme',['phoneme' => 'け'])
-          </div>
-          <div class="col">
-            @include('wai.ja_phoneme',['phoneme' => 'こ'])
-          </div>
-        </div>
-
-        <div class="row justify-content-center">
-          <div class="col">
-            @include('wai.ja_phoneme',['phoneme' => 'が'])
-          </div>
-          <div class="col">
-            @include('wai.ja_phoneme',['phoneme' => 'ぎ'])
-          </div>
-          <div class="col">
-            @include('wai.ja_phoneme',['phoneme' => 'ぐ'])
-          </div>
-          <div class="col">
-            @include('wai.ja_phoneme',['phoneme' => 'げ'])
-          </div>
-          <div class="col">
-            @include('wai.ja_phoneme',['phoneme' => 'ご'])
-          </div>
-        </div>
-
-      </div>
-    </div>
-  </div>
-</div>
-
--->
-
 
 <div class="row justify-content-center">
   <div class="col-lg-10 ">
@@ -125,7 +26,6 @@
     </div>
   </div>
 </div>
-
 
 <div class="row justify-content-center">
   <div class="col-lg-10 ">
