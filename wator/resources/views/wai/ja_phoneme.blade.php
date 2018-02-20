5b05479bff9aa990278bab156dd57f26a4ee8e85
Wator Vapor <watorvapor@gmail.com>
2018-02-20 22:31:01 +0900

    Update ja_phoneme.blade.php

--- wator/resources/views/wai/ja_phoneme.blade.php
+++ wator/resources/views/wai/ja_phoneme.blade.php
@@ -8,9 +8,6 @@
         </button>
       </div>
       <div class="col-sm  m-0 p-0">
-        <h4 class="rounded-circle">{{ $phoneme }}</h4>
-      </div>
-      <div class="col-sm  m-0 p-0">
         <button type="submit" class="btn btn-sm btn-dark rounded-circle" onclick="onClickRecordBtn(this)">
           <i class="material-icons " style="font-size:12px;color:red;">mic</i>
         </button>
