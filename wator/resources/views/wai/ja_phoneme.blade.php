12a999ea15c411206b28a37196c68dc8a2b8f189
Wator Vapor <watorvapor@gmail.com>
2018-02-21 22:10:32 +0900

    Update ja_phoneme.blade.php

--- wator/resources/views/wai/ja_phoneme.blade.php
+++ wator/resources/views/wai/ja_phoneme.blade.php
@@ -1,6 +1,7 @@
 <div class="card card-default text-center">
   <div class="card-body">
     <h1 class="card-title" style="font-size:96px;">{{ $phoneme }}</h1>
+    <h1 class="card-title" style="font-size:36px;">{{ $phoneme_help }}</h1>
     
     <div class="row">
       <div class="col  m-0 p-0">
