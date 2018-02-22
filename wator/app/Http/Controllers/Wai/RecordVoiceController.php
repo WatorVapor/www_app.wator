c2fdba2892ebce871b2755ca70d997cd9f8a0b42
Wator Vapor <watorvapor@gmail.com>
2018-02-22 21:44:05 +0900

    Update RecordVoiceController.php

--- wator/app/Http/Controllers/Wai/RecordVoiceController.php
+++ wator/app/Http/Controllers/Wai/RecordVoiceController.php
@@ -8,7 +8,6 @@ use Illuminate\Http\Request;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\Redis;
-use Wator\Http\Controllers\Wai\RecordVoicePhoneme;
 
 
 class RecordVoiceController extends Controller
