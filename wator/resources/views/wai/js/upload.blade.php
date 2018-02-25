e55ca8568b5d7da92af1bb5ac94b56765f486991
Wator Vapor <watorvapor@gmail.com>
2018-02-25 12:16:58 +0900

    Create upload.blade.php

---
+++ wator/resources/views/wai/js/upload.blade.php
@@ -0,0 +1,41 @@
+<script src="https://unpkg.com/ipfs-api@9.0.0/dist/index.js" integrity="sha384-5bXRcW9kyxxnSMbOoHzraqa7Z0PQWIao+cgeg327zit1hz5LZCEbIMx/LWKPReuB" crossorigin="anonymous"></script>
+<script type="text/javascript">
+function clipPhoneme(data,waveEnergyMax,waveEnergyMaxIndex,sample) {
+  console.log('clipPhoneme waveEnergyMax=<',waveEnergyMax,'>');
+  console.log('clipPhoneme waveEnergyMaxIndex=<',waveEnergyMaxIndex,'>');
+  let clipWindowSize = ClipDurationInSec * sample;
+  let buffer = audioCtx.createBuffer(1, ClipDurationInSec *sample , sample);
+  let audioData = buffer.getChannelData(0);
+  let startClipBuffer = waveEnergyMaxIndex - clipWindowSize;
+  for(var i = 0; i < audioData.length; i++) {
+      audioData[i] = data[i + startClipBuffer];
+  }
+  let nodeSrc = audioCtx.createBufferSource();
+  nodeSrc.buffer = buffer;
+  let dest = audioCtx.createMediaStreamDestination();
+  let mediaRecorder = new MediaRecorder(dest.stream);
+  mediaRecorder.mimeType = 'audio/webm';
+  nodeSrc.connect(audioCtx.destination);
+  console.log('clipPhoneme nodeSrc=<',nodeSrc,'>');
+  let chunks = [];
+  mediaRecorder.ondataavailable = function(evt) {
+    chunks.push(evt.data);
+    console.log('clipPhoneme evt=<',evt,'>');
+  };
+  mediaRecorder.onstop = function(evt) {
+    let blobClip = new Blob(chunks, { 'type' : 'audio/webm' });
+    console.log('clipPhoneme blobClip=<',blobClip,'>');
+    let urlBlob = window.URL.createObjectURL(blobClip);
+    console.log('clipPhoneme urlBlob=<',urlBlob,'>');
+    let audioElem = document.getElementById('wai-recoder-audio-train');
+    audioElem.src = urlBlob;
+    $( '#wai-recoder-clip-operator' ).removeClass( 'd-none' );
+  };
+  nodeSrc.connect(dest);
+  mediaRecorder.start();
+  nodeSrc.start(0);
+  setTimeout(function(){
+    mediaRecorder.stop();
+  },1000);
+}
+</script>
