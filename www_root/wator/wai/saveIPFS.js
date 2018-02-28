b25f11404800cd28773a858d33914e676abf5119
Wator Vapor <watorvapor@gmail.com>
2018-02-28 20:59:59 +0900

    Update saveIPFS.js

--- www_root/wator/wai/saveIPFS.js
+++ www_root/wator/wai/saveIPFS.js
@@ -1,9 +1,14 @@
 //const Buffer = window.IpfsApi().Buffer;
 //let ipfs = window.IpfsApi({host:'www.wator.xyz', port:'443', protocol: 'https'});
+onmessage = function (evt) {
+  postMessage('saveIPFS::evt=<',evt,'>');
+};
+
+/*
 ipfs.id(function (err, identity) {
   if (err) {
     throw err
   }
   console.log('ipfs.id:identity=<',identity,'>');
 });
-
+*/
