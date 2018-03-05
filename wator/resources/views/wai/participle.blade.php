bc9f328a8be10f200b379884666ce9205d0a782a
Wator Vapor <watorvapor@gmail.com>
2018-03-05 22:58:32 +0900

    Update participle.blade.php

--- wator/resources/views/wai/participle.blade.php
+++ wator/resources/views/wai/participle.blade.php
@@ -58,7 +58,7 @@
         <source src="" type="audio/webm">
       </audio>
       <button type="submit" class="btn btn-lg btn-outline-dark rounded-circle" style="height:96px;width:96px;">
-        <i class="material-icons " style="font-size:60px;color:green;">hearing</i>
+        <i class="material-icons " style="font-size:60px;color:green;">volume_up</i>
       </button>
     </div>
   </div>
