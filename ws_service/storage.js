2b957261440b6d1de1f6fa266a6b71108d31a159
Wator Vapor <watorvapor@gmail.com>
2018-03-07 22:49:32 +0900

    Update storage.js

--- ws_service/storage.js
+++ ws_service/storage.js
@@ -2,8 +2,11 @@ const WebSocket = require('ws');
 const config = {host:'127.0.01', port: 19083 }
 const wss = new WebSocket.Server(config);
 wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
+  ws.on('message', function (message) {
     console.log('received: message=<', message,'>');
+  });
+  ws.on('error', function (evt) {
+    console.log('error: evt=<', evt,'>');
   });
 });
 
