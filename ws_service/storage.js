ecdb8e2650f6567456ba4feb88e6a7325ec7a932
Wator Vapor <watorvapor@gmail.com>
2018-03-07 21:41:28 +0900

    Update storage.js

--- ws_service/storage.js
+++ ws_service/storage.js
@@ -6,3 +6,9 @@ wss.on('connection', function connection(ws) {
     console.log('received: message=<', message,'>');
   });
 });
+
+var ipfsAPI = require('ipfs-api');
+//var ipfs = ipfsAPI('/ip4/127.0.0.1/tcp/5001');
+var ipfs = ipfsAPI('master.ipfs.wator.xyz', '5001', {protocol: 'http'});
+console.log('ipfs=<',ipfs,'>');
+
