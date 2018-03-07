21d2a6b9e1eecb93e57b1c6d77559571faf46d98
Wator Vapor <watorvapor@gmail.com>
2018-03-07 21:37:36 +0900

    Update storage.js

--- ws_service/storage.js
+++ ws_service/storage.js
@@ -1,40 +1,8 @@
 const WebSocket = require('ws');
-const config = {host:'127.0.01', port: 19082 }
+const config = {host:'127.0.01', port: 19083 }
 const wss = new WebSocket.Server(config);
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-    //console.log('received: message=<', message,'>');
-    let jsonMsg = JSON.parse(message);
-    if(jsonMsg) {
-      if(jsonMsg.channel && jsonMsg.msg) {
-        setTimeout(function() {
-          if(pub) {
-            pub.publish(jsonMsg.channel,JSON.stringify(jsonMsg.msg));
-          }
-        },1);
-      }
-    }
+    console.log('received: message=<', message,'>');
   });
 });
-
-const redis = require("redis");
-const redisOption = {
-  port:6379,
-  host:'master.redis.wator.xyz',
-  family:'IPv6'
-};
-const sub = redis.createClient(redisOption);
-sub.on('message', function(channel, message) {
-  //console.log('sub::channel=<',channel,'>');
-  //console.log('sub::message=<',message,'>');
-  //console.log('sub::wss=<',wss,'>');
-  wss.clients.forEach(function each(client) {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(message);
-    }
-  })
-});
-sub.subscribe('wai.train.response');
-
-const pub  = redis.createClient(redisOption);
-
