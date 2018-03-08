6e059f5c0a79404e68dccc8d7ed65f3c2b2aa3b4
Wator Vapor <watorvapor@gmail.com>
2018-03-08 20:43:13 +0900

    Update notify.js

--- ws_service/notify.js
+++ ws_service/notify.js
@@ -2,6 +2,7 @@ const WebSocket = require('ws');
 const config = {host:'127.0.01', port: 19082 }
 const wss = new WebSocket.Server(config);
 wss.on('connection', function connection(ws) {
+    
   ws.on('message', function incoming(message) {
     //console.log('received: message=<', message,'>');
     let jsonMsg = JSON.parse(message);
@@ -15,7 +16,22 @@ wss.on('connection', function connection(ws) {
       }
     }
   });
+    
+  ws.on('error', function (evt) {
+    console.log('error: evt=<', evt,'>');
+  });
 });
+
+const interval = setInterval(function ping() {
+  wss.clients.forEach(function each(ws) {
+    if (ws.isAlive === false) {
+      return ws.terminate();
+    }
+    ws.isAlive = false;
+    ws.ping(noop);
+  });
+}, 10000);
+
 
 const redis = require("redis");
 const redisOption = {
