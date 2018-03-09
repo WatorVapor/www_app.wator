e9fbc5931ff3cbbe0f820412fb973da34a42bcb5
Wator Vapor <watorvapor@gmail.com>
2018-03-10 00:32:51 +0900

    Update participle_tts.blade.php

--- wator/resources/views/wai/js/participle_tts.blade.php
+++ wator/resources/views/wai/js/participle_tts.blade.php
@@ -2,6 +2,9 @@
 
 function onClickTTS(elem) {
   console.log('onClickTTS:elem=<',elem,'>');
+  let audioElem = document.getElementById('ui-update-all-words-tts');
+  console.log('onClickTTS:audioElem=<',audioElem,'>');
+  audioElem.play();
 }
 
 </script>
