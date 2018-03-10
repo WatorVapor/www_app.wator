4e6c964207d126503b2fe50825c5f7baf9ee7a5d
Wator Vapor <watorvapor@gmail.com>
2018-03-10 22:15:56 +0900

    Update participle_tts.blade.php

--- wator/resources/views/wai/js/participle_tts.blade.php
+++ wator/resources/views/wai/js/participle_tts.blade.php
@@ -24,6 +24,7 @@ function createClipsElement(clip) {
   audio.src = 'https://ipfs.io/ipfs/' + clip;
   audio.className = 'ui-update-tts-one-clip';
   console.log('createClipsElement:audio=<',audio,'>');
+  audio.load();
   return audio;
 }
 
