8ae929295e953239a03e63a251be0a912e0b85d3
Wator Vapor <watorvapor@gmail.com>
2018-03-10 22:18:57 +0900

    Update participle_tts.blade.php

--- wator/resources/views/wai/js/participle_tts.blade.php
+++ wator/resources/views/wai/js/participle_tts.blade.php
@@ -1,29 +1,29 @@
 <script type="text/javascript">
 
 function onClickTTS(elem) {
-  console.log('onClickTTS:elem=<',elem,'>');
+  //console.log('onClickTTS:elem=<',elem,'>');
   let audioList = document.getElementsByClassName('ui-update-tts-one-clip');
-  console.log('onClickTTS:audioList=<',audioList,'>');
+  //console.log('onClickTTS:audioList=<',audioList,'>');
   doPlayTTS(audioList,0);
 }
 
 function createTTS(tts) {
-  console.log('createTTS:tts=<',tts,'>');
+  //console.log('createTTS:tts=<',tts,'>');
   let clipsElem = document.getElementById('ui-update-tts-all-clips');
-  console.log('onClickTTS:clipsElem=<',clipsElem,'>');
+  //console.log('onClickTTS:clipsElem=<',clipsElem,'>');
   for(let index in tts) {
     let clip = tts[index];
-    console.log('createTTS:clip=<',clip,'>');
+    //console.log('createTTS:clip=<',clip,'>');
     let audioElem = createClipsElement(clip);
     clipsElem.appendChild(audioElem);
   }
 }
 function createClipsElement(clip) {
-  console.log('createClipsElement:clip=<',clip,'>');
+  //console.log('createClipsElement:clip=<',clip,'>');
   let audio = document.createElement('audio');
   audio.src = 'https://ipfs.io/ipfs/' + clip;
   audio.className = 'ui-update-tts-one-clip';
-  console.log('createClipsElement:audio=<',audio,'>');
+  //console.log('createClipsElement:audio=<',audio,'>');
   audio.muted = true;
   audio.play();
   return audio;
@@ -31,11 +31,11 @@ function createClipsElement(clip) {
 
 
 function doPlayTTS(playList,index) {
-  console.log('doPlayTTS:playList=<',playList,'>');
+  //console.log('doPlayTTS:playList=<',playList,'>');
   if(playList.length > index) {
     let audio = playList[index];
     audio.muted = false;
-    console.log('doPlayTTS:audio=<',audio,'>');
+    //console.log('doPlayTTS:audio=<',audio,'>');
     audio.addEventListener("ended", function(){
       doPlayTTS(playList,index+1);
     });
