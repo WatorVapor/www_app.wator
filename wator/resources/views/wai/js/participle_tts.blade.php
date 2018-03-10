2b201c52fef5a34dab0e3b4e42f06f1537e169ff
Wator Vapor <watorvapor@gmail.com>
2018-03-10 09:17:08 +0900

    Update participle_tts.blade.php

--- wator/resources/views/wai/js/participle_tts.blade.php
+++ wator/resources/views/wai/js/participle_tts.blade.php
@@ -7,4 +7,17 @@ function onClickTTS(elem) {
   audioElem.play();
 }
 
+function createTTS(tts) {
+  console.log('createTTS:tts=<',tts,'>');
+  let clipsElem = document.getElementById('ui-update-all-clips');
+  console.log('onClickTTS:clipsElem=<',clipsElem,'>');
+  for(let clip in tts) {
+    console.log('createTTS:clip=<',clip,'>');
+    createClipsElement(clip);
+  }
+}
+function createClipsElement(clip) {
+  console.log('createClipsElement:clip=<',clip,'>');
+}
+
 </script>
