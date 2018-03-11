129862cb013346ed8b0fea6b22579975de88e9fa
Wator Vapor <watorvapor@gmail.com>
2018-03-11 22:05:39 +0900

    Update participle_tts.blade.php

--- wator/resources/views/wai/js/participle_tts.blade.php
+++ wator/resources/views/wai/js/participle_tts.blade.php
@@ -39,7 +39,7 @@ function doPlayTTS(playList,index) {
     audio.addEventListener("ended", function(){
       doPlayTTS(playList,index+1);
     });
-    audio.playbackRate = {{ $tts_speed }};
+    audio.playbackRate = 2.0;
     audio.play();
   }
 }
