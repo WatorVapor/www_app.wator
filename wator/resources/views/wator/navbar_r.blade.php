6329a12e4390176b6780fc231a96ccf530474f55
Wator Vapor <watorvapor@gmail.com>
2018-03-16 23:33:24 +0900

    Update navbar_r.blade.php

--- wator/resources/views/wator/navbar_r.blade.php
+++ wator/resources/views/wator/navbar_r.blade.php
@@ -47,7 +47,7 @@
   $user .= '<a class="btn btn-lg btn-info mr-lg-5" href="/rsaauth/profile" role="button">';
   $user .= '<i class="material-icons md-48">account_box</i>';
   $user .= '</a>';
-  $user .= '<a class="btn btn-lg btn-danger" href="/rsaauth/profile" role="button">';
+  $user .= '<a class="btn btn-lg btn-danger" href="/rsaauth/logout" role="button">';
   $user .= '<i class="material-icons md-48">exit_to_app</i>';
   $user .= '</a>';
 ?>
