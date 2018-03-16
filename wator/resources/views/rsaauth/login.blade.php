63b2642e0229e2ab459218208bcb47979e860c08
Wator Vapor <watorvapor@gmail.com>
2018-03-17 00:41:48 +0900

    Update login.blade.php

--- wator/resources/views/rsaauth/login.blade.php
+++ wator/resources/views/rsaauth/login.blade.php
@@ -16,12 +16,9 @@
       <div class="card-body">
         <form  id="rsaauth_login_form" class="mt-2 mb-2" method="POST" action="/rsaauth/login">
           {{ csrf_field() }}
-          <button type="submit" class="btn btn-success">
-            <spam>{{trans('rsaauth_login.login')}}</span><i class="material-icons " style="color:green;">done</i>
-          </button>
           <div class="form-group text-left">
             <span class="input-group-text" for="rsa.login.accessToken">{{trans('rsaauth_login.accessToken')}}</span>
-            <input type="text" id="rsa.login.accessToken" name="accessToken" class="form-control" disabled cols="40" rows="3"></input>
+            <textarea type="text" id="rsa.login.accessToken" name="accessToken" class="form-control" disabled cols="40" rows="3"></textarea>
           </div>
           <div class="form-group text-left">
              <span class="input-group-text" for="rsa.login.access">{{trans('rsaauth_login.access')}}</span>
@@ -31,6 +28,9 @@
             <span class="input-group-text"for="rsa.login.signature">{{trans('rsaauth_login.signature')}}</span>
             <textarea type="text" id="rsa.login.signature" name="signature" class="form-control" disabled cols="40" rows="14">{{ $RsaLoginAccessKey }}</textarea>
           </div>
+          <button type="submit" class="btn btn-success">
+            <spam>{{trans('rsaauth_login.login')}}</span><i class="material-icons " style="color:green;">done</i>
+          </button>
         </form>
       </div>
     </div>
