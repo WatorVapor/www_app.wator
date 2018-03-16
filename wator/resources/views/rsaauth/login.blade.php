838ca7325f8bf21b63b36d4c715565d84920d578
Wator Vapor <watorvapor@gmail.com>
2018-03-17 00:01:56 +0900

    Update login.blade.php

--- wator/resources/views/rsaauth/login.blade.php
+++ wator/resources/views/rsaauth/login.blade.php
@@ -20,11 +20,11 @@
             <span class="input-group-text" for="rsa.login.accessToken">{{trans('rsaauth_login.accessToken')}}</label>
             <textarea id="rsa.login.accessToken" name="accessToken" class="form-control" cols="40" rows="3" aria-describedby="basic-addon1"></textarea>
           </div>
-          <div class="form-group">
+          <div class="form-group text-left">
              <span class="input-group-text" for="rsa.login.access">{{trans('rsaauth_login.access')}}</span>
              <textarea id="rsa.login.access" name="access" class="form-control" cols="40" rows="3" aria-describedby="basic-addon1" value="{{ $RsaLoginAccessKey }}"></textarea>
           </div>
-          <div class="form-group">
+          <div class="form-group text-left">
             <span class="input-group-text"for="rsa.login.signature">{{trans('rsaauth_login.signature')}}</span>
             <textarea id="rsa.login.signature" name="signature" class="form-control" cols="40" rows="3" aria-describedby="basic-addon1" value="{{ $RsaLoginAccessKey }}"></textarea>
           </div>
