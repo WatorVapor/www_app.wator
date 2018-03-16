1b21cbda67397cc130181e1d5903b579b8720542
Wator Vapor <watorvapor@gmail.com>
2018-03-16 23:38:55 +0900

    Update LogoutController.php

--- wator/app/Http/Controllers/RsaAuth/LogoutController.php
+++ wator/app/Http/Controllers/RsaAuth/LogoutController.php
@@ -24,15 +24,7 @@ class LogoutController extends Controller
         $request->session()->forget('account.rsa.login.name');
         $request->session()->forget('account.rsa.login.token');
         $request->session()->forget('account.rsa.login.access');
-        $strRequest = uniqid();
-        //var_dump($strRequest);
-        $id  = hash('sha512',$strRequest);
-        $request->session()->put('account.rsa.login.access',$id);
-        $autoFlags = 'false';
-        if($auto == 'auto') {
-            $autoFlags = 'true';
-        }
-        return view('rsaauth.login',['RsaLoginAccessKey'=>$id,'auto'=> $autoFlags]);
+        return redirect()->back();
     }
     /**
      * Show the form for creating a new resource.
@@ -51,62 +43,12 @@ class LogoutController extends Controller
      */
     public function store(Request $request)
     {
-        try {
-            $accessToken = $request->input('accessToken');
-            //var_dump($accessToken);
-            $access = $request->input('access');
-            //var_dump($access);
-            $signature = $request->input('signature');
-            //var_dump($signature);
-            if(!isset($accessToken) || !isset($access) || !isset($signature)) {
-                $bodyContent = $request->getContent();
-                $bodyJson = json_decode($bodyContent);
-                $accessToken = $bodyJson->accessToken;
-                $access = $bodyJson->access;
-                $signature = $bodyJson->signature;
-            }
-            
-            $keyPath = $this->keyRoot_ . $accessToken . ''. '/pubKey.pem';
-            //var_dump($keyPath);
-            $fp = fopen($keyPath, 'r');
-            $pubKeyMem = fread($fp, 8192);
-            fclose($fp);
-            $pubkeyid = openssl_pkey_get_public($pubKeyMem);
-            //var_dump($pubkeyid);
-            $ok = openssl_verify($access,hex2bin($signature) , $pubkeyid,"sha256");
-            //var_dump($ok);
-            if ($ok == 1) {
-                $request->session()->put('account.rsa.login.status','success');
-                $request->session()->put('account.rsa.login.token',$accessToken);
-                $request->session()->put('account.rsa.login.access',$access);
-                $profilePath = $this->keyRoot_ . $accessToken . ''. '/profile';
-                if (file_exists($profilePath)) {
-                    $profileStr = file_get_contents($profilePath);
-                    $profileJson = json_decode($profileStr, true);
-                    if(isset($profileJson['user_name'])) {
-                        $request->session()->put('account.rsa.login.name',$profileJson['user_name']);
-                    } else {
-                        $request->session()->forget('account.rsa.login.name');
-                    }
-                } else {
-                    $request->session()->forget('account.rsa.login.name');
-                }                    
-            } else {
-                $request->session()->put('account.rsa.login.status','failure');
-                $request->session()->put('account.rsa.login.token',$accessToken);
-                $request->session()->put('account.rsa.login.access',$access);
-                return response()->json(['status'=>'failure']);
-            }
-        } catch (\Exception $e) {
-            //var_dump($e);
-            $request->session()->put('account.rsa.login.status','failure');
-            $request->session()->forget('account.rsa.login.token');
-            $request->session()->forget('account.rsa.login.access');
-            $request->session()->forget('account.rsa.login.name');
-            return response()->json(['status'=>'failure']);
-       }
-       return response()->json(['status'=>'success']);
-     }
+        $request->session()->forget('account.rsa.login.status');
+        $request->session()->forget('account.rsa.login.name');
+        $request->session()->forget('account.rsa.login.token');
+        $request->session()->forget('account.rsa.login.access');
+        return redirect()->back();
+    }
     /**
      * Display the specified resource.
      *
