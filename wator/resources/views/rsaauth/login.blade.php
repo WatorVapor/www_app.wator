4cc9f32c5fec2deb1d81cef07d1c99e67d57c272
Wator Vapor <watorvapor@gmail.com>
2018-03-17 00:29:27 +0900

    Update login.blade.php

--- wator/resources/views/rsaauth/login.blade.php
+++ wator/resources/views/rsaauth/login.blade.php
@@ -21,15 +21,15 @@
           </button>
           <div class="form-group text-left">
             <span class="input-group-text" for="rsa.login.accessToken">{{trans('rsaauth_login.accessToken')}}</label>
-            <textarea id="rsa.login.accessToken" name="accessToken" class="form-control" disabled cols="40" rows="3" aria-describedby="basic-addon1"></textarea>
+            <textarea id="rsa.login.accessToken" name="accessToken" class="form-control" disabled cols="40" rows="3"></textarea>
           </div>
           <div class="form-group text-left">
              <span class="input-group-text" for="rsa.login.access">{{trans('rsaauth_login.access')}}</span>
-             <textarea id="rsa.login.access" name="access" class="form-control" disabled cols="40" rows="3" aria-describedby="basic-addon1" value="{{ $RsaLoginAccessKey }}">{{ $RsaLoginAccessKey }}</textarea>
+             <textarea id="rsa.login.access" name="access" class="form-control" disabled cols="40" rows="3">{{ $RsaLoginAccessKey }}</textarea>
           </div>
           <div class="form-group text-left">
             <span class="input-group-text"for="rsa.login.signature">{{trans('rsaauth_login.signature')}}</span>
-            <textarea id="rsa.login.signature" name="signature" class="form-control" disabled cols="40" rows="14" aria-describedby="basic-addon1" value="{{ $RsaLoginAccessKey }}">{{ $RsaLoginAccessKey }}</textarea>
+            <textarea id="rsa.login.signature" name="signature" class="form-control" disabled cols="40" rows="14">{{ $RsaLoginAccessKey }}</textarea>
           </div>
         </form>
       </div>
