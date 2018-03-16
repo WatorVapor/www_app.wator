eb9e6c78d1df0ccff1d084633bc1c9d47e08dbd3
Wator Vapor <watorvapor@gmail.com>
2018-03-17 01:26:42 +0900

    Update import.blade.php

--- wator/resources/views/rsaauth/import.blade.php
+++ wator/resources/views/rsaauth/import.blade.php
@@ -9,9 +9,11 @@
 
 <div class="row justify-content-center">
   <div class="col-8 bg-info">
-    <textarea id="importKey" class="w-100 text-left small" rows="40" data-toggle="tooltip" data-placement="top" title="paste public key and public key">
-      -----BEGIN PRIVATE KEY-----
-      -----BEGIN PUBLIC KEY-----
+    <textarea id="importKey" 
+      class="w-100 text-left small" rows="40" 
+      data-toggle="tooltip" data-placement="top" title="paste public key and public key"
+      placeholder="-----BEGIN PRIVATE KEY----- -----BEGIN PUBLIC KEY-----"
+      >
     </textarea>
    </div>
 </div>
@@ -19,7 +21,6 @@
 <script>
   $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
-    document.getElementById("importKey").select();
   });
 </script>
 @endsection
