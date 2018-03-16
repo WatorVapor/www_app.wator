f33e86100a917a5ff49f18e213f3a77dd0364f7b
Wator Vapor <watorvapor@gmail.com>
2018-03-17 01:15:49 +0900

    Update import.blade.php

--- wator/resources/views/rsaauth/import.blade.php
+++ wator/resources/views/rsaauth/import.blade.php
@@ -7,7 +7,7 @@
 
 @section('content')
 
-<div class="row justify-content-md-center">
+<div class="row justify-content-center">
   <div class="col-8 bg-info">
     <textarea id="privateKey" class="w-100 text-left small" rows="40">
       -----BEGIN PRIVATE KEY-----
