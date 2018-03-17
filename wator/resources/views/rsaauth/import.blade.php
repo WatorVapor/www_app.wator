8f405d6a0a88ad71636543b5f86442af2bd974ba
Wator Vapor <watorvapor@gmail.com>
2018-03-17 13:31:08 +0900

    Update import.blade.php

--- wator/resources/views/rsaauth/import.blade.php
+++ wator/resources/views/rsaauth/import.blade.php
@@ -19,7 +19,7 @@
 
 <div class="row justify-content-center" id="import-key-verify">
   <div class="col-2">
-    <button class="btn btn-success btn-block" onclick="onImportKey(this)">
+    <button class="btn btn-info btn-block" onclick="onImportKey(this)">
       <spam>{{trans('rsaauth_login.check')}}</span><i class="material-icons " style="color:green;">check_circle</i>
     </button>
   </div>
@@ -29,15 +29,15 @@
 <div class="row justify-content-center d-none" id="import-key-save">
   <div class="col-2">
     <button class="btn btn-success btn-block" onclick="onImportKey(this)">
-      <spam>{{trans('rsaauth_login.apply')}}</span><i class="material-icons " style="color:green;">done</i>
+      <spam>{{trans('rsaauth_login.apply')}}</span><i class="material-icons " style="color:orange;">done</i>
     </button>
   </div>
 </div>
 
 <div class="row justify-content-center d-none" id="import-key-discard">
   <div class="col-2">
-    <button class="btn btn-success btn-block" onclick="ondiscardKey(this)">
-      <spam>{{trans('rsaauth_login.discard')}}</span><i class="material-icons " style="color:green;">cancel</i>
+    <button class="btn btn-daugh btn-block" onclick="ondiscardKey(this)">
+      <spam>{{trans('rsaauth_login.discard')}}</span><i class="material-icons " style="color:red;">cancel</i>
     </button>
   </div>
 </div>
