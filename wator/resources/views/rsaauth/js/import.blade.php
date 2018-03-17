1bf8831d6a3defb809f3e6a62a8138f770237d4c
Wator Vapor <watorvapor@gmail.com>
2018-03-17 11:55:39 +0900

    Update import.blade.php

--- wator/resources/views/rsaauth/js/import.blade.php
+++ wator/resources/views/rsaauth/js/import.blade.php
@@ -3,7 +3,7 @@ function onImportKey(elem) {
   console.log('onImportKey:elem=<',elem,'>');
   let elemKey = document.getElementById("import-key-value");
   console.log('onImportKey:elemKey=<',elemKey,'>');
-  let keyStr = elemKey.valure;
+  let keyStr = elemKey.value;
   console.log('onImportKey:keyStr=<',keyStr,'>');
 }
 </script>
