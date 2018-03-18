4fdc5dd001f12e15424b448ff43602b5b3cd7da5
Wator Vapor <watorvapor@gmail.com>
2018-03-18 22:37:57 +0900

    Update upload.js

--- www_root/wator/rsaauth/js/upload.js
+++ www_root/wator/rsaauth/js/upload.js
@@ -1,5 +1,5 @@
 var RSAAuth = RSAAuth || {};
-RSAAuth.upPubKey = function() {
+RSAAuth.upPubKey = function(cb) {
   var pubKey = localStorage.getItem('auth.rsa.key.public');
   var token = localStorage.getItem('auth.rsa.token');
   if (pubKey && token) {
@@ -17,10 +17,16 @@ RSAAuth.upPubKey = function() {
       success : function(data) {
         // Success
         console.log(data);
+        if(typeof cb === 'function') {
+          cb(data);
+        }
       },
       error : function(data) {
         // Error
         console.log(data);
+        if(typeof cb === 'function') {
+          cb(data);
+        }
       }
     });
   } else {
