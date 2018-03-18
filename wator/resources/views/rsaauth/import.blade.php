f55c77d4c167706416c4a31186795b2c0c38a5b0
Wator Vapor <watorvapor@gmail.com>
2018-03-18 22:13:31 +0900

    Update import.blade.php

--- wator/resources/views/rsaauth/import.blade.php
+++ wator/resources/views/rsaauth/import.blade.php
@@ -20,7 +20,7 @@
 <div class="row justify-content-center" id="import-key-verify">
   <div class="col-2">
     <button class="btn btn-info btn-block" onclick="onImportKey(this)">
-      <spam>{{trans('rsaauth_login.check')}}</span><i class="material-icons " style="color:orange;">check_circle</i>
+      <spam>{{trans('rsaauth_import.check')}}</span><i class="material-icons " style="color:orange;">check_circle</i>
     </button>
   </div>
 </div>
@@ -29,7 +29,7 @@
 <div class="row justify-content-center d-none" id="import-key-save">
   <div class="col-2">
     <button class="btn btn-success btn-block" onclick="onImportKey(this)">
-      <spam>{{trans('rsaauth_login.apply')}}</span><i class="material-icons " style="color:green;">done</i>
+      <spam>{{trans('rsaauth_import.apply')}}</span><i class="material-icons " style="color:green;">done</i>
     </button>
   </div>
 </div>
@@ -37,7 +37,7 @@
 <div class="row justify-content-center d-none" id="import-key-discard">
   <div class="col-2">
     <button class="btn btn-danger btn-block" onclick="ondiscardKey(this)">
-      <spam>{{trans('rsaauth_login.discard')}}</span><i class="material-icons " style="color:red;">cancel</i>
+      <spam>{{trans('rsaauth_import.discard')}}</span><i class="material-icons " style="color:red;">cancel</i>
     </button>
   </div>
 </div>
