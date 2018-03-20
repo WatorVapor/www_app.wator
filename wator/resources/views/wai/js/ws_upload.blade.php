15d89c41b311ed8d216db62150037c4cafa732ad
Wator Vapor <watorvapor@gmail.com>
2018-03-20 22:42:58 +0900

    Update ws_upload.blade.php

--- wator/resources/views/wai/js/ws_upload.blade.php
+++ wator/resources/views/wai/js/ws_upload.blade.php
@@ -1,8 +1,12 @@
+<!--
 <script src="https://unpkg.com/ipfs-api/dist/index.js"></script>
+-->
 
 
 <script type="text/javascript">
+<!--
 const Buffer = window.IpfsApi().Buffer;
+-->
 
 
 let uriStorage = "wss://" + location.host + "/wator/storage";
@@ -43,10 +47,10 @@ function uploadSliceToLocal(chunks,phoneme) {
   const reader = new FileReader();  
   reader.addEventListener('loadend', (e) => {
     const buffer = e.srcElement.result;
-    let bufText = Buffer.from(buffer);
-    console.log('uploadSliceToLocal:bufText=<',bufText,'>');
+    //let bufText = Buffer.from(buffer);
+    //console.log('uploadSliceToLocal:bufText=<',bufText,'>');
     console.log('uploadSliceToLocal:buffer=<',buffer,'>');
-    localStorage.setItem('wai/train/audio/clip/' + '{{ $lang }}/' + phoneme,bufferToBase64(bufText));
+    localStorage.setItem('wai/train/audio/clip/' + '{{ $lang }}/' + phoneme,bufferToBase64(buffer));
     $( '#wai-recoder-clip-done-next' ).click();
   }); 
   reader.readAsArrayBuffer(blob);
