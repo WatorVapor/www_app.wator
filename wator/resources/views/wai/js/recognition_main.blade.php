efe4d64a90359a6393d4e29067228441b4892676
Wator Vapor <watorvapor@gmail.com>
2018-03-23 00:43:24 +0900

    Update recognition_main.blade.php

--- wator/resources/views/wai/js/recognition_main.blade.php
+++ wator/resources/views/wai/js/recognition_main.blade.php
@@ -40,10 +40,39 @@ let ctx = canvas.getContext('2d');
 
 function onAudioProcess(evt) {
   //console.log('onAudioProcess:evt=<',evt,'>');
+  let audioData = evt.inputBuffer.getChannelData(0);
+  //console.log('onAudioProcess:input=<',input,'>');
+  checkPink2Pink(audioData);
+}
+
+function checkPink2Pink(wave) {
+  //let pinkT = wave[0];
+  //let pinkB = wave[0];
+  let pinkT = [];
+  let pinkB = [];
+  
+  let pinks = [];
+  
+  for(let i = 1;i < wave.length -1;i++) {
+    if(wave[i] > wave[i-1] && wave[i] > wave[i+1]) {
+      pinkT.push(i);
+      pinks.push([i,wave[i]]);
+    }
+    if(wave[i] < wave[i-1] && wave[i] < wave[i+1]) {
+      pinkB.push(i); 
+      pinks.push([i,wave[i]]);
+    }
+  }
+  console.log('checkPink2Pink:pingT=<',pingT,'>');
+  console.log('checkPink2Pink:pingB=<',pingB,'>');
+  drawDataXY(pinks,wave.length);
+}
+
+function drawData(wave,length) {
   let width = canvas.width;
   let height = canvas.height;
-  //console.log('onAudioProcess:width=<',width,'>');
-  //console.log('onAudioProcess:height=<',height,'>');
+  //console.log('drawData:width=<',width,'>');
+  //console.log('drawData:height=<',height,'>');
   
   let pink = height/2;
   
@@ -52,36 +81,37 @@ function onAudioProcess(evt) {
   ctx.strokeStyle = 'rgba(0, 255, 0, 1.0)';
   ctx.lineWidth = 0.5;
   ctx.moveTo(0, pink);
-  let audioData = evt.inputBuffer.getChannelData(0);
-  //console.log('onAudioProcess:input=<',input,'>');
-  for(let i = 0;i < audioData.length;i++) {
-    let x= width *i /audioData.length;
-    let y = pink + audioData[i] * pink;
-    //console.log('onAudioProcess:x=<',x,'>');
-    //console.log('onAudioProcess:y=<',y,'>');
+  for(let i = 0;i < wave.length;i++) {
+    let x= width *wave[i][0] /length;
+    let y = pink + wave[i][1] * pink;
+    //console.log('drawData:x=<',x,'>');
+    //console.log('drawData:y=<',y,'>');
     ctx.lineTo(x, y);
   }
   ctx.stroke();
-  checkPink2Pink(audioData);
-  
 }
 
-function checkPink2Pink(wave) {
-  //let pinkT = wave[0];
-  //let pinkB = wave[0];
-  let pingT = [];
-  let pingB = [];
+function drawDataXY(wave) {
+  let width = canvas.width;
+  let height = canvas.height;
+  //console.log('drawData:width=<',width,'>');
+  //console.log('drawData:height=<',height,'>');
   
-  for(let i = 1;i < wave.length -1;i++) {
-    if(wave[i] > wave[i-1] && wave[i] > wave[i+1]) {
-      pingT.push(i); 
-    }
-    if(wave[i] < wave[i-1] && wave[i] < wave[i+1]) {
-      pingB.push(i); 
-    }
+  let pink = height/2;
+  
+  ctx.beginPath();
+  ctx.clearRect(0, 0, width, height);
+  ctx.strokeStyle = 'rgba(0, 255, 0, 1.0)';
+  ctx.lineWidth = 0.5;
+  ctx.moveTo(0, pink);
+  for(let i = 0;i < wave.length;i++) {
+    let x= width *i /wave.length;
+    let y = pink + wave[i] * pink;
+    //console.log('drawData:x=<',x,'>');
+    //console.log('drawData:y=<',y,'>');
+    ctx.lineTo(x, y);
   }
-  console.log('checkPink2Pink:pingT=<',pingT,'>');
-  console.log('checkPink2Pink:pingB=<',pingB,'>');
+  ctx.stroke();
 }
 
 
