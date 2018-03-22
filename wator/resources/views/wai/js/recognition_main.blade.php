408475371e13fb608d91bf1b4d713dec3f7ca6d5
Wator Vapor <watorvapor@gmail.com>
2018-03-22 23:38:26 +0900

    Update recognition_main.blade.php

--- wator/resources/views/wai/js/recognition_main.blade.php
+++ wator/resources/views/wai/js/recognition_main.blade.php
@@ -25,8 +25,8 @@ function onMediaError(e) {
 
 function onMediaSuccess(stream) {
   console.log('onMediaSuccess:stream=<',stream,'>');
-  let source = audioContext.createMediaStreamSource(stream);
-  let jsNode = audioContext.createJavaScriptNode(2048, 1, 1);
+  let source = audioCtx.createMediaStreamSource(stream);
+  let jsNode = audioCtx.createJavaScriptNode(2048, 1, 1);
   jsNode.onaudioprocess = onAudioProcess;
   source.connect(jsNode);
   jsNode.connect(audioCtx.destination);
