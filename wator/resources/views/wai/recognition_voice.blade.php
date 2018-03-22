20f94783681ecfaafd86413eb97c5b35178b5fb2
Wator Vapor <watorvapor@gmail.com>
2018-03-23 00:10:55 +0900

    Update recognition_voice.blade.php

--- wator/resources/views/wai/recognition_voice.blade.php
+++ wator/resources/views/wai/recognition_voice.blade.php
@@ -18,8 +18,8 @@
 </div>
 
 <div class="row justify-content-center">
-  <div class="col-10 justify-content-center ">
-    <canvas id="wai-recognition-wave" class="border border-danger w-100" height="200"></canvas>
+  <div class="col-12 justify-content-center ">
+    <canvas id="wai-recognition-wave" class="border border-danger w-100" height="100"></canvas>
   </div>
 </div>
 
