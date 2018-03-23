580c0dbab768ef67e9bc5eda9a6f4f7447032fd9
Wator Vapor <watorvapor@gmail.com>
2018-03-23 09:29:48 +0900

    Update recognition_main.blade.php

--- wator/resources/views/wai/js/recognition_main.blade.php
+++ wator/resources/views/wai/js/recognition_main.blade.php
@@ -107,7 +107,7 @@ function drawDataXY(wave,length) {
   
   ctx.beginPath();
   ctx.clearRect(0, 0, width, height);
-  ctx.strokeStyle = 'yellow';
+  ctx.strokeStyle = 'gold';
   ctx.lineWidth = 1;
   //ctx.moveTo(0, pink);
   for(let i = 0;i < wave.length;i++) {
