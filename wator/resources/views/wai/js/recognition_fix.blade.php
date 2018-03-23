ba6c5c9c47da5c2f7841f92adeac33472ff0aa2a
Wator Vapor <watorvapor@gmail.com>
2018-03-23 21:33:54 +0900

    Update recognition_fix.blade.php

--- wator/resources/views/wai/js/recognition_fix.blade.php
+++ wator/resources/views/wai/js/recognition_fix.blade.php
@@ -18,96 +18,34 @@ function doAudioRecord() {
 function onMediaError(e) {
   console.error('media error e=<', e,'>');
 }
+
+
+const RECORD_TIME_MS = 10000;
+const RECORD_INTERVAL_MS = 1000;
+
+
 function onMediaSuccess(stream) {
-  console.log('onMediaSuccess:stream=<',stream,'>');
-  let source = audioCtx.createMediaStreamSource(stream);
-  let filter = audioCtx.createBiquadFilter();
-  filter.type = 'lowpass';
-  filter.frequency.value = 1024;
-  
-  let jsProcess = audioCtx.createScriptProcessor(16384, 1, 1);
-  jsProcess.onaudioprocess = onAudioProcess;
-  source.connect(filter);
-  filter.connect(jsProcess);
-  jsProcess.connect(audioCtx.destination);
-}
-let canvas = document.getElementById('wai-recognition-wave');
-let ctx = canvas.getContext('2d');
-function onAudioProcess(evt) {
-  //console.log('onAudioProcess:evt=<',evt,'>');
-  let audioData = evt.inputBuffer.getChannelData(0);
-  //console.log('onAudioProcess:input=<',input,'>');
-  checkPink2Pink(audioData);
-}
-function checkPink2Pink(wave) {
-  //let pinkT = wave[0];
-  //let pinkB = wave[0];
-  let pinkT = [];
-  let pinkB = [];
-  
-  let pinks = [];
-  
-  for(let i = 1;i < wave.length -1;i++) {
-    if(wave[i] > wave[i-1] && wave[i] > wave[i+1]) {
-      pinkT.push(i);
-      pinks.push([i,wave[i]]);
-    }
-    if(wave[i] < wave[i-1] && wave[i] < wave[i+1]) {
-      pinkB.push(i); 
-      pinks.push([i,wave[i]]);
-    }
+  let mr = new MediaRecorder(stream);
+  mr.mimeType = 'audio/webm';
+  let chunks4analyze = [];
+  mr.ondataavailable = function (e) {
+    chunks4analyze.push(e.data);
   }
-  console.log('checkPink2Pink:pinkT=<',pinkT,'>');
-  console.log('checkPink2Pink:pinkB=<',pinkB,'>');
-  drawDataXY(pinks,wave.length);
-}
-function drawData(wave) {
-  let width = canvas.width;
-  let height = canvas.height;
-  //console.log('drawData:width=<',width,'>');
-  //console.log('drawData:height=<',height,'>');
-  
-  let pink = height/2;
-  
-  ctx.beginPath();
-  ctx.clearRect(0, 0, width, height);
-  ctx.strokeStyle = 'green';
-  ctx.lineWidth = 5;
-  ctx.moveTo(0, pink);
-  for(let i = 0;i < wave.length;i++) {
-    let x= width * i /wave.length;
-    let y = pink + wave[i] * pink;
-    //console.log('drawData:x=<',x,'>');
-    //console.log('drawData:y=<',y,'>');
-    ctx.lineTo(x, y);
-    ctx.moveTo(x, y);
+  mr.onerror = function (e) {
+    console.log('error:e=<',e,'>');
   }
-  ctx.stroke();
-}
-function drawDataXY(wave,length) {
-  let width = canvas.width;
-  let height = canvas.height;
-  //console.log('drawData:width=<',width,'>');
-  //console.log('drawData:height=<',height,'>');
-  
-  let pink = height/2;
-  
-  ctx.beginPath();
-  ctx.clearRect(0, 0, width, height);
-  ctx.strokeStyle = 'pink';
-  ctx.lineWidth = 1;
-  //ctx.moveTo(0, pink);
-  for(let i = 0;i < wave.length;i++) {
-    let absY = Math.abs(wave[i][1]);
-    if(absY > 0.01) {
-      let x = width * wave[i][0] / length;
-      let y = pink + wave[i][1] * pink;
-      //console.log('drawData:x=<',x,'>');
-      //console.log('drawData:y=<',y,'>');
-      ctx.lineTo(x, y);
-      ctx.moveTo(x, y);
-    }
+  mr.onstart = function (e) {
+    console.log('onstart:e=<',e,'>');
   }
-  ctx.stroke();
+  mr.onstop = function (e) {
+    console.log('onstop:e=<',e,'>');
+    console.log('onstop:chunks4analyze=<',chunks4analyze,'>');
+    analyzeBlobWebm(chunks4analyze);
+  }
+  mr.start(RECORD_INTERVAL_MS);
+  setTimeout(function(){
+    mr.stop();
+  },RECORD_TIME_MS);
 }
+
 </script>
