f77df9dd76c99808495ab3833987d1fadfb98b4e
Wator Vapor <watorvapor@gmail.com>
2018-03-26 21:42:27 +0900

    Update recognition_fix.blade.php

--- wator/resources/views/wai/js/recognition_fix.blade.php
+++ wator/resources/views/wai/js/recognition_fix.blade.php
@@ -111,7 +111,7 @@ function createWaveSVG(wave,peaks,freqs) {
   let counter = 0;
   for(let i = 0;i < freqs.length ;i++) {
     let y = peak;
-    let offset = (counter++%4)*(peak/4);
+    let offset = (counter++%5 -2.5)*(peak/4);
     y += offset;
     let x = freqs[i][0];
     svg += '<text font-size="3" x="';
