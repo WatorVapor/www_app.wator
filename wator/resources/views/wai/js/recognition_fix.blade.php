4d257f00ceae6fe0784404d1adf4c5207e45e50a
Wator Vapor <watorvapor@gmail.com>
2018-03-29 23:05:48 +0900

    Update recognition_fix.blade.php

--- wator/resources/views/wai/js/recognition_fix.blade.php
+++ wator/resources/views/wai/js/recognition_fix.blade.php
@@ -22,7 +22,7 @@ function onMediaError(e) {
 
 const RECORD_TIME_MS = 3000;
 const dMinDeltaRawFeqWave = 0.001;
-const dMinDeltaLowFeqWave = 0.16;
+const dMinDeltaLowFeqWave = 0.08;
 const dMinDeltaMiddleFeqWave = 0.04;
 const dMinDeltaHighFeqWave = 0.01;
 
