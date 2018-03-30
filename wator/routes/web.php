e4f7bb27b4c34130ea2d9318f66b320e71a7f5b6
Wator Vapor <watorvapor@gmail.com>
2018-03-31 08:44:42 +0900

    Update web.php

--- wator/routes/web.php
+++ wator/routes/web.php
@@ -45,6 +45,9 @@ Route::post('/wai/text/participle', 'Wai\ParticipleController@store');
 Route::get('/wai/text/record_voice/{name?}', 'Wai\RecordVoiceController@index');
 Route::post('/wai/text/record_voice/{name?}', 'Wai\RecordVoiceController@store');
 
+Route::get('/wai/text/record_continue/{name?}', 'Wai\RecordContinueController@index');
+Route::post('/wai/text/record_continue/{name?}', 'Wai\RecordContinueController@store');
+
 Route::get('/wai/text/recognition', 'Wai\RecognitionVoiceController@index');
 Route::get('/wai/text/recognition_fix', 'Wai\RecognitionVoiceController@index_fix');
 
