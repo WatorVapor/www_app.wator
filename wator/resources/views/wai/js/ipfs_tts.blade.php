12e9debbc15816b43778227eaf05a51b9bace865
Wator Vapor <watorvapor@gmail.com>
2018-03-31 11:32:09 +0900

    Update ipfs_tts.blade.php

--- wator/resources/views/wai/js/ipfs_tts.blade.php
+++ wator/resources/views/wai/js/ipfs_tts.blade.php
@@ -1,5 +1,10 @@
 <script type="text/javascript">
 let ttsStorage = new IpfsStorage(); 
+
+ttsStorage.onReady = function(evt) {
+  console.log('ttsStorage.onReady:evt=<',evt,'>');
+}
+
 function creatAudioBufferByIPFSClips(clips) {
   console.log('creatAudioBufferByIPFSClips:clips=<',clips,'>');
 }
