f92493a6ae9aff0ddfda9845942af84260c7d817
Wator Vapor <watorvapor@gmail.com>
2018-03-31 11:38:43 +0900

    Update ipfs_tts.blade.php

--- wator/resources/views/wai/js/ipfs_tts.blade.php
+++ wator/resources/views/wai/js/ipfs_tts.blade.php
@@ -1,11 +1,33 @@
 <script type="text/javascript">
 let ttsStorage = new IpfsStorage(); 
+let ttsStorageReadyState = false;
+let ttsCached = false;
+let ttsCB = false;
 
 ttsStorage.onReady = function(evt) {
   console.log('ttsStorage.onReady:evt=<',evt,'>');
+  ttsStorageReadyState = true;
+  if(ttsCached) {
+    setTimeout(function(){
+      creatAudioBufferByIPFSClips(ttsCached,ttsCB);
+    },1);
+  }
 }
 
-function creatAudioBufferByIPFSClips(clips) {
-  console.log('creatAudioBufferByIPFSClips:clips=<',clips,'>');
+let baseDuration = 0.4;
+
+function creatAudioBufferByIPFSClips(tts,cb) {
+  console.log('creatAudioBufferByIPFSClips:tts=<',tts,'>');
+  if(tts.lang ==='ja') {
+    baseDuration = 0.2;
+  }
+  if(ttsStorageReadyState) {
+    if(tts.list.length > 0){
+      createClipsAudio(0,tts.list);
+    }
+  } else {
+    ttsCached = tts;
+    ttsCB = cb;
+  }
 }
 </script>
