9d079dbd3327795c2127c4af274394b6cc729ef7
Wator Vapor <watorvapor@gmail.com>
2018-03-31 10:23:56 +0900

    Update hear.blade.php

--- wator/resources/views/wai/js/continue/hear.blade.php
+++ wator/resources/views/wai/js/continue/hear.blade.php
@@ -1,1 +1,7 @@
-
+<script type="text/javascript">
+let phonemes = {{ $ipfs }};
+function onClickRecordBtn(elem) {
+  console.log('onClickHearingBtn:elem=<',elem,'>');
+  console.log('onClickHearingBtn:phonemes=<',phonemes,'>');
+}
+</script>
