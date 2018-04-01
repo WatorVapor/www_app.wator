e1f94fa7403fce5a57d1517cd0734416a38e1d4a
Wator Vapor <watorvapor@gmail.com>
2018-04-01 12:19:37 +0900

    Update freq_demux.blade.php

--- wator/resources/views/wai/js/freq_demux.blade.php
+++ wator/resources/views/wai/js/freq_demux.blade.php
@@ -40,51 +40,56 @@ class AudioFreqDemux {
     this.totalBuffer.push(...audioData);
     console.log('onData_:this.sampleRate=<',this.sampleRate,'>');
   }
+
+
+
+  checkPeak2Peak(wave,dMinDeltaWave) {
+    let peakT = [];
+    let peakB = [];
+
+    let peaks = [];
+    let peakPrev = 0.0;
+
+    for(let i = 1;i < wave.length -1;i++) {
+      if(wave[i] >= wave[i-1] && wave[i] >= wave[i+1]) {
+        let delta = Math.abs(peakPrev - wave[i]);
+        if(delta > dMinDeltaWave){
+          peakT.push(i);
+          peaks.push([i,wave[i]]);
+          peakPrev = wave[i];
+        }
+      }
+      if(wave[i] <= wave[i-1] && wave[i] <= wave[i+1]) {
+        let delta = Math.abs(peakPrev - wave[i]);
+        if(delta > dMinDeltaWave){
+          peakB.push(i); 
+          peaks.push([i,wave[i]]);
+          peakPrev = wave[i];
+        }
+      }
+    }
+    //console.log('checkPeak2Peak:peakT=<',peakT,'>');
+    //console.log('checkPeak2Peak:peakB=<',peakB,'>');
+    return peaks;
+  }
+
+  calFreq(peaks) {
+    let freqs = [];
+
+    for(let i = 1;i < peaks.length;i++) {
+      let freq = peaks[i][0] - peaks[i-1][0];
+      let index = peaks[i][0];
+      freqs.push([index,freq,peaks[i][1]]);
+    }
+    //console.log('calFreq:freqs=<',freqs,'>');
+    return freqs;
+  }
+
+
 };
 
 
 
-function checkPeak2Peak(wave,dMinDeltaWave) {
-  let peakT = [];
-  let peakB = [];
-  
-  let peaks = [];
-  let peakPrev = 0.0;
-  
-  for(let i = 1;i < wave.length -1;i++) {
-    if(wave[i] >= wave[i-1] && wave[i] >= wave[i+1]) {
-      let delta = Math.abs(peakPrev - wave[i]);
-      if(delta > dMinDeltaWave){
-        peakT.push(i);
-        peaks.push([i,wave[i]]);
-        peakPrev = wave[i];
-      }
-    }
-    if(wave[i] <= wave[i-1] && wave[i] <= wave[i+1]) {
-      let delta = Math.abs(peakPrev - wave[i]);
-      if(delta > dMinDeltaWave){
-        peakB.push(i); 
-        peaks.push([i,wave[i]]);
-        peakPrev = wave[i];
-      }
-    }
-  }
-  //console.log('checkPeak2Peak:peakT=<',peakT,'>');
-  //console.log('checkPeak2Peak:peakB=<',peakB,'>');
-  return peaks;
-}
-
-function calFreq(peaks) {
-  let freqs = [];
-  
-  for(let i = 1;i < peaks.length;i++) {
-    let freq = peaks[i][0] - peaks[i-1][0];
-    let index = peaks[i][0];
-    freqs.push([index,freq,peaks[i][1]]);
-  }
-  //console.log('calFreq:freqs=<',freqs,'>');
-  return freqs;
-}
 
 
 </script>
