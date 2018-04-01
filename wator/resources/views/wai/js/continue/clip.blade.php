c393164654bfef6caf6dec79379c1937767d055f
Wator Vapor <watorvapor@gmail.com>
2018-04-01 10:17:55 +0900

    Update clip.blade.php

--- wator/resources/views/wai/js/continue/clip.blade.php
+++ wator/resources/views/wai/js/continue/clip.blade.php
@@ -40,23 +40,28 @@ function clipPhoneme(data,waveEnergyMax,waveEnergyMaxIndex,sample) {
 
 
 
+const dMinDeltaRawFeqWave = 0.001;
+const dMinDeltaLowFeqWave = 0.08;
+const dMinDeltaMiddleFeqWave = 0.02;
+const dMinDeltaHighFeqWave = 0.01;
+
 
 function splitPhonemeClips(source) {
   console.log('splitPhonemeClips source=<',source,'>');
 
-let fraw = new FilterAudioPipe(source,dMinDeltaRawFeqWave);
+  let fraw = new AudioFreqDemux (source,dMinDeltaRawFeqWave);
   
-  let f100 = new FilterAudioPipe(source,dMinDeltaLowFeqWave,100,400);
-//  let f200 = new FilterAudioPipe(source,dMinDeltaLowFeqWave,200,300);
-//  let f300 = new FilterAudioPipe(source,dMinDeltaLowFeqWave,300,500);
-  let f400 = new FilterAudioPipe(source,dMinDeltaMiddleFeqWave,700);
-//  let f500 = new FilterAudioPipe(source,dMinDeltaLowFeqWave,500,700);
-//  let f600 = new FilterAudioPipe(source,dMinDeltaLowFeqWave,600,700);
-  let f700 = new FilterAudioPipe(source,dMinDeltaMiddleFeqWave,700,1000);
-//  let f800 = new FilterAudioPipe(source,dMinDeltaHighFeqWave,800,900);
-//  let f900 = new FilterAudioPipe(source,dMinDeltaHighFeqWave,900,1100);
-  let fk = new FilterAudioPipe(source,dMinDeltaHighFeqWave,1000,1600);
-  let fear = new FilterAudioPipe(source,dMinDeltaHighFeqWave,1600,16384);
+  let f100 = new AudioFreqDemux(source,dMinDeltaLowFeqWave,100,400);
+//  let f200 = new AudioFreqDemux(source,dMinDeltaLowFeqWave,200,300);
+//  let f300 = new AudioFreqDemux(source,dMinDeltaLowFeqWave,300,500);
+  let f400 = new AudioFreqDemux(source,dMinDeltaMiddleFeqWave,700);
+//  let f500 = new AudioFreqDemux(source,dMinDeltaLowFeqWave,500,700);
+//  let f600 = new AudioFreqDemux(source,dMinDeltaLowFeqWave,600,700);
+  let f700 = new AudioFreqDemux(source,dMinDeltaMiddleFeqWave,700,1000);
+//  let f800 = new AudioFreqDemux(source,dMinDeltaHighFeqWave,800,900);
+//  let f900 = new AudioFreqDemux(source,dMinDeltaHighFeqWave,900,1100);
+  let fk = new AudioFreqDemux(source,dMinDeltaHighFeqWave,1000,1600);
+  let fear = new AudioFreqDemux(source,dMinDeltaHighFeqWave,1600,16384);
 
 }
 
