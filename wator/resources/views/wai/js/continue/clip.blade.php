92fb08a81e617530574adb82c9fdaefb2ecbacd2
Wator Vapor <watorvapor@gmail.com>
2018-04-02 00:23:35 +0900

    Update clip.blade.php

--- wator/resources/views/wai/js/continue/clip.blade.php
+++ wator/resources/views/wai/js/continue/clip.blade.php
@@ -12,20 +12,23 @@ function splitPhonemeClips(audioCtx,source) {
     //console.log('fRaw freqs=<',freqs,'>');
   });
   let fLow = new AudioFreqDemux(audioCtx,source,dMinDeltaLowFeqWave,function(freqs){
-    if(freqs.length >0) {
+    if(freqs.length > 0) {
       console.log('fLow freqs=<',freqs,'>');
     }
   },50,500);
+  
   let fMiddle = new AudioFreqDemux(audioCtx,source,dMinDeltaMiddleFeqWave,function(freqs){
-    if(freqs.length >100) {
+    if(freqs.length > 100) {
       console.log('fMiddle freqs=<',freqs,'>');
     }
   },500,1000);
+  
   let fHigh = new AudioFreqDemux(audioCtx,source,dMinDeltaHighFeqWave,function(freqs){
-    if(freqs.length >100) {
+    if(freqs.length > 100) {
       console.log('fHigh freqs=<',freqs,'>');
     }
   },1000,1600);
+  
 }
 
 
