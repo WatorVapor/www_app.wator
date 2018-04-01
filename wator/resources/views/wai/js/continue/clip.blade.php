f88485f2fcdb0f17fac55a29a3862d33c2bdfe9a
Wator Vapor <watorvapor@gmail.com>
2018-04-02 00:05:54 +0900

    Update clip.blade.php

--- wator/resources/views/wai/js/continue/clip.blade.php
+++ wator/resources/views/wai/js/continue/clip.blade.php
@@ -11,21 +11,15 @@ function splitPhonemeClips(audioCtx,source) {
   let fraw = new AudioFreqDemux (audioCtx,source,dMinDeltaRawFeqWave,function(freqs){
     //console.log('fraw freqs=<',freqs,'>');
   });
-  let f100 = new AudioFreqDemux(audioCtx,source,dMinDeltaLowFeqWave,function(freqs){
-    console.log('f100 freqs=<',freqs,'>');
+  let flow = new AudioFreqDemux(audioCtx,source,dMinDeltaLowFeqWave,function(freqs){
+    console.log('flow freqs=<',freqs,'>');
   },50,500);
-/*
-//  let f200 = new AudioFreqDemux(audioCtx,source,dMinDeltaLowFeqWave,200,300);
-//  let f300 = new AudioFreqDemux(audioCtx,source,dMinDeltaLowFeqWave,300,500);
-  let f400 = new AudioFreqDemux(audioCtx,source,dMinDeltaMiddleFeqWave,700);
-//  let f500 = new AudioFreqDemux(audioCtx,source,dMinDeltaLowFeqWave,500,700);
-//  let f600 = new AudioFreqDemux(audioCtx,source,dMinDeltaLowFeqWave,600,700);
-  let f700 = new AudioFreqDemux(audioCtx,source,dMinDeltaMiddleFeqWave,700,1000);
-//  let f800 = new AudioFreqDemux(audioCtx,source,dMinDeltaHighFeqWave,800,900);
-//  let f900 = new AudioFreqDemux(audioCtx,source,dMinDeltaHighFeqWave,900,1100);
-  let fk = new AudioFreqDemux(audioCtx,source,dMinDeltaHighFeqWave,1000,1600);
-  let fear = new AudioFreqDemux(audioCtx,source,dMinDeltaHighFeqWave,1600,16384);
-*/
+  let fmiddle = new AudioFreqDemux(audioCtx,source,dMinDeltaMiddleFeqWave,function(freqs){
+    console.log('fmiddle freqs=<',freqs,'>');
+  },500,1000);
+  let fHigh = new AudioFreqDemux(audioCtx,source,dMinDeltaHighFeqWave,function(freqs){
+    console.log('fHigh freqs=<',freqs,'>');
+  },1000,1600);
 }
 
 
