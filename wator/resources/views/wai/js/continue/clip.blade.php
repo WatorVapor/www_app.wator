b53539ca719ea1bf6f1c1403c7a986b67d565275
Wator Vapor <watorvapor@gmail.com>
2018-04-02 22:19:21 +0900

    Update clip.blade.php

--- wator/resources/views/wai/js/continue/clip.blade.php
+++ wator/resources/views/wai/js/continue/clip.blade.php
@@ -1,5 +1,8 @@
 <script type="text/javascript">
 
+
+const FilterWindowSize = 8192;
+
 const dMinDeltaRawFeqWave = 0.001;
 const dMinDeltaLowFeqWave = 0.16;
 const dMinDeltaMiddleFeqWave = 0.04;
@@ -8,9 +11,20 @@ const dMinDeltaHighFeqWave = 0.02;
 
 function splitPhonemeClips(audioCtx,source) {
   console.log('splitPhonemeClips source=<',source,'>');
+  
+  let jsProcess = audioCtx.createScriptProcessor(FilterWindowSize, 1, 1);
+  jsProcess.onaudioprocess = function(evt) {
+    let audioBuffer = evt.inputBuffer
+    console.log('onaudioprocess:audioBuffer=<',audioBuffer,'>');
+  };
+  source.connect(jsProcess);
+
+/*
+
   let fRaw = new AudioFreqDemux (audioCtx,source,dMinDeltaRawFeqWave,function(freqs){
     //console.log('fRaw freqs=<',freqs,'>');
   });
+  
   let fLow = new AudioFreqDemux(audioCtx,source,dMinDeltaLowFeqWave,function(freqs){
     if(freqs.length > 5) {
       console.log('fLow freqs=<',freqs,'>');
@@ -28,6 +42,7 @@ function splitPhonemeClips(audioCtx,source) {
       console.log('fHigh freqs=<',freqs,'>');
     }
   },1000,1600);
+  */
   
 }
 
