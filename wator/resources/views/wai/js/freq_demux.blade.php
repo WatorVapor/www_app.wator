00981dcdd2665156769a0b9d709ab7baebcd0377
Wator Vapor <watorvapor@gmail.com>
2018-04-03 23:19:51 +0900

    Update freq_demux.blade.php

--- wator/resources/views/wai/js/freq_demux.blade.php
+++ wator/resources/views/wai/js/freq_demux.blade.php
@@ -52,6 +52,7 @@ class AudioFreqDemux {
       this.cb(freq);
     }
     */
+    console.log('onData_:this<',this,'>');
     console.log('onData_:this.convolutionalBuffer.length=<',this.convolutionalBuffer.length,'>');
   }
   
