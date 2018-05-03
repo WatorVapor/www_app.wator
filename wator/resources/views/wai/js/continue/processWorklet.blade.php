3b87303da79e64286dc66ecb42ef72c35b07152b
Wator Vapor <watorvapor@gmail.com>
2018-05-03 21:54:57 +0900

    Update processWorklet.blade.php

--- wator/resources/views/wai/js/continue/processWorklet.blade.php
+++ wator/resources/views/wai/js/continue/processWorklet.blade.php
@@ -1,4 +1,11 @@
 <script type="text/javascript">
-
+class WaiAudioProcess {
+  constructor() {
+    this.buffer = [];
+  }  
+  process(inputs, outputs) {
+    console.log('WaiAudioProcess:process inputs=<',inputs,'>');
+  }
+}
 
 </script>
