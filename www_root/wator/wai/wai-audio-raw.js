cc5b81627dfea4647bae6c8467ed53b6a7673a68
Wator Vapor <watorvapor@gmail.com>
2018-05-03 22:48:12 +0900

    Update wai-audio-raw.js

--- www_root/wator/wai/wai-audio-raw.js
+++ www_root/wator/wai/wai-audio-raw.js
@@ -14,14 +14,34 @@ class WaiAudioRaw extends AudioWorkletProcessor {
     this.buffer.push(...input);
     //console.log('WaiAudioRaw:process this.buffer.length=<',this.buffer.length,'>');
     if(this.buffer.length >= RawWindowSize) {
-      console.log('WaiAudioRaw:process this.buffer.length=<',this.buffer.length,'>');
+      //console.log('WaiAudioRaw:process this.buffer.length=<',this.buffer.length,'>');
       //console.log('WaiAudioRaw:process this.buffer=<',this.buffer,'>');
+      if(isStongWave(this.buffer)) {
+        console.log('WaiAudioRaw:process this.buffer=<',this.buffer,'>');
+      }
       this.buffer.splice(0,RawConvSize);
     }
     return true;
   }
 }
 
+
+function isStongWave(wave) {
+  //console.log('isStongWave:wave=<',wave,'>');
+  let energy = 0.0;
+  for(let i = 0;i < wave.length ;i++) {
+    let level = Math.abs(wave[i]);
+    //console.log('isStongWave:level=<',level,'>');
+    energy += level;
+  }
+  //console.log('isStongWave:energy=<',energy,'>');
+  let avarageEnergy = energy/wave.length;
+  //console.log('isStongWave:avarageEnergy=<',avarageEnergy,'>');
+  if(avarageEnergy > dAvarageEnergyMin) {
+    return true;
+  }
+  return false;
+}
 
 
 
