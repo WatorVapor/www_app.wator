f0b35304591e4659dbeb8cae1d6a66d91bf40b8a
Wator Vapor <watorvapor@gmail.com>
2018-05-04 21:53:08 +0900

    Update WaiRedisNotify.php

--- wator/app/Console/Commands/WaiRedisNotify.php
+++ wator/app/Console/Commands/WaiRedisNotify.php
@@ -4,9 +4,13 @@ namespace Wator\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Redis;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Notifications\Notification;
+use Wator\Notifications\TwitterParticipleNotification;
 
 class WaiRedisNotify extends Command
 {
+    use Notifiable;
     /**
      * The name and signature of the console command.
      *
@@ -40,7 +44,14 @@ class WaiRedisNotify extends Command
     {
         //
         Redis::subscribe(['wai.train.response'], function ($message) {
-            echo $message;
+            $jsonRes = json_decode($message,true);
+            //var_dump($jsonRes);
+            $data = ['result'=>$jsonRes['wai']];
+            $staticHTML = view('wai.snsbot',$data)->__toString();
+            $htmlFileName = hash('sha256',$staticHTML) . '.html';
+            file_put_contents('/autogen/wator/wai/static/' . $htmlFileName,$staticHTML);
+            $url_sns = 'https://www.wator.xyz//autogen/wai/static/' . $htmlFileName;
+            $notify = $this->notify(new TwitterParticipleNotification($url_sns));
         });
     }
 }
