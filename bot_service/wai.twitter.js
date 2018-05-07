f90a0246936ed31a2b325ecbda1603bec9ef4427
Wator Vapor <watorvapor@gmail.com>
2018-05-07 20:25:05 +0900

    Update wai.twitter.js

--- bot_service/wai.twitter.js
+++ bot_service/wai.twitter.js
@@ -1,1 +1,12 @@
+const redis = require("redis");
+const redisOption = {
+  port:6379,
+  host:'127.0.0.1'
+};
+const sub = redis.createClient(redisOption);
+sub.on('message', function(channel, message) {
+  console.log('sub::channel=<',channel,'>');
+  console.log('sub::message=<',message,'>');
+});
+sub.subscribe('wai.train.response');
 
