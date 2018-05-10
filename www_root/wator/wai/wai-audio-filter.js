d527f1bb38189fce7ce894b2c33dc1166a6bc59c
Wator Vapor <watorvapor@gmail.com>
2018-05-10 21:54:13 +0900

    Update wai-audio-filter.js

--- www_root/wator/wai/wai-audio-filter.js
+++ www_root/wator/wai/wai-audio-filter.js
@@ -7,12 +7,13 @@ class WaiAudioFilter extends AudioWorkletProcessor {
     let self = this;
     this.port.onmessage = (event) => {
       console.log(event.data);
-      self.sampleRate = event.data;
+      self.param = event.data;
     };
   }
   process(inputs, outputs) {
     //console.log('WaiAudioRaw:process inputs=<',inputs,'>');
     //console.log('WaiAudioRaw:process outputs=<',outputs,'>');
+    console.log('WaiAudioRaw:process this.param=<',this.param,'>');
     return true;
   }
 }
