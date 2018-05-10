8ea2eb006bb98fec819f814dbcfbd0912f59ab06
Wator Vapor <watorvapor@gmail.com>
2018-05-10 21:21:26 +0900

    Update wai-audio-raw.js

--- www_root/wator/wai/wai-audio-raw.js
+++ www_root/wator/wai/wai-audio-raw.js
@@ -18,7 +18,7 @@ class WaiAudioRaw extends AudioWorkletProcessor {
 //    console.log('WaiAudioRaw:process outputs=<',outputs,'>');
     for(let i = 0;i < outputs.length;i++) {
       for(let j = 0;j < outputs[i].length;j++) {
-        console.log('WaiAudioRaw:process inputs[i][j]=<',inputs[i][j],'>');
+        //console.log('WaiAudioRaw:process inputs[i][j]=<',inputs[i][j],'>');
         for(let k = 0;k < outputs[i][j].length;k++) {
           outputs[i][j][k] = inputs[i][j][k];
         }
