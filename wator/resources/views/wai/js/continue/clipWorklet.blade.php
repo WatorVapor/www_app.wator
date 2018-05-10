00c7554a722a3492fab3322e0befeba909348dce
Wator Vapor <watorvapor@gmail.com>
2018-05-10 21:52:40 +0900

    Update clipWorklet.blade.php

--- wator/resources/views/wai/js/continue/clipWorklet.blade.php
+++ wator/resources/views/wai/js/continue/clipWorklet.blade.php
@@ -15,16 +15,31 @@ function splitPhonemeClips(audioCtx,source) {
     audioCtx.audioWorklet.addModule('/wator/wai/wai-audio-filter.js').then(() => {
       
       let waiAudioFilter100 = new AudioWorkletNode(audioCtx, 'wai-audio-filter');
-      waiAudioFilter100.port.postMessage({sampleRate:audioCtx.sampleRate,delta:0.005});
+      waiAudioFilter100.port.postMessage({sampleRate:audioCtx.sampleRate,limen:0.005});
       filter100.connect(waiAudioFilter100);      
       waiAudioFilter100.connect(audioCtx.destination);
       
       let waiAudioFilter500 = new AudioWorkletNode(audioCtx, 'wai-audio-filter');
-      waiAudioFilter500.port.postMessage({sampleRate:audioCtx.sampleRate,delta:0.01});
+      waiAudioFilter500.port.postMessage({sampleRate:audioCtx.sampleRate,limen:0.01});
       filter500.connect(waiAudioFilter500);      
       waiAudioFilter500.connect(audioCtx.destination);
       
-      
+      let waiAudioFilter1000 = new AudioWorkletNode(audioCtx, 'wai-audio-filter');
+      waiAudioFilter1000.port.postMessage({sampleRate:audioCtx.sampleRate,limen:0.02});
+      filter1000.connect(waiAudioFilter1000);      
+      waiAudioFilter1000.connect(audioCtx.destination);
+
+      let waiAudioFilter1500 = new AudioWorkletNode(audioCtx, 'wai-audio-filter');
+      waiAudioFilter1500.port.postMessage({sampleRate:audioCtx.sampleRate,limen:0.03});
+      filter1500.connect(waiAudioFilter1500);      
+      waiAudioFilter1500.connect(audioCtx.destination);
+
+      let waiAudioFilter2000 = new AudioWorkletNode(audioCtx, 'wai-audio-filter');
+      waiAudioFilter2000.port.postMessage({sampleRate:audioCtx.sampleRate,limen:0.05});
+      filter2000.connect(waiAudioFilter2000);      
+      waiAudioFilter2000.connect(audioCtx.destination);
+
+
     });
   });
 }
