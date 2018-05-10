3ffafef30f98039613a2d5435c3997102a8bd427
Wator Vapor <watorvapor@gmail.com>
2018-05-10 21:09:12 +0900

    Update wai-audio-raw.js

--- www_root/wator/wai/wai-audio-raw.js
+++ www_root/wator/wai/wai-audio-raw.js
@@ -14,61 +14,10 @@ class WaiAudioRaw extends AudioWorkletProcessor {
     };
   }
   process(inputs, outputs) {
-    //console.log('WaiAudioRaw:process inputs=<',inputs,'>');
-    let input = inputs[0][0];
-    //console.log('WaiAudioRaw:process input=<',input,'>');
-    this.buffer.push(...input);
-    //console.log('WaiAudioRaw:process this.buffer.length=<',this.buffer.length,'>');
-    if(this.buffer.length >= RawWindowSize) {
-      //console.log('WaiAudioRaw:process this.buffer.length=<',this.buffer.length,'>');
-      //console.log('WaiAudioRaw:process this.buffer=<',this.buffer,'>');
-      if(isStongWave(this.buffer)) {
-        console.log('WaiAudioRaw:process inputs=<',inputs,'>');
-        console.log('createRecognition this=<',this,'>');
-        createRecognition(this.buffer);
-      }
-      this.buffer.splice(0,RawConvSize);
-    }
+    console.log('WaiAudioRaw:process inputs=<',inputs,'>');
+    console.log('WaiAudioRaw:process outputs=<',outputs,'>');
     return true;
   }
 }
 registerProcessor('wai-audio-raw', WaiAudioRaw);
-
-
-function isStongWave(wave) {
-  //console.log('isStongWave:wave=<',wave,'>');
-  let energy = 0.0;
-  for(let i = 0;i < wave.length ;i++) {
-    let level = Math.abs(wave[i]);
-    //console.log('isStongWave:level=<',level,'>');
-    energy += level;
-  }
-  //console.log('isStongWave:energy=<',energy,'>');
-  let avarageEnergy = energy/wave.length;
-  //console.log('isStongWave:avarageEnergy=<',avarageEnergy,'>');
-  if(avarageEnergy > dAvarageEnergyMin) {
-    return true;
-  }
-  return false;
-}
-
-
-function createRecognition(buffer,ctx) {
-  let audioCtx = new AudioContext();
-  console.log('createRecognition buffer=<',buffer,'>');
-  console.log('createRecognition ctx=<',ctx,'>');
-  let myArrayBuffer = audioCtx.createBuffer(audioBuffer.numberOfChannels, frameCount, audioCtx.sampleRate);
-  let source = audioCtx.createBufferSource();
-  source.buffer = myArrayBuffer;
-  startDemuxFreqs(audioCtx,source);
-  source.start();
-  source.onended = function(evt) {
-    console.log('onaudioprocess:onended evt=<',evt,'>');
-    source.disconnect();
-    stopDemuxFreqs();
-  }
-}
-
-
-
 
