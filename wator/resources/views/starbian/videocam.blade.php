b094067759f95f6c014f53832ff6f3732610f3be
Wator Vapor <watorvapor@gmail.com>
2018-07-15 08:49:21 +0900

    Create videocam.blade.php

---
+++ wator/resources/views/starbian/videocam.blade.php
@@ -0,0 +1,43 @@
+@extends('wator.app')
+@section('appnavbar')
+  @include('starbian.navbar')
+@endsection
+
+@section('content')
+<div class="row mt-lg-5 justify-content-center">
+  <div class="col-6">
+    <button type="button" class="btn btn-success btn-lg btn-block" onclick="onStartGoFuro(this)">!!!HotUp!!!</button>
+  </div>
+</div>
+
+<div class="row mt-lg-5 justify-content-center">
+  <div class="col-4">
+    <div class="card card-default text-center bg-secondary">
+      <div class="card-header">
+        My Public key
+      </div>
+      <div class="card-body">
+        <button type="button" class="btn btn-primary btn-sm  pull-right btn-clipboard" id="btn-copy-key" data-clipboard-target="#text-this-device-key">Copy</button>
+        <pre class="card-text text-danger small" id="text-this-device-key" rows="40"></pre>
+      </div>
+    </div>
+  </div>
+  <div class="col-4">
+    <div class="card card-default text-center bg-info">
+      <div class="card-header">
+        Go Furo Public key
+      </div>
+      <div class="card-body">
+        <button type="button" class="btn btn-success btn-sm" id="btn-add-key" onclick="onAddRemoteKey(this)">+ Key for remote for GoFuRo</button>
+        <textarea class="form-control input-sm" id="text-remote-device-key" rows="10" ></textarea>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script type="text/javascript">
+  new Clipboard('.btn-clipboard');
+</script>
+
+@include('starbian.js.videocam')
+@endsection
