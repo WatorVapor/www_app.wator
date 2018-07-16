457246e05ad11e482ad0ebc676225f6594931e7c
Wator Vapor <watorvapor@gmail.com>
2018-07-16 21:31:09 +0900

    Update videocam.blade.php

--- wator/resources/views/starbian/js/videocam.blade.php
+++ wator/resources/views/starbian/js/videocam.blade.php
@@ -1,2 +1,29 @@
 <script type="text/javascript">
+
+let uri = "wss://www.wator.xyz/ws/starbian";
+let ws = new WebSocket(uri);
+
+ws.onopen = onNotifyOpen_;
+ws.onmessage = onNotifyMessage_;
+ws.onclose = onNotifyClose_;
+ws.onerror = onNotifyError_;
+
+function onNotifyOpen_(evt) {
+  console.log('onNotifyOpen_:evt=<',evt,'>');
+}
+function onNotifyMessage_(evt) {
+  console.log('onNotifyMessage_:evt.data=<',evt.data,'>');
+  let jsonMsg = JSON.parse(evt.data);
+  console.log('onNotifyMessage_:jsonMsg=<',jsonMsg,'>');
+  if(typeof onUpdateData === 'function' && jsonMsg) {
+  }
+}
+function onNotifyClose_(evt) {
+  console.log('onNotifyClose_:evt=<',evt,'>');
+}
+function onNotifyError_(evt) {
+  console.log('onNotifyError_:evt=<',evt,'>');
+}
+
+
 </script>
