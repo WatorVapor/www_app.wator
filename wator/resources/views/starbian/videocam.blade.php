9f9690d1bc2edb27a7a35963a5e873218adccb04
Wator Vapor <watorvapor@gmail.com>
2018-07-17 23:21:26 +0900

    Update videocam.blade.php

--- wator/resources/views/starbian/videocam.blade.php
+++ wator/resources/views/starbian/videocam.blade.php
@@ -29,8 +29,9 @@
         Camera Public key
       </div>
       <div class="card-body">
+        <textarea class="form-control input-sm" id="text-remote-device-key" rows="4" ></textarea>
+        <br/>
         <button type="button" class="btn btn-success btn-sm" id="btn-add-key" onclick="onAddRemoteKey(this)">+ Key for remote for Camera</button>
-        <textarea class="form-control input-sm" id="text-remote-device-key" rows="3" ></textarea>
       </div>
     </div>
   </div>
