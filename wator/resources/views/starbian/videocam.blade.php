57b81b2d15c48e3a4c760931414d146435f639be
Wator Vapor <watorvapor@gmail.com>
2018-07-24 06:32:40 +0800

    Update videocam.blade.php

--- wator/resources/views/starbian/videocam.blade.php
+++ wator/resources/views/starbian/videocam.blade.php
@@ -35,10 +35,12 @@
 <hr/>
 <div class="row mt-lg-5 justify-content-center">
   <div class="col-10" id="vue-ui-remote-device-keys">
-    <button v-for="key in remoteDeviceKeys" type="button" class="btn btn-success btn-lg btn-block"
-      v-bind:href="{key}">
-      Connect to Camera @{{ key }}
-    </button>
+    <div v-for="key in remoteDeviceKeys">
+      <button type="button" class="btn btn-success btn-lg btn-block"
+        v-bind:href="key">
+        Connect to Camera @{{ key }}
+      </button>
+    </div>  
   </div>
 </div>
 
