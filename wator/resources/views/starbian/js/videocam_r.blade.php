e93c4e91f980b25f7720ae40254a5abd3e09913d
Wator Vapor <watorvapor@gmail.com>
2018-08-06 22:04:31 +0900

    Update videocam_r.blade.php

--- wator/resources/views/starbian/js/videocam_r.blade.php
+++ wator/resources/views/starbian/js/videocam_r.blade.php
@@ -5,6 +5,9 @@ function onNotifyReady() {
   let params = location.pathname.split('/');
   let key = params[params.length -1];
   console.log('onNotifyReady:key=<',key,'>');
+  if(key) {
+    sendMsg(key,{start:true});
+  }
 }
 
 </script>
