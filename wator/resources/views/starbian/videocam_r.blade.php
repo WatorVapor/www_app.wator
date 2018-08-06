f21eff131982e8044e6817e9f5490fb509111d32
Wator Vapor <watorvapor@gmail.com>
2018-08-06 21:29:19 +0900

    Update videocam_r.blade.php

--- wator/resources/views/starbian/videocam_r.blade.php
+++ wator/resources/views/starbian/videocam_r.blade.php
@@ -7,16 +7,6 @@
 <hr/>
 <div class="row mt-lg-5 justify-content-center">
   <div class="col-5">
-    <div class="card card-default text-center bg-secondary">
-      <div class="card-header">
-        My Public key
-      </div>
-      <div class="card-body">
-        <pre class="card-text text-danger" id="text-this-device-key" rows="40" style="white-space: pre-wrap ;">@{{ pub_key }}</pre>
-        <br/>
-        <button type="button" class="btn btn-primary btn-sm  pull-right btn-clipboard" id="btn-copy-key" data-clipboard-target="#text-this-device-key">Copy</button>
-      </div>
-    </div>
   </div>
 </div>
 
