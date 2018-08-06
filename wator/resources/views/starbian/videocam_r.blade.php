dc32f00556e52303a45eb1770d53482666098808
Wator Vapor <watorvapor@gmail.com>
2018-08-06 21:17:22 +0900

    Update videocam_r.blade.php

--- wator/resources/views/starbian/videocam_r.blade.php
+++ wator/resources/views/starbian/videocam_r.blade.php
@@ -18,107 +18,14 @@
       </div>
     </div>
   </div>
-  <div class="col-5">
-    <div class="card card-default text-center bg-info">
-      <div class="card-header">
-        Camera Public key
-      </div>
-      <div class="card-body">
-        <textarea class="form-control input-sm" id="text-remote-device-key" rows="3" ></textarea>
-        <br/>
-        <button type="button" class="btn btn-success btn-sm" id="btn-add-key" onclick="onAddRemoteKey(this)">+ Key for remote for Camera</button>
-      </div>
-    </div>
-  </div>
 </div>
 
-<hr/>
-<div class="row mt-lg-5 justify-content-center">
-  <div class="col-10" id="vue-ui-remote-device-keys">
-    <a type="button" v-for="remote in remoteDeviceKeys" class="btn btn-success btn-lg btn-block"
-      v-bind:href="remote.url" target="_blank">
-      Connect to Camera @{{ remote.key }}
-    </a>
-  </div>
-</div>
+
 
 
 <script type="text/javascript">
   new Clipboard('.btn-clipboard');
 </script>
 
-<script type="text/javascript">
-  function onUpdatePublicKey(key) {
-    console.log('onUpdatePublicKey key=<' , key , '>');
-    let app1 = new Vue({
-      el: '#text-this-device-key',
-      data: {
-          pub_key: key
-      }
-    });
-  }
-</script>
-
-
-<script type="text/javascript">
-  function onAddRemoteKey(elem) {
-    try {
-      console.log('onAddRemoteKey elem=<' , elem , '>');
-      let root = elem.parentElement;
-      console.log('onAddRemoteKey root=<' , root , '>');
-      let textKey = root.getElementsByTagName('textarea')[0].value;
-      console.log('onAddRemoteKey textKey=<' , textKey , '>');
-      if(textKey) {
-        WATOR.addRemoteKey(textKey.trim());
-      }
-    } catch(e) {
-      console.error(e);
-    }
-  }
-</script>
-
-<script type="text/javascript">
-  function onRemoteKeyRead() {
-    let remotekeys = WATOR.getRemoteKeys();
-    let urls = [];
-    for(let i = 0;i < remotekeys.length ; i++) {
-      let url = 'https://www.wator.xyz/starbian/cloud/videocam/' + remotekeys[i];
-      let keyPairs = {key:remotekeys[i],url:url}
-      urls.push(keyPairs);
-    }
-    console.log('onRemoteKeyRead remotekeys=<' , remotekeys , '>');
-    let app2 = new Vue({
-      el: '#vue-ui-remote-device-keys',
-      data: {
-          remoteDeviceKeys: urls
-      }
-    });
-  }
-  $(document).ready(function(){
-    onRemoteKeyRead();
-    let elemBtn = document.getElementById('vue-ui-remote-device-keys');
-    console.log('onRemoteKeyRead elemBtn=<' , elemBtn , '>');
-  });
-</script>
-
-
-<script type="text/javascript">
-  function onStartVidoeCam (elem) {
-    try {
-      console.log('onStartVidoeCam elem=<' , elem , '>');
-      let textContent = elem.textContent;
-      console.log('onStartVidoeCam textContent=<' , textContent , '>');
-      if(textContent) {
-        let textKey = textContent.replace('Connect to Camera ','');
-        console.log('onStartVidoeCam textKey=<' , textKey , '>');
-        WATOR.connect(textKey.trim());
-      }
-    } catch(e) {
-      console.error(e);
-    }
-  }
-</script>
-
-
-@include('starbian.js.videocam')
+@include('starbian.js.videocam_r')
 @endsection
