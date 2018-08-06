1a265ab9329e84e637703b0524bc17ea54ee1e21
Wator Vapor <watorvapor@gmail.com>
2018-08-06 22:07:15 +0900

    Update videocam_r.blade.php

--- wator/resources/views/starbian/js/videocam_r.blade.php
+++ wator/resources/views/starbian/js/videocam_r.blade.php
@@ -1,13 +1,22 @@
 <script type="text/javascript">
 
 
+let keyChannel = false;
+
 function onNotifyReady() {
   let params = location.pathname.split('/');
   let key = params[params.length -1];
   console.log('onNotifyReady:key=<',key,'>');
   if(key) {
+    keyChannel = key;
     sendMsg(key,{start:true});
+    startCamera();
   }
+  
+}
+
+function startCamera() {
+  console.log('startCamera:keyChannel=<',keyChannel,'>');
 }
 
 </script>
