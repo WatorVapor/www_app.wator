83b616e84c45c86836b8e7d93d05ef70e9e31a2d
Wator Vapor <watorvapor@gmail.com>
2018-08-07 22:40:37 +0900

    Update videocam.blade.php

--- wator/resources/views/starbian/videocam.blade.php
+++ wator/resources/views/starbian/videocam.blade.php
@@ -39,6 +39,7 @@
       <a type="button" v-for="remote in remoteDeviceKeys" class="btn btn-success "
         v-bind:href="remote.url" target="_blank">
         <i class="material-icons">videocam</i>
+        <span class="badge label label-info">@{{ remote.key }}</span>
       </a>
       <a type="button" v-for="remote in remoteDeviceKeys" class="btn btn-success"
         v-bind:href="remote.url" target="_blank">
