0b9de7b97588268697170090e58910ef0178f84b
Wator Vapor <watorvapor@gmail.com>
2018-08-08 20:48:45 +0900

    Update videocam.blade.php

--- wator/resources/views/starbian/videocam.blade.php
+++ wator/resources/views/starbian/videocam.blade.php
@@ -143,7 +143,7 @@
   function onRemoveRemoteKey (elem) {
     try {
       console.log('onRemoveRemoteKey elem=<' , elem , '>');
-      let keyElem = elem.parentElement;
+      let keyElem = elem.parentElement.parentElement;
       console.log('onRemoveRemoteKey keyElem=<' , keyElem , '>');
     } catch(e) {
       console.error(e);
