0e527ceb9fefc9dd2e22effddb915a56acd3cd01
Wator Vapor <watorvapor@gmail.com>
2018-08-08 21:23:12 +0900

    Create videocam_recv.blade.php

---
+++ wator/resources/views/starbian/videocam_recv.blade.php
@@ -0,0 +1,25 @@
+@extends('wator.app')
+@section('appnavbar')
+  @include('starbian.navbar')
+@endsection
+
+@section('content')
+<hr/>
+<div class="row mt-lg-5 justify-content-center">
+  <div class="col-5">
+    <video id="video" autoplay controls width="640" height="480"></video>
+  </div>
+</div>
+
+
+
+
+<script type="text/javascript">
+  new Clipboard('.btn-clipboard');
+</script>
+<script src="//webrtc.github.io/adapter/adapter-latest.js"></script>
+
+@include('starbian.js.wss')
+@include('starbian.js.keys')
+@include('starbian.js.videocam_recv')
+@endsection
