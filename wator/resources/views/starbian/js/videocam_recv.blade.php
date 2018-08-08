ed0968b795a4a127e24f9d84da3bd7b19ca02ae4
Wator Vapor <watorvapor@gmail.com>
2018-08-08 21:25:01 +0900

    Create videocam_recv.blade.php

---
+++ wator/resources/views/starbian/js/videocam_recv.blade.php
@@ -0,0 +1,40 @@
+<script type="text/javascript">
+let keyChannel = false;
+    
+function onNotifyReady() {
+  let params = location.pathname.split('/');
+  let key = params[params.length -1];
+  console.log('onNotifyReady:key=<',key,'>');
+  if(key) {
+    keyChannel = key;
+    sendMsg(key,{start:true});
+    startCamera();
+  }
+  
+}
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || window.navigator.mozGetUserMedia;
+window.URL = window.URL || window.webkitURL;
+RTCSessionDescription = window.RTCSessionDescription || window.webkitRTCSessionDescription || window.mozRTCSessionDescription;
+RTCPeerConnection = window.RTCPeerConnection || window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
+const configuration = {iceServers: [
+    {urls: 'stun:stun.l.google.com:19302'},
+    {urls: 'stun:stun1.l.google.com:19302'},
+    {urls: 'stun:stun2.l.google.com:19302'}
+  ]
+};
+const pc = new RTCPeerConnection(configuration);
+pc.onicecandidate = ({candidate}) => { 
+  console.log('onicecandidate:candidate=<',candidate,'>');
+}
+function startCamera() {
+  let option = {video: true, audio: true}
+  navigator.getUserMedia(option, onStreamGot,onStreamError)
+}
+function onStreamGot(stream) {
+  console.log('onStreamGot:stream=<',stream,'>');
+  console.log('onStreamGot:pc=<',pc,'>');
+}
+function onStreamError(error) {
+  console.log('onStreamError:error=<',error,'>');
+}
+</script>
