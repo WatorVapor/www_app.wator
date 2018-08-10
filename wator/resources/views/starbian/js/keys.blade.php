9b3f9df955362df61dc6b89b4071871d55addb25
Wator Vapor <watorvapor@gmail.com>
2018-08-10 09:58:36 +0900

    Update keys.blade.php

--- wator/resources/views/starbian/js/keys.blade.php
+++ wator/resources/views/starbian/js/keys.blade.php
@@ -188,6 +188,7 @@ function exportECDHPubKey(pubKey) {
 
 async function sha256(str) {
   const buf = await crypto.subtle.digest("SHA-256", new TextEncoder("utf-8").encode(str));
+  console.log('WATOR.sign buf=<' , buf , '>');
   return Array.prototype.map.call(new Uint8Array(buf), x=>(('00'+x.toString(16)).slice(-2))).join('');
 }
 
