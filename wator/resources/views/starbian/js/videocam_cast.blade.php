29d012fd846d9e8a263b94377cf3ff21b89db030
Wator Vapor <watorvapor@gmail.com>
2018-08-22 14:45:13 +0900

    Update videocam_cast.blade.php

--- wator/resources/views/starbian/js/videocam_cast.blade.php
+++ wator/resources/views/starbian/js/videocam_cast.blade.php
@@ -4,19 +4,34 @@ const params = location.pathname.split('/');
 const keyChannel = params[params.length -1];
 console.log('keyChannel=<',keyChannel,'>');
 let notify = new WatorNotify(keyChannel);
+notify.isReady = false;
 
 notify.onReady = () => {
   notify.publish({start:true});
+  notify.isReady = true;
 };
 notify.subscribe( (msg) => {
   console.log('notify.subcribe:msg=<',msg,'>');
   if(msg && msg.start) {
+    sendLocalCache();
   }
 });
 console.log('notify=<',notify,'>');
 
 
 
+
+let localOfferCache = false;
+let localICECache = [];
+
+function sendLocalCache() {
+  if(localOfferCache) {
+    notify.publish({offer:localOfferCache});
+  }
+  if(localICECache) {
+    notify.publish({ice:localICECache});
+  }
+}
 
 const configuration = {
   iceServers: [
@@ -28,6 +43,9 @@ const configuration = {
 const pc = new RTCPeerConnection(configuration);
 pc.onicecandidate = ({candidate}) => { 
   console.log('onicecandidate:candidate=<',candidate,'>');
+  if(candidate) {
+    sendICE(candidate);
+  }
 }
 startCamera();
 
@@ -57,6 +75,23 @@ function onOfferError(error) {
 function onOfferGot(offer) {
   console.log('onOfferGot:offer=<',offer,'>');
   pc.setLocalDescription(offer);
+  sendOffer(offer);
+}
+
+function sendOffer(offer) {
+  if(notify.isReady) {
+    notify.publish({offer:offer});
+  } else {
+    localOfferCache = offer;
+  }
+}
+
+function sendICE(ice) {
+  if(notify.isReady) {
+    notify.publish({ice:ice});
+  } else {
+    localICECache.push(ice);
+  }
 }
 
 
