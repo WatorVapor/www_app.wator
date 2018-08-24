f9c99ab0f6f2a9938a0557cede4598164f8efe4e
Wator Vapor <watorvapor@gmail.com>
2018-08-24 13:37:56 +0900

    Update gofuro.blade.php

--- wator/resources/views/starbian/js/gofuro.blade.php
+++ wator/resources/views/starbian/js/gofuro.blade.php
@@ -1,7 +1,12 @@
 <script type="text/javascript">
 
+let notify = false;
 function publishHotUpGofuro(keyChannel) {
-  let notify = new WatorNotify(keyChannel);
+  if(notify && notify.isReady) {
+    notify.publish({hotup:true});
+    return;
+  }
+  notify = new WatorNotify(keyChannel);
   notify.isReady = false;
   notify.onReady = () => {
     notify.publish({hotup:true});
