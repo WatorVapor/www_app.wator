671c5eb499373a87346065fb7fed8a1115d3f3fd
Wator Vapor <watorvapor@gmail.com>
2018-08-27 22:12:10 +0900

    Update videocam.blade.php

--- wator/resources/views/starbian/js/videocam.blade.php
+++ wator/resources/views/starbian/js/videocam.blade.php
@@ -1,2 +1,11 @@
 <script type="text/javascript">
+  function onSharedPubKey (elem) {
+    try {
+      console.log('onSharedPubKey  elem=<' , elem , '>');
+      let root = elem.parentElement;
+      console.log('onSharedPubKey root=<' , root , '>');
+    } catch(e) {
+      console.error(e);
+    }
+  }
 </script>
