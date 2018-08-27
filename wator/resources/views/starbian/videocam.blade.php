3587211d6c210964657fcdf8dbea755123260dc2
Wator Vapor <watorvapor@gmail.com>
2018-08-27 22:03:48 +0900

    Update videocam.blade.php

--- wator/resources/views/starbian/videocam.blade.php
+++ wator/resources/views/starbian/videocam.blade.php
@@ -21,8 +21,8 @@
         <h3 class="ml-lg-5 pull-left bg-success text-danger" id="text-share-key-onetime-password">8182</h3>
         <h4 class="ml-lg-1 pull-left" id="text-share-key-count-down">100</h4>
         <div class="progress">
-          <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-        </div>
+          <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">100%</div>
+         </div>
 
 
       </div>
