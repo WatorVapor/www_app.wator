5c16c866ebf2eeb6af25d890962f3e4f76fe0a68
Wator Vapor <watorvapor@gmail.com>
2018-08-28 09:40:42 +0900

    Update keys.blade.php

--- wator/resources/views/starbian/js/keys.blade.php
+++ wator/resources/views/starbian/js/keys.blade.php
@@ -298,7 +298,7 @@ function buf2hex(buf) {
 
 
 /*
-WATOR.verify = function(key,msg,sign,cb) {
+WATOR.verify = function(content,key,msg,sign,cb) {
   let keyBuff = hex2buf(key);
   console.log('WATOR.verify keyBuff=<' , keyBuff , '>');
   let msgBuff = new TextEncoder("utf-8").encode(msg)
@@ -339,22 +339,35 @@ WATOR.verify = function(key,msg,sign,cb) {
 */
 
 
-WATOR.verify = function(key,msg,sign,cb) {
-  let keyBuff = hex2buf(key);
-  //console.log('WATOR.verify keyBuff=<' , keyBuff , '>');
-  let msgBuff = new TextEncoder("utf-8").encode(msg)
-  //console.log('WATOR.verify msgBuff=<' , msgBuff , '>');
-  let signBuff = hex2buf(sign);
-  //console.log('WATOR.verify signBuff=<' , signBuff , '>');
-  
-  let pubKey = KEYUTIL.getKey(key);
-  //console.log('WATOR.verify pubKey=<' , pubKey , '>');
-  
-  let signEngine = new KJUR.crypto.Signature({alg: 'SHA256withECDSA'});
-  signEngine.init({xy: pubKey.pubKeyHex, curve: 'secp256r1'});
-  signEngine.updateString(msg);
-  let result = signEngine.verify(sign);
-  cb(result);
+WATOR.verify = function(content,key,msg,sign,cb) {
+  crypto.subtle.digest("SHA-256", new TextEncoder("utf-8").encode(JSON.stringify(content)))
+  .then(function(buf){
+    let hashCal = Array.prototype.map.call(new Uint8Array(buf), x=>(('00'+x.toString(16)).slice(-2))).join('');
+    console.log('WATOR.verify hashCal=<' , hashCal , '>');
+    console.log('WATOR.verify msg=<' , msg , '>');
+    if(hashCal !== msg) {
+      cb(false);
+    } else {
+      let keyBuff = hex2buf(key);
+      //console.log('WATOR.verify keyBuff=<' , keyBuff , '>');
+      let msgBuff = new TextEncoder("utf-8").encode(msg)
+      //console.log('WATOR.verify msgBuff=<' , msgBuff , '>');
+      let signBuff = hex2buf(sign);
+      //console.log('WATOR.verify signBuff=<' , signBuff , '>');
+
+      let pubKey = KEYUTIL.getKey(key);
+      //console.log('WATOR.verify pubKey=<' , pubKey , '>');
+
+      let signEngine = new KJUR.crypto.Signature({alg: 'SHA256withECDSA'});
+      signEngine.init({xy: pubKey.pubKeyHex, curve: 'secp256r1'});
+      signEngine.updateString(msg);
+      let result = signEngine.verify(sign);
+      cb(result);
+    }
+  })
+  .catch(function(err){
+    console.error(err);
+  });
 };
 
 
