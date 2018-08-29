9b8f4ed296f3aa77f0473316d41ae21e3fc2b982
Wator Vapor <watorvapor@gmail.com>
2018-08-29 09:59:40 +0900

    Update app_js.blade.php

--- wator/resources/views/wator/app_js.blade.php
+++ wator/resources/views/wator/app_js.blade.php
@@ -10,9 +10,23 @@
   integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
   crossorigin="anonymous">
 </script>
-<script src="//cdn.jsdelivr.net/npm/vue"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jsrsasign/8.0.12/jsrsasign-all-min.js" type="text/javascript"></script>
-<script src="/wator/js/clipboard.min.js" type="text/javascript"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Base64/1.0.1/base64.min.js" 
+  integrity="sha256-TIM6MO3yNF4Lvw+A1XLEyy/qiHW4MAjQCjJIqIhHKyY=" 
+  crossorigin="anonymous">
+</script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jsrsasign/8.0.12/jsrsasign-all-min.js" 
+  integrity="sha256-EI1piDYqyKFAy+ykWQZRjH5kkw9eIYE/IahQWg1rXt0=" 
+  crossorigin="anonymous">
+</script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/vue/2.5.17/vue.min.js" 
+  integrity="sha256-FtWfRI+thWlNz2sB3SJbwKx5PgMyKIVgwHCTwa3biXc=" 
+  crossorigin="anonymous">
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.1/clipboard.min.js" 
+  integrity="sha256-hIvIxeqhGZF+VVeM55k0mJvWpQ6gTkWk3Emc+NmowYA=" 
+  crossorigin="anonymous">
+</script>
+
 <script src="/wator/js/ga.js" type="text/javascript"></script>
 <script src="/wator/js/login.js" type="text/javascript"></script>
 <script src="/wator/js/lang.js" type="text/javascript"></script>
