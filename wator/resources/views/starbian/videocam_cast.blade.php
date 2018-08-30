3501083861101158e26504d667142f9359952332
Wator Vapor <watorvapor@gmail.com>
2018-08-30 21:50:08 +0900

    Update videocam_cast.blade.php

--- wator/resources/views/starbian/videocam_cast.blade.php
+++ wator/resources/views/starbian/videocam_cast.blade.php
@@ -8,12 +8,6 @@
 <div class="row mt-lg-5 justify-content-center">
 </div>
 
-
-
-
-<script type="text/javascript">
-  new Clipboard('.btn-clipboard');
-</script>
 <script src="//webrtc.github.io/adapter/adapter-latest.js"></script>
 
 @include('starbian.js.starbian')
