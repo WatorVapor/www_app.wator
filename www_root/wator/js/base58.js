19dd07cf86f6d0c1f2c1eaf6b7ef293d53dee6ec
Wator Vapor <watorvapor@gmail.com>
2018-08-30 23:18:35 +0900

    Update base58.js

--- www_root/wator/js/base58.js
+++ www_root/wator/js/base58.js
@@ -3,3 +3,79 @@ var to_b58_inside = function(B,A){var d=[],s="",i,j,c,n;for(i in B){j=0,c=B[i];s
 var from_b58_inside = function(S,A){var d=[],b=[],i,j,c,n;for(i in S){j=0,c=A.indexOf(S[i]);if(c<0)return undefined;c||b.length^i?i:b.push(0);while(j in d||c){n=d[j];n=n?n*58+c:c;c=n>>8;d[j]=n%256;j++}}while(j--)b.push(d[j]);return new Uint8Array(b)};
 var to_b58 = function(B) { return to_b58_inside(B,b58_map);}
 var from_b58 = function(S){return to_b58_inside(S,b58_map);}
+
+
+
+(function (Bitcoin) {
+  Bitcoin.Base58 = {
+    alphabet: "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz",
+    validRegex: /^[1-9A-HJ-NP-Za-km-z]+$/,
+    base: BigInteger.valueOf(58),
+
+    /**
+     * Convert a byte array to a base58-encoded string.
+     *
+     * Written by Mike Hearn for BitcoinJ.
+     *   Copyright (c) 2011 Google Inc.
+     *
+     * Ported to JavaScript by Stefan Thomas.
+     */
+    encode: function (input) {
+      var bi = BigInteger.fromByteArrayUnsigned(input);
+      var chars = [];
+
+      while (bi.compareTo(B58.base) >= 0) {
+        var mod = bi.mod(B58.base);
+        chars.unshift(B58.alphabet[mod.intValue()]);
+        bi = bi.subtract(mod).divide(B58.base);
+      }
+      chars.unshift(B58.alphabet[bi.intValue()]);
+
+      // Convert leading zeros too.
+      for (var i = 0; i < input.length; i++) {
+        if (input[i] == 0x00) {
+          chars.unshift(B58.alphabet[0]);
+        } else break;
+      }
+
+      return chars.join('');
+    },
+
+    /**
+     * Convert a base58-encoded string to a byte array.
+     *
+     * Written by Mike Hearn for BitcoinJ.
+     *   Copyright (c) 2011 Google Inc.
+     *
+     * Ported to JavaScript by Stefan Thomas.
+     */
+    decode: function (input) {
+      var bi = BigInteger.valueOf(0);
+      var leadingZerosNum = 0;
+      for (var i = input.length - 1; i >= 0; i--) {
+        var alphaIndex = B58.alphabet.indexOf(input[i]);
+        if (alphaIndex < 0) {
+          throw "Invalid character";
+        }	
+        bi = bi.add(BigInteger.valueOf(alphaIndex)
+                    .multiply(B58.base.pow(input.length - 1 -i)));
+
+        // This counts leading zero bytes
+        if (input[i] == "1") leadingZerosNum++;
+        else leadingZerosNum = 0;
+      }
+      var bytes = bi.toByteArrayUnsigned();
+
+      // Add leading zeros
+      while (leadingZerosNum-- > 0) bytes.unshift(0);
+
+      return bytes;
+    }
+  };
+
+  var B58 = Bitcoin.Base58;
+})(
+  'undefined' != typeof Bitcoin ? Bitcoin : module.exports
+);
+
+
