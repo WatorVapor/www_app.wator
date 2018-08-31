cec35c85818e6882bbb4f4cbaeb4a81ea16038e2
Wator Vapor <watorvapor@gmail.com>
2018-09-01 07:50:36 +0900

    Create key..blade.php

---
+++ wator/resources/views/starbian/layout/key..blade.php
@@ -0,0 +1,46 @@
+<div class="row mt-lg-5 justify-content-center">
+  <div class="col-5">
+    <div class="card card-default text-center bg-secondary">
+      <div class="card-header">
+        My Public key
+      </div>
+      <div class="card-body">
+        <pre class="card-text text-success" id="text-this-device-key" rows="40" style="white-space: pre-wrap ;">@{{ pub_key }}</pre>
+        <br/>
+        <button type="button" class="btn btn-primary btn-sm  pull-right btn-clipboard" id="btn-copy-key" data-clipboard-target="#text-this-device-key">Copy</button>
+      </div>
+      <div class="card-body">
+        <button type="button" class="btn btn-primary btn-warning btn-sm  pull-left" id="btn-share-key" onclick="onSharedPubKey(this)">
+          <i class="material-icons">dialpad</i><i class="material-icons">share</i>
+        </button>
+        <h3 class="ml-lg-3 pull-left bg-success text-danger d-none" id="text-share-key-onetime-password">8182</h3>
+        <div class="progress mr-lg-3 d-none" id="top-share-key-onetime-progress">
+          <div id="text-share-key-onetime-progress" class="progress-bar progress-bar-striped progress-bar-animated " 
+              role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+            100
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-5">
+    <div class="card card-default text-center bg-info">
+      <div class="card-header">
+        Remote Device Public key
+      </div>
+      <div class="card-body">
+        <textarea class="form-control input-sm" id="text-remote-device-key" rows="3" ></textarea>
+        <br/>
+        <button type="button" class="btn btn-success btn-sm" id="btn-add-key" onclick="onAddRemoteKey(this)">+ Key for remote for Camera</button>
+      </div>
+      <div class="card-body">
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="password show on remote device" aria-label="one time password" aria-describedby="button-addon2">
+          <div class="input-group-append">
+            <button class="btn btn-danger btn-sm" type="button" onclick="onSearchPubKey(this)" id="button-addon2"><i class="material-icons">search</i></button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
