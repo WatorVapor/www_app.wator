67e0325765654be46cefa973e49e6892827d0d72
Wator Vapor <watorvapor@gmail.com>
2018-09-02 21:54:22 +0900

    Update videocam_cast.blade.php

--- wator/resources/views/starbian/videocam_cast.blade.php
+++ wator/resources/views/starbian/videocam_cast.blade.php
@@ -4,8 +4,11 @@
 @endsection
 
 @section('content')
-<hr/>
+
 <div class="row mt-lg-5 justify-content-center">
+  <div class="col-5">
+    <video id="video" autoplay controls width="640" height="480"></video>
+  </div>
 </div>
 
 <script src="//webrtc.github.io/adapter/adapter-latest.js"></script>
