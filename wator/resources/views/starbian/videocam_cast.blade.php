27e7afc21cc40c0ba1c4a4b5262a27efa4baad21
Wator Vapor <watorvapor@gmail.com>
2018-09-02 22:08:17 +0900

    Update videocam_cast.blade.php

--- wator/resources/views/starbian/videocam_cast.blade.php
+++ wator/resources/views/starbian/videocam_cast.blade.php
@@ -6,9 +6,9 @@
 @section('content')
 
 <div class="row mt-lg-5 justify-content-center">
-  <div class="col-1">
+  <div class="col-2">
     <div class="form-check">
-      <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+      <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1">
       <label class="form-check-label" for="exampleRadios1">
         Default radio
       </label>
