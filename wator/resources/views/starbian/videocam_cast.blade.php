c9ef699abcb9eae2e3a20a5ec7ae1f6a49a7ff49
Wator Vapor <watorvapor@gmail.com>
2018-09-02 21:57:22 +0900

    Update videocam_cast.blade.php

--- wator/resources/views/starbian/videocam_cast.blade.php
+++ wator/resources/views/starbian/videocam_cast.blade.php
@@ -6,6 +6,11 @@
 @section('content')
 
 <div class="row mt-lg-5 justify-content-center">
+  <div class="col-1">
+    <a type="button" class="btn btn-success btn-block" onclick="onTestDeviceKey(this)">
+      <i class="material-icons">remove_circle</i>
+    </a>
+  </div>
   <div class="col-5">
     <video id="video" autoplay controls width="640" height="480"></video>
   </div>
