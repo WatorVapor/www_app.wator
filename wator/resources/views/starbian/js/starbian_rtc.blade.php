a53a0ded3b5e02c140eea81eff86e466893db98b
Wator Vapor <watorvapor@gmail.com>
2018-09-03 23:14:01 +0900

    Update starbian_rtc.blade.php

--- wator/resources/views/starbian/js/starbian_rtc.blade.php
+++ wator/resources/views/starbian/js/starbian_rtc.blade.php
@@ -62,6 +62,9 @@ class StarBianRtc {
     if(msg && msg.offer) {
       this.onRemoteOffer_(msg.offer);
     }
+    if(msg && msg.offer_answer) {
+      this.onOfferAndAnswerStatus_(msg.offer_answer);
+    }
   }
 
   // private
@@ -188,7 +191,7 @@ class StarBianRtc {
 
   sendOfferAnswerStatus_(status) {
     if(this.starbian_.isReady) {
-      this.starbian_.publish({offer_answer:status});
+      this.starbian_.publish({offer_answer:{status:status}});
     }
   }
 
@@ -199,6 +202,10 @@ class StarBianRtc {
       this.localICECache_.push(ice);
     }
   }
+  onOfferAndAnswerStatus_(status) {
+    console.log('onOfferAndAnswerStatus_:status=<',status,'>');  
+  }
+
   onRemoteICE_(ice) {
     console.log('onRemoteICE_:typeof ice=<',typeof ice,'>');  
     console.log('onRemoteICE_:ice=<',ice,'>');  
@@ -249,100 +256,5 @@ class DeviceSetting {
     });
   }
 }
-
-/*
-let localOfferCache = false;
-let localICECache = [];
-function sendLocalCache() {
-  if(localOfferCache) {
-    notify.publish({offer:localOfferCache});
-  }
-  if(localICECache) {
-    notify.publish({ice:localICECache});
-  }
-}
-const configuration = {
-  iceServers: [
-    {urls: 'stun:stun.l.google.com:19302'},
-    {urls: 'stun:stun1.l.google.com:19302'},
-    {urls: 'stun:stun2.l.google.com:19302'}
-  ]
-};
-startCamera();
-let localStreamCache = false;
-function startCamera() {
-  let option = {video: true, audio: true}
-  console.log('startCamera=<',option,'>');
-  let pMedia = navigator.mediaDevices.getUserMedia(option);
-  pMedia.then(onStreamGot);
-  pMedia.catch(onStreamError);
-}
-function onStreamError(error) {
-  console.log('onStreamError:error=<',error,'>');
-}
-function onStreamGot(stream) {
-  console.log('onStreamGot:stream=<',stream,'>');
-  localStreamCache = stream;
-}
-let pc = false;
-function startWebRTC() {
-  if(!localStreamCache) {
-    console.log('startWebRTC !!! eroor localStreamCache=<',localStreamCache,'>');
-    return;
-  }
-  pc = new RTCPeerConnection(configuration);
-  pc.onicecandidate = ({candidate}) => { 
-    console.log('onicecandidate:candidate=<',candidate,'>');
-    if(candidate) {
-      sendICE(candidate);
-    }
-  }
-  pc.addStream(localStreamCache);
-  let pOffer = pc.createOffer();
-  pOffer.then(onOfferGot);
-  pOffer.catch(onOfferError);
-}
-function onOfferError(error) {
-  console.log('onOfferError:error=<',error,'>');
-}
-function onOfferGot(offer) {
-  console.log('onOfferGot:offer=<',offer,'>');
-  pc.setLocalDescription(offer);
-  sendOffer(offer);
-}
-function sendOffer(offer) {
-  if(starbian.isReady) {
-    starbian.publish({offer:offer});
-  } else {
-    localOfferCache = offer;
-  }
-}
-function sendICE(ice) {
-  if(starbian.isReady) {
-    starbian.publish({ice:ice});
-  } else {
-    localICECache.push(ice);
-  }
-}
-function onRemoteAnswer(answer) {
-  console.log('onRemoteAnswer:answer=<',answer,'>');  
-  let sdp = new RTCSessionDescription(answer);
-  console.log('onRemoteAnswer:sdp=<',sdp,'>');
-  let pRsdp =pc.setRemoteDescription(sdp);
-  pRsdp.then(onSetRemoteDescriptionGot);
-  pRsdp.catch(onSetRemoteDescriptionError);
-}
-function onSetRemoteDescriptionError(error) {
-  console.log('onSetRemoteDescriptionError:error=<',error,'>');
-}
-function onSetRemoteDescriptionGot() {
-}
-function onRemoteICE(ice) {
-  console.log('onRemoteICE:typeof ice=<',typeof ice,'>');  
-  console.log('onRemoteICE:ice=<',ice,'>');
-  pc.addIceCandidate( new RTCIceCandidate(ice));
-}
-*/
-
 
 </script>
