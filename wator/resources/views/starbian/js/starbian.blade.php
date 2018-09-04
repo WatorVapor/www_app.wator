cbcbd5557368e6847374864cf467afe6a8ad537a
Wator Vapor <watorvapor@gmail.com>
2018-09-04 21:24:54 +0900

    Update starbian.blade.php

--- wator/resources/views/starbian/js/starbian.blade.php
+++ wator/resources/views/starbian/js/starbian.blade.php
@@ -724,7 +724,7 @@ class StarBianIpfsProxy {
   }
 
   onShareKey_(shareKey) {
-    //console.log('onShareKey_ shareKey =<' , shareKey ,'>');
+    console.log('onShareKey_ shareKey =<' , shareKey ,'>');
     //console.log('onShareKey_ this.targetPubKeyPassword_ =<' , this.targetPubKeyPassword_ ,'>');
     //console.log('onShareKey_ typeof this.targetPubKeyCallback_ =<' , typeof this.targetPubKeyCallback_,'>');
     if(this.targetPubKeyPassword_ === shareKey.password.toString()) {
