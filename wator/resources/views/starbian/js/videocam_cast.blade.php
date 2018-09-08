4b191ce15c39dad17a9dfb84899f40cb16396753
Wator Vapor <watorvapor@gmail.com>
2018-09-08 22:48:53 +0900

    Update videocam_cast.blade.php

--- wator/resources/views/starbian/js/videocam_cast.blade.php
+++ wator/resources/views/starbian/js/videocam_cast.blade.php
@@ -3,7 +3,10 @@
 const params = location.pathname.split('/');
 const keyChannel = params[params.length -1];
 let rtc = new StarBianRtc(keyChannel);
-
+rtc.subscribeMedia( (event) => {
+  console.log('subscribeMedia event=<',event,'>');
+  //document.getElementById("video").srcObject = event.streams[0];
+});
 
 /*
 starbian.onReady = () => {
