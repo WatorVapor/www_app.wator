059905fed1d2bd0b60a0659551c595278bf17ef8
Wator Vapor <watorvapor@gmail.com>
2018-09-08 22:48:36 +0900

    Update videocam_recv.blade.php

--- wator/resources/views/starbian/js/videocam_recv.blade.php
+++ wator/resources/views/starbian/js/videocam_recv.blade.php
@@ -4,6 +4,11 @@ const params = location.pathname.split('/');
 const keyChannel = params[params.length -1];
 let rtc = new StarBianRtc(keyChannel);
 
+rtc.subscribeMedia( (event) => {
+  console.log('subscribeMedia event=<',event,'>');
+  document.getElementById("video").srcObject = event.streams[0];
+});
+
 /*
 starbian.isReady = false;
 
