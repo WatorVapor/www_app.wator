e18f4e2aed7d3ad7f5933b03d16f015a4eb512e8
Wator Vapor <watorvapor@gmail.com>
2018-09-09 23:46:47 +0900

    Update iot.blade.php

--- wator/resources/views/starbian/layout/iot.blade.php
+++ wator/resources/views/starbian/layout/iot.blade.php
@@ -12,7 +12,7 @@
         </a>
       </div>
       <div class="col-8">
-        <span class="label label-info d-inline-block text-truncate" >@{{ remote.key }}</span>
+        <span class="label label-info d-inline-block text-truncate" style="white-space: pre-wrap ; font-size: small;">@{{ remote.key }}</span>
       </div>
     </div>
   </div>
