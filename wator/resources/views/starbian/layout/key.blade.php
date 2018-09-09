ea414eeb7bba4466c047f609946a80a25e355264
Wator Vapor <watorvapor@gmail.com>
2018-09-09 23:28:16 +0900

    Update key.blade.php

--- wator/resources/views/starbian/layout/key.blade.php
+++ wator/resources/views/starbian/layout/key.blade.php
@@ -5,7 +5,7 @@
         My Public key
       </div>
       <div class="card-body">
-        <pre class="card-text text-success" id="text-this-device-key" style="white-space: pre-wrap ; font-size: large;">
+        <pre class="card-text text-success" id="text-this-device-key" style="white-space: pre-wrap ; font-size: x-large;">
           @{{ pub_key }}
         </pre>
         <br/>
