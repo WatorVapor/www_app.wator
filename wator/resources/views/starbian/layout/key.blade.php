6fad1e150a48f7e3245ccf3102e93043e75aa7f5
Wator Vapor <watorvapor@gmail.com>
2018-09-09 23:25:36 +0900

    Update key.blade.php

--- wator/resources/views/starbian/layout/key.blade.php
+++ wator/resources/views/starbian/layout/key.blade.php
@@ -33,7 +33,7 @@
       <div class="card-body">
         <textarea class="form-control input-sm" id="text-remote-device-key" rows="11" ></textarea>
         <br/>
-        <button type="button" class="btn btn-success btn-sm" id="btn-add-key" onclick="onAddRemoteKey(this)">+ Key for remote for Camera</button>
+        <button type="button" class="btn btn-success btn-sm" id="btn-add-key" onclick="onAddRemoteKey(this)">+ Key of remote Device</button>
       </div>
       <div class="card-body">
         <div class="input-group mb-3">
