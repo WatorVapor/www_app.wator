e214fa03190515e64548b6290c32e53c289a695a
Wator Vapor <watorvapor@gmail.com>
2018-09-09 23:38:34 +0900

    Update key.blade.php

--- wator/resources/views/starbian/layout/key.blade.php
+++ wator/resources/views/starbian/layout/key.blade.php
@@ -29,7 +29,7 @@
         Remote Device Public key
       </div>
       <div class="card-body">
-        <textarea class="form-control input-sm" id="text-remote-device-key" rows="8" ></textarea>
+        <textarea class="form-control input-sm" id="text-remote-device-key" rows="6" ></textarea>
         <br/>
         <button type="button" class="btn btn-success btn-sm" id="btn-add-key" onclick="onAddRemoteKey(this)">+ Key of remote Device</button>
       </div>
