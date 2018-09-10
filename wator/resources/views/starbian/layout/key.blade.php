ef2b975ad046ff2b051eb73373e9481f82e5cdb0
Wator Vapor <watorvapor@gmail.com>
2018-09-10 22:55:45 +0900

    Update key.blade.php

--- wator/resources/views/starbian/layout/key.blade.php
+++ wator/resources/views/starbian/layout/key.blade.php
@@ -105,3 +105,60 @@
   }
 </script>
 
+<script type="text/javascript">
+let broadcastKey = new StarBian.BroadCast();
+broadcastKey.isReady = false;
+broadcastKey.onReady = () => {
+  broadcastKey.isReady = true;
+};
+
+
+function onSharedPubKey (elem) {
+  try {
+    console.log('onSharedPubKey  elem=<' , elem , '>');
+    elem.setAttribute('disabled','true');
+    broadcastKey.broadcastPubKey( (status,password) => {
+      console.log('onSharedPubKey status=<' , status , '>');
+      if(status < 1) {
+        elem.setAttribute('disabled','false');
+        $("#text-share-key-onetime-password").addClass("d-none");
+        $("#top-share-key-onetime-progress").addClass("d-none");
+        return;
+      }
+      if(password) {
+        $("#text-share-key-onetime-password").text(password);
+      }
+      $("#text-share-key-onetime-password").removeClass("d-none");
+      $("#text-share-key-onetime-password").toggleClass('bg-success');
+      
+      
+      $("#text-share-key-onetime-progress").text(status *10);
+      $("#text-share-key-onetime-progress").attr('style','width: ' + status *10 + '%');
+      $("#text-share-key-onetime-progress").attr('aria-valuenow',status *10);
+      $("#top-share-key-onetime-progress").removeClass("d-none");
+      console.log('onSharedPubKey password=<' , password , '>');
+    });
+  } catch(e) {
+    console.error(e);
+  }
+}
+
+function onSearchPubKey (elem) {
+  try {
+    console.log('onSearchPubKey  elem=<' , elem , '>');
+    let root = elem.parentElement.parentElement;
+    console.log('onSearchPubKey root=<' , root , '>');
+    let textPassword = root.getElementsByTagName('input')[0].value;
+    console.log('onSearchPubKey textPassword=<' , textPassword , '>');
+    if(!textPassword) {
+      return;
+    }
+    broadcastKey.listenPubKey(textPassword.trim(), (pubKey) => {
+      console.log('onSearchPubKey pubKey=<' , pubKey , '>');
+      $("#text-remote-device-key").text(pubKey);
+    });
+  } catch(e) {
+    console.error(e);
+  }
+}
+</script>
