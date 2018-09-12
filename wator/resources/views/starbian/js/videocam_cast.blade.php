b99b070f99c0215dd95a13b132fc3a8d517b0698
Wator Vapor <watorvapor@gmail.com>
2018-09-12 22:00:17 +0900

    Update videocam_cast.blade.php

--- wator/resources/views/starbian/js/videocam_cast.blade.php
+++ wator/resources/views/starbian/js/videocam_cast.blade.php
@@ -10,12 +10,21 @@ if(keyChannel) {
   });
   rtc.subscribeLocalMedia((localStream) => {
     console.log('subscribeMedia localStream=<',localStream,'>');
-    ctracker.start(localStream);
+    if(!ctracker.localStreamStarbian) {
+      ctracker.localStreamStarbian = localStream;
+      ctracker.start(localStream);
+      setInterval(onGetFaceDectectCheck,5000);
+    }
   });
 }
 
 const ctracker = new clm.tracker();
 ctracker.init();
 console.log('ctracker=<',ctracker,'>');
+
+onGetFaceDectectCheck = () => {
+  let positions = ctracker.getCurrentPosition();
+  console.log('onGetFaceDectectCheck: positions=<',positions,'>');
+};
 
 </script>
