a871767f0c81484f0bfcc93233767d9cbe22dc9f
Wator Vapor <watorvapor@gmail.com>
2018-09-15 11:29:41 +0900

    Update m_localstorage.blade.php

--- wator/resources/views/starbian/js/m_localstorage.blade.php
+++ wator/resources/views/starbian/js/m_localstorage.blade.php
@@ -10,6 +10,9 @@ saveLocalStorage = () => {
   console.log('saveLocalStorage: WebViewLocalStorage=<',WebViewLocalStorage,'>');
   for (let key in localStorage){
    console.log('saveLocalStorage: key=<',key,'>');
+   let value = localStorage.getItem(key);
+   console.log('saveLocalStorage: value=<',value,'>');
+   WebViewLocalStorage.setItem(key,value);
   }
 };
 
