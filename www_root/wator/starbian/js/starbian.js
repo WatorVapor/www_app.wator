b34fcbb9831f2d4050b0b09b30520568e0c4604c
Wator Vapor <watorvapor@gmail.com>
2018-09-15 09:54:45 +0900

    Update starbian.js

--- www_root/wator/starbian/js/starbian.js
+++ www_root/wator/starbian/js/starbian.js
@@ -7,8 +7,8 @@ StarBian.SHARE_PUBKEY_TIMEOUT_WARN = 100 +10;
 
 //console.log('androidLocalStorage=<',androidLocalStorage,'>');
 //console.log('localStorage=<',localStorage,'>');
-localStorage = androidLocalStorage || localStorage;
-console.log('localStorage=<',localStorage,'>');
+//localStorage = androidLocalStorage || localStorage;
+//console.log('localStorage=<',localStorage,'>');
 
 /**
 * @return {array} key
