cd7dc79c3e6a00fd82795be2b541309f9ecfc816
Wator Vapor <watorvapor@gmail.com>
2018-09-16 12:15:45 +0900

    Update videocam_cast.blade.php

--- wator/resources/views/starbian/js/videocam_cast.blade.php
+++ wator/resources/views/starbian/js/videocam_cast.blade.php
@@ -1,31 +1,36 @@
 <script type="text/javascript">
 
-const params = location.pathname.split('/');
-const keyChannel = params[params.length -1];
-console.log('keyChannel=<' , keyChannel , '>');
-if(keyChannel) {
-  let rtc = new StarBianRtc(keyChannel,'offer');
-  rtc.subscribeMedia( (event) => {
-    console.log('subscribeMedia event=<',event,'>');
-    //document.getElementById("video").srcObject = event.streams[0];
-  });
-  rtc.subscribeLocalMedia((localStream) => {
-    console.log('subscribeMedia localStream=<',localStream,'>');
-    
-    if(!ctracker.localStreamStarbian) {
-      let clmVideo = document.getElementById("video-clmtrackr");
-      clmVideo.srcObject = localStream;
-      ctracker.localStreamStarbian = localStream;
-      ctracker.start(clmVideo);
-      setInterval(onGetFaceDectectCheck,5000);
-    }
-    
-  });
-} else {
-  let remotekeys = StarBian.getRemoteKey();
-  console.log('remotekeys=<' , remotekeys , '>');
-}
+$(document).ready(function(){
+  onInitVideoCam();
+});
 
+onInitVideoCam = () => {
+  let elemKey = document.getElementById('wator-remote-key');
+  const keyChannel = elemKey.textContent;
+  console.log('keyChannel=<' , keyChannel , '>');
+  if(keyChannel) {
+    let rtc = new StarBianRtc(keyChannel,'offer');
+    rtc.subscribeMedia( (event) => {
+      console.log('subscribeMedia event=<',event,'>');
+      //document.getElementById("video").srcObject = event.streams[0];
+    });
+    rtc.subscribeLocalMedia((localStream) => {
+      console.log('subscribeMedia localStream=<',localStream,'>');
+
+      if(!ctracker.localStreamStarbian) {
+        let clmVideo = document.getElementById("video-clmtrackr");
+        clmVideo.srcObject = localStream;
+        ctracker.localStreamStarbian = localStream;
+        ctracker.start(clmVideo);
+        setInterval(onGetFaceDectectCheck,5000);
+      }
+
+    });
+  } else {
+    let remotekeys = StarBian.getRemoteKey();
+    console.log('remotekeys=<' , remotekeys , '>');
+  }
+}
 
 const ctracker = new clm.tracker();
 ctracker.init();
