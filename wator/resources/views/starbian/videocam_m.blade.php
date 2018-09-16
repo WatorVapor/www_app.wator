d0e39d1c84f5e9a76976543429105a7c094767fb
Wator Vapor <watorvapor@gmail.com>
2018-09-16 10:03:53 +0900

    Update videocam_m.blade.php

--- wator/resources/views/starbian/videocam_m.blade.php
+++ wator/resources/views/starbian/videocam_m.blade.php
@@ -7,6 +7,6 @@
 <script src="/wator/starbian/js/starbian.js" type="text/javascript"></script>
 @include('starbian.layout.key')
 <hr/>
-@include('starbian.layout.camera')
+@include('starbian.layout.camera',['mobile'=>True])
 @include('starbian.js.m_localstorage')
 @endsection
