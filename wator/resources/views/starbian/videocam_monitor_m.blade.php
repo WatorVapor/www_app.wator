cdfe92b28bfb6e41e02d9b155db19178778a0859
Wator Vapor <watorvapor@gmail.com>
2018-09-28 22:06:21 +0900

    Update videocam_monitor_m.blade.php

--- wator/resources/views/starbian/videocam_monitor_m.blade.php
+++ wator/resources/views/starbian/videocam_monitor_m.blade.php
@@ -3,4 +3,66 @@
 <script src="/wator/starbian/js/starbian.js" type="text/javascript"></script>
 <hr/>
 @include('starbian.js.m_localstorage')
+
+<script type="text/javascript">
+
+const STAIBIAN_CAMERA_HELLO_TEXT = [
+  '门口来人了，快看看去。',
+  '主人，来客人了。'
+];
+sayCheckDoorCamera = () => {
+  //console.log('sayCheckDoorCamera: speechSynthesis.speaking=<',speechSynthesis.speaking,'>');
+  try {
+    //console.log('sayCheckDoorCamera: speechSynthesis.speaking=<',speechSynthesis.speaking,'>');
+    if(!speechSynthesis) {
+      sayCheckDoorCameraAndroid();
+      return;
+    }
+  } catch(e) {
+    sayCheckDoorCameraAndroid();
+    return;
+  }
+  console.log('sayCheckDoorCamera: speechSynthesis.speaking=<',speechSynthesis.speaking,'>');
+  if(speechSynthesis.speaking) {
+    console.warn('sayCheckDoorCamera: speechSynthesis.speaking=<',speechSynthesis.speaking,'>');
+    speechSynthesis.cancel();
+    return;
+  }
+  let voices = speechSynthesis.getVoices();
+  console.log('sayCheckDoorCamera: voices=<',voices,'>');
+  let maxRandom = STAIBIAN_CAMERA_HELLO_TEXT.length;
+  let index = Math.floor(Math.random()*(maxRandom));
+  let txt = STAIBIAN_CAMERA_HELLO_TEXT[index];
+  let uttr = new SpeechSynthesisUtterance(txt);
+  uttr.lang = 'zh-CN';
+  uttr.pitch  = 1.3;
+  uttr.rate   = 1;
+  uttr.volume = 1;
+  uttr.voice = null;
+  uttr.onstart = (evt) => {
+    //console.log('sayCheckDoorCamera uttr.onstart evt=<',evt,'>');
+  };
+  uttr.onpause = (evt) => {
+    //console.log('sayCheckDoorCamera uttr.onpause evt=<',evt,'>');
+  };
+  uttr.onerror = (evt) => {
+    //console.log('sayHello uttr.onerror evt=<',evt,'>');
+  };
+  uttr.onboundary = (evt) => {
+    //console.log('sayCheckDoorCamera uttr.onboundary evt=<',evt,'>');
+  };
+  uttr.onend　=　(evt) => {
+    //console.log('sayCheckDoorCamera uttr.onend evt=<',evt,'>');
+  };
+  speechSynthesis.speak(uttr);
+}
+sayCheckDoorCameraAndroid = () => {
+  let maxRandom = STAIBIAN_CAMERA_HELLO_TEXT.length;
+  let index = Math.floor(Math.random()*(maxRandom));
+  let txt = STAIBIAN_CAMERA_HELLO_TEXT[index];
+  androidTTS.say(txt);
+}
+</script>
+
+
 @endsection
