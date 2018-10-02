7115a4068ab5f363dce8c60ac627561dc8e00d70
Wator Vapor <watorvapor@gmail.com>
2018-10-02 21:50:47 +0900

    Update videocam_recv.blade.php

--- wator/resources/views/starbian/videocam_recv.blade.php
+++ wator/resources/views/starbian/videocam_recv.blade.php
@@ -8,7 +8,7 @@
 <div class="row mt-lg-5 justify-content-center">
   <div class="col-1">
     <a type="button" class="btn btn-danger btn-lg btn-block" onclick="onRestartRemoteApp(this)">
-      <i class="material-icons">ren</i>
+      <i class="material-icons">autorenew</i>
     </a>
   </div>
 </div>
