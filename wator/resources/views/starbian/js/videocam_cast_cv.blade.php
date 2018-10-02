599c61a49dcb4ec41e519a932b73f4d4d410ad7b
Wator Vapor <watorvapor@gmail.com>
2018-10-02 20:10:42 +0900

    Update videocam_cast_cv.blade.php

--- wator/resources/views/starbian/js/videocam_cast_cv.blade.php
+++ wator/resources/views/starbian/js/videocam_cast_cv.blade.php
@@ -27,10 +27,8 @@ createWebRTCConnection = (keyChannel) => {
   rtc.subscribeLocalMedia((localStream) => {
     console.log('subscribeMedia localStream=<',localStream,'>');
     if(!ctracker.localStreamStarbian) {
-      let clmVideo = document.getElementById("video-clmtrackr");
-      clmVideo.srcObject = localStream;
-      ctracker.localStreamStarbian = localStream;
-      ctracker.start(clmVideo);
+      let cvVideo = document.getElementById("video-opencv");
+      cvVideo.srcObject = localStream;
       setInterval(onGetFaceDectectCheck,2000);
     }
   });
@@ -50,9 +48,7 @@ callMaster = () => {
     }
   }
 }
-const ctracker = new clm.tracker();
-ctracker.init();
-console.log('ctracker=<',ctracker,'>');
+
 const FaceDetectNotifyIntervalMS = 1000 * 5;
 let prevFaceDetectTime = new Date();
 let sayByebyeTimeout = false;
@@ -60,33 +56,5 @@ let ForbiddenTalking = false;
 const FaceDetectSayByeIntervalMS = 1000 * 100;
 const FaceDetectForbiddenIntervalMS= 1000 * 100;
 onGetFaceDectectCheck = () => {
-  let positions = ctracker.getCurrentPosition();
-  //console.log('onGetFaceDectectCheck: positions=<',positions,'>');
-  if(positions && positions.length) {
-    let sum = 0;
-    for(let i = 1;i < positions.length ;i++) {
-      let pos1 = positions[i -1];
-      let pos2 = positions[i];
-      //console.log('onGetFaceDectectCheck: pos1=<',pos1,'>');
-      //console.log('onGetFaceDectectCheck: pos2=<',pos2,'>');
-      sum += Math.abs(pos2[0] - pos1[0]) + Math.abs(pos2[1] - pos1[1])
-    }
-    console.log('onGetFaceDectectCheck: sum=<',sum,'>');
-    if(sum > FACE_LINE_POINT_SUM_MIN) {
-      let now = new Date();
-      let diff = now - prevFaceDetectTime;
-      if(diff < FaceDetectNotifyIntervalMS) {
-        return;
-      }
-      prevFaceDetectTime = now;
-      if(!ForbiddenTalking) {
-        sayHello();
-        callMaster();
-      }
-      if(!sayByebyeTimeout) {
-        sayByebyeTimeout = setTimeout(onSayByeBye,FaceDetectSayByeIntervalMS);
-      }
-    }
-  }
 };
 </script>
