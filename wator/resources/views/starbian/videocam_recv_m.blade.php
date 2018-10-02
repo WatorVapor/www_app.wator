89f9fbc8c257ba8e294859f8bc274be268dccd66
Wator Vapor <watorvapor@gmail.com>
2018-10-02 21:41:32 +0900

    Update videocam_recv_m.blade.php

--- wator/resources/views/starbian/videocam_recv_m.blade.php
+++ wator/resources/views/starbian/videocam_recv_m.blade.php
@@ -7,6 +7,14 @@
   </div>
 </div>
 
+<div class="row mt-lg-5 justify-content-center">
+  <div class="col-5">
+    <a type="button" class="btn btn-danger btn-lg btn-block" onclick="onRestartRemoteApp(this)">
+      <i class="material-icons">remove_circle</i>
+    </a>
+  </div>
+</div>
+
 <script src="//webrtc.github.io/adapter/adapter-latest.js"></script>
 <script src="/wator/starbian/js/starbian.js" type="text/javascript"></script>
 <script src="/wator/starbian/js/starbian_rtc.js" type="text/javascript"></script>
