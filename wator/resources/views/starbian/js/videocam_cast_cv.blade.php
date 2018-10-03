bd6970f3239faa83d0d935e81defab8ad17244cb
Wator Vapor <watorvapor@gmail.com>
2018-10-03 22:29:28 +0900

    Update videocam_cast_cv.blade.php

--- wator/resources/views/starbian/js/videocam_cast_cv.blade.php
+++ wator/resources/views/starbian/js/videocam_cast_cv.blade.php
@@ -10,6 +10,62 @@
 </script>
 <script async src="https://huningxin.github.io/opencv.js/build/asm.js/opencv.js"></script>
 
+<script type="text/js-worker">
+ var Module = {
+    preRun: [function() {
+      Module.FS_createPreloadedFile('/', 'haarcascade_eye.xml', 'https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_eye.xml', true, false);
+      Module.FS_createPreloadedFile('/', 'haarcascade_frontalface_default.xml', 'https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml', true, false);
+      Module.FS_createPreloadedFile('/', 'haarcascade_profileface.xml', 'https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_profileface.xml', true, false);
+    }],
+    _main: function() {opencvIsReady();}
+  };
+ 
+  self.importScripts('https://huningxin.github.io/opencv.js/build/asm.js/opencv.js');
+ 
+  function opencvIsReady() {
+    console.log('OpenCV.js is ready for worker');
+    workerInit();
+  }
+  
+  var classifier;
+  function workerInit() {
+    classifier = new cv.CascadeClassifier();
+    let status = classifier.load('haarcascade_frontalface_default.xml');
+    console.log('worker init status ' + status);
+    self.postMessage({response: 'init_ok'});
+  }
+  
+  self.addEventListener('message', (msg) => {
+    switch (msg.data.cmd) {
+      case 'detect': {
+        let objects = [];
+        let buffer = msg.data.buf;
+        let height = msg.data.height;
+        let width = msg.data.width;
+        let mat = new cv.Mat(height, width, cv.CV_8UC4);
+        mat.data.set(new Uint8Array(buffer));
+        cv.cvtColor(mat, mat, cv.COLOR_RGBA2GRAY);
+        let results = new cv.RectVector();
+        classifier.detectMultiScale(mat, results, 1.1, 3, 0, {width : 0, height : 0}, {width : 0, height : 0});
+        console.log('worker detect ' + results.size());
+        for (let i = 0; i < results.size(); i++) {
+          objects.push(results.get(i));
+        }
+        self.postMessage({response: 'detect', buf: buffer, objects: objects}, [buffer]);
+        mat.delete();
+        results.delete();
+      }
+      case 'passthrough': {
+        let buffer = msg.data.buf;
+        self.postMessage({response: 'passthrough', buf: buffer}, [buffer]);
+        break;
+      }
+      default: {
+        throw('invalid command from main thread');
+      }
+    }
+  }, false);
+</script>
 
 
 
