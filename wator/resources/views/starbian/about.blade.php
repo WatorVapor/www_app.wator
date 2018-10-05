a91b16efc1611d6044a1e50ff439c4479b87346f
Wator Vapor <watorvapor@gmail.com>
2018-10-06 08:38:29 +0900

    Update about.blade.php

--- wator/resources/views/starbian/about.blade.php
+++ wator/resources/views/starbian/about.blade.php
@@ -50,10 +50,39 @@
   <div class="col">
     <div class="card card-default text-center">
       <div class="card-body">
-        <h4 class="card-title">Web Bluetooth</h4>
+        <h4 class="card-title">PubSub Based On IPFS</h4>
       </div>
       <img class="card-img-top" src="/wator/images/starbian/ipfs.pubsub.png" alt="Card image cap">
     </div>
   </div>
 </div>
+
+<div class="row">
+  <div class="col">
+    <div class="card card-default text-center">
+      <div class="card-body">
+        <h4 class="card-title">Security Digital Signature</h4>
+      </div>
+      <img class="card-img-top" src="/wator/images/starbian/ipfs.ecdsa.png" alt="Card image cap">
+    </div>
+  </div>
+  <div class="col">
+    <div class="card card-default text-center">
+      <div class="card-body">
+        <h4 class="card-title">Elliptic Curve Diffie–Hellman key Exchange</h4>
+      </div>
+      <img class="card-img-top" src="/wator/images/starbian/ipfs.ecdh.png" alt="Card image cap">
+    </div>
+  </div>
+  <div class="col">
+    <div class="card card-default text-center">
+      <div class="card-body">
+        <h4 class="card-title">AES-GCM Cryptographic</h4>
+      </div>
+      <img class="card-img-top" src="/wator/images/starbian/ipfs.aes.gcm.png" alt="Card image cap">
+    </div>
+  </div>
+</div>
+
+
 @endsection
