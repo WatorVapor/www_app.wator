a31b0643567e74cfe1c8bf264eb3ec13546497fe
Wator Vapor <watorvapor@gmail.com>
2018-10-06 08:39:36 +0900

    Update about.blade.php

--- wator/resources/views/starbian/about.blade.php
+++ wator/resources/views/starbian/about.blade.php
@@ -4,40 +4,6 @@
 @endsection
 
 @section('content')
-<!--
-<div class="row">
-  <div class="col">
-    <div class="card card-default">
-      <div class="card-heading">
-        <h6>Devices</h6>
-      </div>
-      <div class="card-body">
-        <img src="/wator/images/ppio/SmartHome-WebRTC-IoT-Hub.png" class="img-thumbnail" alt="rsa gif" width="100%">
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card card-default">
-      <div class="card-heading">
-        <h6>Webrtc</h6>
-      </div>
-      <div class="card-body">
-        <img src="/wator/images/ppio/webrtc-sample-image.png" class="img-thumbnail" alt="webrtc over rsa peer" width="100%">
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card card-default">
-      <div class="card-heading">
-        <h6>Webrtc Signal</h6>
-      </div>
-      <div class="card-body">
-        <img src="/wator/images/ppio/webrtc-another-web-3-638.jpg" class="img-thumbnail" alt="signal" width="100%">
-      </div>
-    </div>
-  </div>
-</div>
--->
 <div class="row">
   <div class="col">
     <div class="card card-default text-center">
@@ -50,7 +16,7 @@
   <div class="col">
     <div class="card card-default text-center">
       <div class="card-body">
-        <h4 class="card-title">PubSub Based On IPFS</h4>
+        <h4 class="card-title">P2P PubSub Based On IPFS</h4>
       </div>
       <img class="card-img-top" src="/wator/images/starbian/ipfs.pubsub.png" alt="Card image cap">
     </div>
