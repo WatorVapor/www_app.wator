80a154866c9e40686d6045f5dd4ddb7430ed02ae
Wator Vapor <watorvapor@gmail.com>
2018-10-08 14:15:23 +0900

    Update starbian_l_storage.blade.php

--- wator/resources/views/starbian/js/starbian_l_storage.blade.php
+++ wator/resources/views/starbian/js/starbian_l_storage.blade.php
@@ -1,6 +1,6 @@
 <script type="text/javascript">
 try {    
-    let uri = "ws://127.0.0.1:";
+    let uri = "ws://127.0.0.1:18080";
     let ws = new WebSocket(uri);
 } catch (e) {
   console.error('e=<' , e , '>');
