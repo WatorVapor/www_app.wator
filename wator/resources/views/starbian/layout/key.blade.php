38d54a46a3684e4ebdc566fa6b938975322a8301
Wator Vapor <watorvapor@gmail.com>
2018-10-14 13:46:37 +0900

    Update key.blade.php

--- wator/resources/views/starbian/layout/key.blade.php
+++ wator/resources/views/starbian/layout/key.blade.php
@@ -9,8 +9,7 @@
           @{{ pub_key }}
         </pre>
         <button type="button" class="btn btn-primary btn-sm mt-sm-1 pull-right btn-clipboard" id="btn-copy-key" data-clipboard-target="#text-this-device-key">Copy</button>
-        <br/>
-        <button type="button" class="btn btn-primary btn-sm mt-sm-1 pull-right" id="btn-qrcode-key" onclick="onSharedPubKey(this)" >QRCode</button>
+        <button type="button" class="btn btn-success btn-sm mt-sm-1 pull-right" id="btn-qrcode-key" onclick="onQRCodePubKey(this)" >QRCode</button>
         <br/>
         <button type="button" class="btn btn-primary btn-warning btn-sm  pull-left" id="btn-share-key" onclick="onSharedPubKey(this)">
           <i class="material-icons">dialpad</i><i class="material-icons">share</i>
