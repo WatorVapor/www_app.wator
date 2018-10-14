e8175e25db0c64616531423bdbfe9bb96214002a
Wator Vapor <watorvapor@gmail.com>
2018-10-14 14:55:32 +0900

    Update camera.blade.php

--- wator/resources/views/starbian/layout/camera.blade.php
+++ wator/resources/views/starbian/layout/camera.blade.php
@@ -86,7 +86,12 @@
         </a>
       </div>
       <div class="col-4">
-        <span class="label label-info d-inline-block text-truncate" style="white-space: pre-wrap ; font-size: large;">@{{ remote.key }}</span>
+        <pre class="card-text text-success" id="text-this-device-key" style="white-space: pre-wrap ; font-size: x-small;">
+          @{{ remote.key }}
+         </pre>
+         <!--
+         <span class="label label-info d-inline-block text-truncate" style="white-space: pre-wrap ; font-size: large;">@{{ remote.key }}</span>
+         -->
       </div>
     </div>
   </div>
