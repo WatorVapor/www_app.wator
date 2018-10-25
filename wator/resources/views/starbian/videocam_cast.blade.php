6bdf82e4790b41c8bce24adf071f69380f6ea1e8
Wator Vapor <watorvapor@gmail.com>
2018-10-25 22:52:58 +0900

    Update videocam_cast.blade.php

--- wator/resources/views/starbian/videocam_cast.blade.php
+++ wator/resources/views/starbian/videocam_cast.blade.php
@@ -61,7 +61,8 @@
 
 
 
-<script src="//webrtc.github.io/adapter/adapter-latest.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/webrtc-adapter/6.4.0/adapter.min.js" integrity="sha256-UH0Npcih7yj1s23pQK0UCrCSxx7AkT91CCMsUGnZ9Ew=" crossorigin="anonymous"></script>
+
 <script src="/wator/starbian/js/starbian.js" type="text/javascript"></script>
 <script src="/wator/starbian/js/starbian_rtc.js" type="text/javascript"></script>
 
