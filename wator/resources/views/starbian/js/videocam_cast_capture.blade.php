c958f776f5c1fc94e2cc318115b7bb313e2069f3
Wator Vapor <watorvapor@gmail.com>
2018-10-27 12:26:38 +0900

    Update videocam_cast_capture.blade.php

--- wator/resources/views/starbian/js/videocam_cast_capture.blade.php
+++ wator/resources/views/starbian/js/videocam_cast_capture.blade.php
@@ -1,51 +1,48 @@
 <script type="text/javascript">
-
-
-function GotCapture() {
-  let video = document.getElementById('video-face');
-  console.log('GotCapture video=<',video,'>');
-  let video = document.getElementById('video-face');
-  console.log('GotCapture video=<',video,'>');
-}
+  function GotCapture() {
+    let video = document.getElementById('video-face');
+    console.log('GotCapture video=<',video,'>');
+    let video = document.getElementById('video-face');
+    console.log('GotCapture video=<',video,'>');
+  }
 </script>
 
 <script type="text/javascript">
-$(document).ready(function(){
-  onInitVideoCam();
-});
-onInitVideoCam = () => {
-  let elemKey = document.getElementById('wator-remote-key');
-  const keyChannel = elemKey.textContent.trim();
-  console.log('keyChannel=<' , keyChannel , '>');
-  if(keyChannel) {
-    createWebRTCConnection(keyChannel);
-  } else {
-    let remotekeys = StarBian.getRemoteKey();
-    console.log('remotekeys=<' , remotekeys , '>');
-    for(let i = 0;i < remotekeys.length;i++) {
-      createWebRTCConnection(remotekeys[i]);
+  $(document).ready(function(){
+    onInitVideoCam();
+  });
+  onInitVideoCam = () => {
+    let elemKey = document.getElementById('wator-remote-key');
+    const keyChannel = elemKey.textContent.trim();
+    console.log('keyChannel=<' , keyChannel , '>');
+    if(keyChannel) {
+      createWebRTCConnection(keyChannel);
+    } else {
+      let remotekeys = StarBian.getRemoteKey();
+      console.log('remotekeys=<' , remotekeys , '>');
+      for(let i = 0;i < remotekeys.length;i++) {
+        createWebRTCConnection(remotekeys[i]);
+      }
     }
   }
-}
 
-let rtcConnectionList = [];
-createWebRTCConnection = (keyChannel) => {
-  let rtc = new StarBianRtc(keyChannel,'offer');
-  rtc.subscribeMedia( (event) => {
-    console.log('subscribeMedia event=<',event,'>');
-    //document.getElementById("video").srcObject = event.streams[0];
-  });
-  rtc.subscribeLocalMedia((localStream) => {
-    console.log('subscribeMedia localStream=<',localStream,'>');
-    let cvVideo = document.getElementById("video-face");
-    cvVideo.srcObject = localStream;
-    setInterval(GotCapture,1000);
-  });
-  rtc.subscribeMsg( (msg,channel) => {
-    console.log('subscribeMsg msg=<',msg,'>');
-    console.log('subscribeMsg channel=<',channel,'>');
-  });
-  rtcConnectionList.push(rtc);
-}
-
+  let rtcConnectionList = [];
+  createWebRTCConnection = (keyChannel) => {
+    let rtc = new StarBianRtc(keyChannel,'offer');
+    rtc.subscribeMedia( (event) => {
+      console.log('subscribeMedia event=<',event,'>');
+      //document.getElementById("video").srcObject = event.streams[0];
+    });
+    rtc.subscribeLocalMedia((localStream) => {
+      console.log('subscribeMedia localStream=<',localStream,'>');
+      let cvVideo = document.getElementById("video-face");
+      cvVideo.srcObject = localStream;
+      setInterval(GotCapture,1000);
+    });
+    rtc.subscribeMsg( (msg,channel) => {
+      console.log('subscribeMsg msg=<',msg,'>');
+      console.log('subscribeMsg channel=<',channel,'>');
+    });
+    rtcConnectionList.push(rtc);
+  }
 </script>
