7cb98f25b03e156587c1b4a711f15eb08ab374d3
Wator Vapor <watorvapor@gmail.com>
2019-02-06 22:05:22 +0900

    Update create.js

--- www_root/wator/rsaauth/js/create.js
+++ www_root/wator/rsaauth/js/create.js
@@ -33,13 +33,16 @@ RSAAuth.createKeyPair_ = function(cb) {
   console.log('RSAAuth.createKeyPair_:: ecKeypair=<',ecKeypair,'>');
   let jwkPrv = KEYUTIL.getJWKFromKey(ecKeypair.prvKeyObj);
   localStorage.setItem('auth.rsa.key.private',JSON.stringify(jwkPrv));
-  /*
   console.log('RSAAuth.createKeyPair_:: jwkPrv=<',jwkPrv,'>');
   let jwkPub = KEYUTIL.getJWKFromKey(ecKeypair.pubKeyObj);
   console.log('RSAAuth.createKeyPair_:: jwkPub=<',jwkPub,'>');
-  */
+  localStorage.setItem('auth.rsa.key.public',JSON.stringify(jwkPub));
+  
   let pubHex = ecKeypair.pubKeyObj.pubKeyHex;
   console.log('RSAAuth.createKeyPair_:: pubHex=<',pubHex,'>');
+  const token = KJUR.crypto.Util.sha256(pubHex);
+  console.log('RSAAuth.createKeyPair_:: token=<',token,'>');
+  localStorage.setItem('auth.rsa.token',token);
 }
 /*
 RSAAuth.createKeyPair_ = function(cb) {
