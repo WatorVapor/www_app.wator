6c1af7ad9bfa529863497fd73a5299d94a186260
Wator Vapor <watorvapor@gmail.com>
2019-02-06 22:15:07 +0900

    Update create.js

--- www_root/wator/rsaauth/js/create.js
+++ www_root/wator/rsaauth/js/create.js
@@ -1,14 +1,17 @@
 var RSAAuth = RSAAuth || {};
 RSAAuth.debug = RSAAuth.debug || true;
 
+RSAAuth.LS_AUTH_KEY_PRV = 'wator.auth.ecdsa.key.private';
+RSAAuth.LS_AUTH_KEY_PUB = 'wator.auth.ecdsa.key.public';
+RSAAuth.LS_AUTH_KEY_TOKEN = 'wator.auth.ecdsa.key.token';
 RSAAuth.getPubKey = function() {
-  return localStorage.getItem('auth.rsa.key.public');
+  return localStorage.getItem(RSAAuth.LS_AUTH_KEY_PUB);
 }
 RSAAuth.getPriKey = function() {
-  return localStorage.getItem('auth.rsa.key.private');
+  return localStorage.getItem(RSAAuth.LS_AUTH_KEY_PRV);
 }
 RSAAuth.getToken = function() {
-  return localStorage.getItem('auth.rsa.token');
+  return localStorage.getItem(RSAAuth.LS_AUTH_KEY_TOKEN);
 }
 
 /*
@@ -20,9 +23,14 @@ RSAAuth.createKey = function(cb) {
   }
   RSAAuth.createKeyPair_(cb);
 }
+/*
+* 
+*/
+RSAAuth.SS_AUTH_RUN = 'wator.auth.ecdsa.run';
+RSAAuth.SS_AUTH_ACCESS = 'wator.auth.ecdsa.access';
 RSAAuth.clearAccess = function() {
-  sessionStorage.removeItem('auth.rsa.run');
-  sessionStorage.removeItem('auth.rsa.access');
+  sessionStorage.removeItem(RSAAuth.SS_AUTH_RUN);
+  sessionStorage.removeItem(RSAAuth.SS_AUTH_ACCESS);
 }
 
 /*
@@ -30,114 +38,20 @@ RSAAuth.clearAccess = function() {
 */
 RSAAuth.createKeyPair_ = function(cb) {
   const ecKeypair = KEYUTIL.generateKeypair("EC", "P-256");
-  console.log('RSAAuth.createKeyPair_:: ecKeypair=<',ecKeypair,'>');
+  //console.log('RSAAuth.createKeyPair_:: ecKeypair=<',ecKeypair,'>');
   let jwkPrv = KEYUTIL.getJWKFromKey(ecKeypair.prvKeyObj);
-  localStorage.setItem('auth.rsa.key.private',JSON.stringify(jwkPrv));
-  console.log('RSAAuth.createKeyPair_:: jwkPrv=<',jwkPrv,'>');
+  localStorage.setItem(RSAAuth.LS_AUTH_KEY_PRV,JSON.stringify(jwkPrv));
+  //console.log('RSAAuth.createKeyPair_:: jwkPrv=<',jwkPrv,'>');
   let jwkPub = KEYUTIL.getJWKFromKey(ecKeypair.pubKeyObj);
-  console.log('RSAAuth.createKeyPair_:: jwkPub=<',jwkPub,'>');
-  localStorage.setItem('auth.rsa.key.public',JSON.stringify(jwkPub));
+  //console.log('RSAAuth.createKeyPair_:: jwkPub=<',jwkPub,'>');
+  localStorage.setItem(RSAAuth.LS_AUTH_KEY_PUB,JSON.stringify(jwkPub));
   
   let pubHex = ecKeypair.pubKeyObj.pubKeyHex;
-  console.log('RSAAuth.createKeyPair_:: pubHex=<',pubHex,'>');
+  //console.log('RSAAuth.createKeyPair_:: pubHex=<',pubHex,'>');
   const token = KJUR.crypto.Util.sha256(pubHex);
-  console.log('RSAAuth.createKeyPair_:: token=<',token,'>');
-  localStorage.setItem('auth.rsa.token',token);
+  //console.log('RSAAuth.createKeyPair_:: token=<',token,'>');
+  localStorage.setItem(RSAAuth.LS_AUTH_KEY_TOKEN,token);
   if (typeof cb == 'function') {
     cb('success');
   }
 }
-/*
-RSAAuth.createKeyPair_ = function(cb) {
-  console.log(window.crypto.subtle);
-  window.crypto.subtle.generateKey(
-  {
-    name: "RSASSA-PKCS1-v1_5",
-    modulusLength: 4096, //can be 1024, 2048, or 4096
-    publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
-    hash: {name: "SHA-256"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
-    },
-    true, //whether the key is extractable (i.e. can be used in exportKey)
-    ["sign", "verify"] //can be any combination of "sign" and "verify"
-  )
-  .then(function(key){
-    //returns a keypair object
-    if (RSAAuth.debug) {
-      console.log(key);
-      console.log(key.publicKey);
-      console.log(key.privateKey);
-    }
-    window.crypto.subtle.exportKey(
-    "jwk", //can be "jwk" (public or private), "spki" (public only), or "pkcs8" (private only)
-    key.publicKey //can be a publicKey or privateKey, as long as extractable was true
-    )
-    .then(function(keydata){
-      //returns the exported key data
-      if (RSAAuth.debug) {
-        console.log(keydata);
-      }
-      var keyObj = KEYUTIL.getKey(keydata);
-      if (RSAAuth.debug) {
-        console.log(keyObj);
-      }
-      var pem = KEYUTIL.getPEM(keyObj);
-      if (RSAAuth.debug) {
-        console.log(pem);
-      }
-      localStorage.setItem('auth.rsa.key.public',pem);
-      if (RSAAuth.debug) {
-        console.log( typeof cb);
-      }
-      var token = KJUR.crypto.Util.sha512(pem);
-      if (RSAAuth.debug) {
-        console.log(token);
-      }
-      localStorage.setItem('auth.rsa.token',token);
-      if (typeof cb == 'function') {
-        cb('success');
-      }
-    })
-    .catch(function(err){
-      console.error(err);
-      console.trace();
-    });
-    window.crypto.subtle.exportKey(
-    "jwk", //can be "jwk" (public or private), "spki" (public only), or "pkcs8" (private only)
-    key.privateKey //can be a publicKey or privateKey, as long as extractable was true
-    )
-    .then(function(keydata){
-        //returns the exported key data
-      if (RSAAuth.debug) {
-        console.log(keydata);
-      }
-      var keyObj = KEYUTIL.getKey(keydata);
-      if (RSAAuth.debug) {
-        console.log(keyObj);
-      }
-      var pem = KEYUTIL.getPEM(keyObj,"PKCS8PRV");
-      if (RSAAuth.debug) {
-        console.log(pem);
-      }
-      localStorage.setItem('auth.rsa.key.private',pem);
-      if (typeof cb == 'function') {
-        cb('success');
-      }
-    })
-    .catch(function(err){
-      console.error(err);
-      console.trace();
-      if (typeof cb == 'function') {
-        cb('failure');
-      }
-    });
-  })
-  .catch(function(err){
-    console.error(err);
-    console.trace();
-    if (typeof cb == 'function') {
-      cb('failure');
-    }
-  });
-}
-*/
-
