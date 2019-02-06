7502eda8b4a26f2ef6231a3c64520696183b7a8d
Wator Vapor <watorvapor@gmail.com>
2019-02-06 23:42:49 +0900

    Update ecdsa.js

--- ecdsa/ecdsa.js
+++ ecdsa/ecdsa.js
@@ -1,1 +1,15 @@
 const rs = require('jsrsasign');
+const http = require('http');
+const PORT = 17263;
+const HOSTNAME = '127.0.0.1';
+
+const server = http.createServer( (req, res) => {
+  res.writeHead(200, {"Content-Type": "application/json"});
+  let result = {};
+  result.good = true;
+  let json = JSON.stringify(result);
+  res.end(json);
+});
+
+server.listen(PORT, HOSTNAME, function () {
+});
