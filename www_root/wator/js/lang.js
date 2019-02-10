f42e9ef5e56c09dbd574216ef7bafb1bcbfd8385
Wator Vapor <watorvapor@gmail.com>
2019-02-10 19:55:43 +0900

    sendto

--- www_root/wator/js/lang.js
+++ www_root/wator/js/lang.js
@@ -15,30 +15,6 @@ function updateLanguage() {
       console.log('elemLang.value=<',elemLang.value,'>');
     }
     document.forms['secauth_navi_lang_form'].submit();
-
-/*    
-    var url = '/secauth/language';
-    var JSONdata ={lang:lang};
-    $.ajax({
-      type : 'post',
-      url : url,
-      data : JSON.stringify(JSONdata),
-      contentType: 'application/JSON',
-      dataType : 'JSON',
-      scriptCharset: 'utf-8',
-      success : function(data) {
-        // Success
-        console.log('data=<',data,'>');
-        sessionStorage.setItem('operation.lang.run',JSON.stringify(data));
-        location.reload(true);
-      },
-      error : function(data) {
-        // Error
-        console.log(data);
-        sessionStorage.setItem('operation.lang.run.error',JSON.stringify(data));
-      }
-    });
-*/
   }
 }
 
