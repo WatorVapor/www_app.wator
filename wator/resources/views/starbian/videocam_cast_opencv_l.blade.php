47d6ae28a6662300f47bfbe1ae84f818a26043ec
Wator Vapor <watorvapor@gmail.com>
2019-04-06 22:24:27 +0900

    wai text searcher

--- wator/resources/views/starbian/videocam_cast_opencv_l.blade.php
+++ wator/resources/views/starbian/videocam_cast_opencv_l.blade.php
@@ -51,8 +51,8 @@
 
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/webrtc-adapter/6.4.0/adapter.min.js" integrity="sha256-UH0Npcih7yj1s23pQK0UCrCSxx7AkT91CCMsUGnZ9Ew=" crossorigin="anonymous"></script>
-<script src="/wator/starbian/js/starbian.js" type="text/javascript"></script>
-<script src="/wator/starbian/js/starbian_rtc.js" type="text/javascript"></script>
+<script src="/wator/starbian/js/starbian.js?v={{ time() }}" type="text/javascript"></script>
+<script src="/wator/starbian/js/starbian_rtc.js?v={{ time() }}" type="text/javascript"></script>
 
 
 @include('starbian.js.videocam_cast_cv')
