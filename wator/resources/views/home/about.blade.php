cf352d5efbcc76a77d14187347954c2d04279125
Wator Vapor <watorvapor@gmail.com>
2019-05-26 21:51:15 +0900

    color

--- wator/resources/views/home/about.blade.php
+++ wator/resources/views/home/about.blade.php
@@ -4,7 +4,7 @@
 @endsection
 @section('content')
 <div class="row justify-content-center mt-5 mb-1" >
-  <div class="col-8 text-left text-nowrap bg-light">
+  <div class="col-8 text-left text-nowrap text-primary bg-light">
     <pre class="font-weight-bold" >{{trans('about.content')}}</pre>
   </div>
 </div>
