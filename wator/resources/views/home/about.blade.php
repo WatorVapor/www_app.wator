afbdf17f8111496db7127badbb5dd38d73e4aaf1
Wator Vapor <watorvapor@gmail.com>
2019-05-26 21:50:00 +0900

    wai text searcher

--- wator/resources/views/home/about.blade.php
+++ wator/resources/views/home/about.blade.php
@@ -3,8 +3,8 @@
   @include('home.navbar')
 @endsection
 @section('content')
-<div class="row justify-content-center mt-3" >
-  <div class="col-8 text-left text-nowrap bg-secondary">
+<div class="row justify-content-center mt-5 mb-1" >
+  <div class="col-8 text-left text-nowrap bg-light">
     <pre class="font-weight-bold" >{{trans('about.content')}}</pre>
   </div>
 </div>
